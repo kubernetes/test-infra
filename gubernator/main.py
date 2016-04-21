d7764fab544ab55c7d84c782a01b3781e0cf4196
Ryan Hitchman <rmmh@google.com>
2016-04-21 12:15:43 -0700

    [gubernator] Limit the list of builds to display only 150.

--- gubernator/main.py
+++ gubernator/main.py
@@ -55,7 +55,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     trim_blocks=True,
     autoescape=True)
-
+JINJA_ENVIRONMENT.line_statement_prefix = '%'
 filters.register(JINJA_ENVIRONMENT.filters)
 
 
@@ -99,7 +99,6 @@ def memcache_memoize(prefix, exp=60 * 60, neg_exp=60):
     return wrapper
 
 
-@memcache_memoize('gs://')
 def gcs_read(path):
     """Read a file from GCS. Returns None on errors."""
     try:
