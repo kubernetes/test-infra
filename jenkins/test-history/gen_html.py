1f52b5d4bbf6cb81fd388b6d69e923408ac3bb0c
Joe Finney <spxtr@google.com>
2016-05-13 16:47:08 -0700

    Rewrite gen_html.

--- jenkins/test-history/gen_html.py
+++ jenkins/test-history/gen_html.py
@@ -14,61 +14,59 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Creates an HTML report for all jobs starting with a given prefix.
+"""Creates an HTML report out of the JSON."""
 
-Reads the JSON from tests.json, and prints the HTML to stdout.
 
-This code is pretty nasty, but gets the job done.
-
-It would be really spiffy if this used an HTML template system, but for now
-we're old-fashioned. We could also generate these with JS, directly from the
-JSON. That would allow custom filtering and stuff like that.
-"""
-
+from __future__ import division
 from __future__ import print_function
 
+
 import argparse
-import cgi
 import collections
+import jinja2
 import json
 import os
 import re
-import string
 import sys
 import time
 
 
-TestMetadata = collections.namedtuple('TestMetadata', [
-    'okay',
-    'unstable',
+BLOCKING_JOBS = [
+    'kubelet-gce-e2e-ci',
+    'kubernetes-build',
+    'kubernetes-e2e-gce',
+    'kubernetes-e2e-gce-scalability',
+    'kubernetes-e2e-gce-slow',
+    'kubernetes-e2e-gke',
+    'kubernetes-e2e-gke-slow',
+    'kubernetes-kubemark-5-gce',
+    'kubernetes-kubemark-500-gce',
+    'kubernetes-test-go',
+]
+
+JobSummary = collections.namedtuple('JobSummary', [
+    'name',
+    'passed',
     'failed',
-    'skipped',
+    'latest_failure',
+    'tests',
+    'stable',
+    'unstable',
+    'broken',
 ])
 
 
-BUCKET_TO_PREFIX = {}
-PREFIX_TO_URL = {}
-
-
-def load_buckets_info(infile):
-    BUCKET_TO_PREFIX.clear()
-    with open(infile) as buf:
-        buckets = json.load(buf)
+def load_prefixes(in_file):
+    """Returns a dictionary from bucket to prefix using in_file."""
+    result = {}
+    buckets = json.load(in_file)
     for bucket, data in buckets.iteritems():
-        BUCKET_TO_PREFIX[bucket] = data['prefix']
-
-
-def prefix_for(bucket):
-    prefix = BUCKET_TO_PREFIX.get(bucket)
-    if prefix is None:
-        prefix = bucket[5:-1].replace('/', '_') + '$'
-    PREFIX_TO_URL[prefix] = bucket
-    return prefix
+        result[bucket] = data['prefix']
+    return result
 
 
 def slugify(inp):
-    """
-    Convert a string into a url-safe slug fragment.
+    """Convert a string into a url-safe slug fragment.
 
     This matches the slugify code in Gubernator.
     """
@@ -76,284 +74,173 @@ def slugify(inp):
     return re.sub(r'\s+', '-', inp).lower()
 
 
-def gubernator_url(suite, build_number, test_name):
+def gubernator_url(bucket, job, build, test_name=''):
     """Build a link to a test failure on Gubernator."""
-    if '$' in suite:
-        prefix, suite = suite.split('$')
-        path = PREFIX_TO_URL[prefix + '$']
-    else:
-        path = PREFIX_TO_URL['']
-    if path.startswith('gs://'):
-        path = path[5:]
-    return 'https://k8s-gubernator.appspot.com/build/%s%s/%s#%s' % (
-        path, suite, build_number, slugify(test_name))
+    return 'https://k8s-gubernator.appspot.com/build/{}{}/{}#{}'.format(
+        bucket[5:], job, build, slugify(test_name))
 
 
-def gen_tests(data, prefix, exact_match):
-    """Creates the HTML for all test cases.
+def job_results(bucket, prefix, job_name, job_data, test_names):
+    """Generates a JobSummary namedtuple along with a list of test results.
+
+    This function is a bit of a monolith and should probably be cleared up.
 
     Args:
-        data: Parsed JSON data that was created by gen_json.py.
-        prefix: Considers Jenkins jobs that start with this.
-        exact_match: Only match Jenkins jobs with name equal to prefix.
-
+        bucket: The bucket name.
+        prefix: The contributor prefix to prepend to the job name.
+        job_name: The job name.
+        job_data: The JSON data as returned by list_jobs.
+        test_names: The JSON test names data.
     Returns:
-        (html, TestMetadata) for matching tests
+        A (JobSummary, tests) tuple where tests is a list of dicts with this
+        structure, sorted by failure count, passed count, then name: [{
+            'name': 'abc',
+            'runs': 1,
+            'passed': 1,
+            'failed': 0,
+            'duration': 0.0,
+            'latest_failure': None or 'https://gubernater-link...',
+        }]
     """
-    # TODO: convert this whole mess to use a real templating language.
-    html = ['<ul class="test">']
-    totals = collections.defaultdict(int)
-    for test in sorted(data, key=string.lower):
-        test_html = ['<ul class="suite">']
-        has_test = False
-        has_failed = False
-        has_unstable = False
-        for suite in sorted(data[test]):
-            if not suite.startswith(prefix):
-                continue
-            if exact_match and suite != prefix:
-                continue
-            has_test = True
-            num_failed = 0
-            num_builds = 0
-            total_time = 0
-            most_recent_failure = None
-            for build in data[test][suite]:
-                num_builds += 1
-                if build['failed']:
-                    num_failed += 1
-                    most_recent_failure = build['build']
-                total_time += build['time']
-            avg_time = total_time / num_builds
-            unit = 's'
-            if avg_time > 60:
-                avg_time /= 60
-                unit = 'm'
-            if num_failed == num_builds:
-                has_failed = True
-                status = 'failed'
-            elif num_failed > 0:
-                has_unstable = True
-                status = 'unstable'
+    full_job_name = '{}{}'.format(prefix, job_name)
+    num_failed = 0
+    num_passed = 0
+    latest_failure = None
+    test_summaries = {}
+    for build in job_data:
+        build_passed = True
+        tests_data = job_data[build]['tests']
+        for test in tests_data:
+            test_id = test['name']
+            if test_id not in test_summaries:
+                test_summaries[test_id] = {
+                    'name': test_names[test_id],
+                    'runs': 0,
+                    'passed': 0,
+                    'failed': 0,
+                    'duration': 0.0,
+                    'latest_failure': None,
+                }
+            if 'skipped' in test:
+                pass
             else:
-                status = 'okay'
-            test_html.append('<li class="suite">')
-            fail_rate = '%d/%d' % (num_builds - num_failed, num_builds)
-            if most_recent_failure:
-                fail_rate = ('<a href="%s" title="Latest Failure">%s</a>' % (
-                    gubernator_url(suite, most_recent_failure, test),
-                    fail_rate))
-            suite_results = '<span class="%s">%s</span>' % (
-                status, fail_rate)
-            suite_results += ' <span class="time">%.0f%s</span>' % (
-                avg_time, unit)
-            if most_recent_failure:
-                suite_results += '</a>'
-            test_html.append(suite_results)
-            test_html.append(suite)
-            test_html.append('</li>')
-        test_html.append('</ul>')
-        if has_failed:
-            status = 'failed'
-        elif has_unstable:
-            status = 'unstable'
-        elif has_test:
-            status = 'okay'
+                test_summaries[test_id]['runs'] += 1
+                test_summaries[test_id]['duration'] += test['time']
+                if 'failed' in test:
+                    test_summaries[test_id]['latest_failure'] = build
+                    latest_failure = build
+                    build_passed = False
+                    test_summaries[test_id]['failed'] += 1
+                else:
+                    test_summaries[test_id]['passed'] += 1
+        if build_passed:
+            num_passed += 1
         else:
-            status = 'skipped'
-        totals[status] += 1
-        html.append('<li class="test %s">' % status)
-        if exact_match and len(test_html) == 6:
-            # There's a test result, place it to the left of the test name
-            # instead of in a list underneath it.
-            if not test_html[2].startswith('<span'):
-                raise ValueError('couldn\'t extract results for prepending')
-            html.append(test_html[2])
-            html.append(test)
-        else:
-            html.append(test)
-            html.extend(test_html)
-        html.append('</li>')
-    html.append('</ul>')
-    return '\n'.join(html), TestMetadata(
-        totals['okay'], totals['unstable'], totals['failed'], totals['skipped'])
+            num_failed += 1
+    stable = 0
+    unstable = 0
+    broken = 0
+    tests = []
+    for test in sorted(test_summaries.values(), key=lambda t: t['name'].lower()):
+        if test['latest_failure'] is not None:
+            test['latest_failure'] = gubernator_url(bucket, job_name, test['latest_failure'], test['name'])
+        if test['runs'] > 0:
+            tests.append(test)
+            if test['failed'] == 0:
+                stable += 1
+            elif test['failed'] < test['runs']:
+                unstable += 1
+            else:
+                broken += 1
+    if latest_failure is not None:
+        latest_failure = gubernator_url(bucket, job_name, latest_failure)
+    job_summary = JobSummary(
+        full_job_name,
+        num_passed,
+        num_failed,
+        latest_failure,
+        stable + unstable + broken,
+        stable,
+        unstable,
+        broken)
+    tests = sorted(tests, key=lambda t: (t['failed'], t['passed']), reverse=True)
+    return job_summary, tests
 
 
-def html_header(title, script):
-    """Return html header items."""
-    html = ['<html>', '<head>']
-    html.append('<link rel="stylesheet" type="text/css" href="style.css" />')
-    if title:
-        html.append('<title>%s</title>' % cgi.escape(title))
-    if script:
-        html.append('<script src="script.js"></script>')
-    html.append('</head>')
-    html.append('<body>')
-    return html
+def list_jobs(data):
+    """Generates a (bucket, name, data) tuple for each job."""
+    for bucket, jobs in data['buckets'].iteritems():
+        for name, job in jobs.iteritems():
+            yield bucket, name, job
 
 
-def gen_html(data, prefix, exact_match=False):
-    """Creates the HTML for the entire page.
+def merge_bad_tests(bad_tests, new_tests):
+    """Merge unstable and broken tests from new_tests into bad_tests.
 
     Args:
-        Same as gen_tests.
+        bad_tests: Dictionary from test name to test results.
+        new_tests: List of new test results to merge.
     Returns:
-        Same as gen_tests.
+        Nothing, modifies bad_tests in place.
     """
-    tests_html, meta = gen_tests(data, prefix, exact_match)
-    if exact_match:
-        msg = 'Suite %s' % cgi.escape(prefix)
-    elif prefix:
-        msg = 'Suites starting with %s' % cgi.escape(prefix)
-    else:
-        msg = 'All suites'
-    html = html_header(title=msg, script=True)
-    html.append('<div id="header">%s:' % msg)
-    fmt = '<span class="total %s" onclick="toggle(\'%s\');">%s</span>'
-    html.append(fmt % ('okay', 'okay', meta.okay))
-    html.append(fmt % ('unstable', 'unstable', meta.unstable))
-    html.append(fmt % ('failed', 'failed', meta.failed))
-    html.append(fmt % ('skipped', 'skipped', meta.skipped))
-    html.append('</div>')
-    html.append(tests_html)
-    html.append('</body>')
-    html.append('</html>')
-    return '\n'.join(html), meta
+    for new_test in new_tests:
+        if new_test['failed'] > 0:
+            name = new_test['name']
+            if name not in bad_tests:
+                bad_tests[name] = {
+                    'name': name,
+                    'runs': 0,
+                    'passed': 0,
+                    'failed': 0,
+                    'latest_failure': None,
+                }
+            bad_tests[name]['runs'] += new_test['runs']
+            bad_tests[name]['passed'] += new_test['passed']
+            bad_tests[name]['failed'] += new_test['failed']
+            bad_tests[name]['latest_failure'] = new_test['latest_failure']
 
 
-def gen_metadata_links(suites):
-    """Write clickable pass, ustabled, failed stats."""
-    html = []
-    for (name, target), meta in sorted(suites.iteritems()):
-        html.append('<a class="suite-link" href="%s">' % target)
-        html.append('<span class="total okay">%d</span>' % meta.okay)
-        html.append('<span class="total unstable">%d</span>' % meta.unstable)
-        html.append('<span class="total failed">%d</span>' % meta.failed)
-        html.append(name)
-        html.append('</a>')
-    return html
+def main(in_path, buckets_path, out_dir):
+    """Uses in_path and buckets_path to write a static report under out_dir."""
+    with open(in_path) as data_file:
+        data = json.load(data_file)
 
+    templates_path = '{}/templates'.format(os.path.abspath(os.path.dirname(__file__)))
 
-def write_html(outdir, path, html):
-    """Write html to outdir/path."""
-    with open(os.path.join(outdir, path), 'w') as buf:
-        buf.write(html)
+    summaries = []
+    bad_tests = {}
+    with open(buckets_path) as buckets_file:
+        prefixes = load_prefixes(buckets_file)
+    for bucket, job_name, job_data in list_jobs(data):
+        if bucket not in prefixes:
+            raise ValueError('Unknown bucket: {}'.format(bucket))
+        prefix = prefixes[bucket]
+        full_name = '{}{}'.format(prefix, job_name)
+        job, tests = job_results(bucket, prefix, job_name, job_data, data['test_names'])
+        if full_name in BLOCKING_JOBS:
+            merge_bad_tests(bad_tests, tests)
+        summaries.append(job)
+        if job.tests > 0:
+            with open('{}/job.html'.format(templates_path)) as job_template_file:
+                job_template = jinja2.Template(job_template_file.read())
+            job_html = job_template.render({
+                'job_name': job_name,
+                'tests': tests,
+            })
+            with open('{}/suite-{}.html'.format(out_dir, full_name), 'w') as job_file:
+                job_file.write(job_html)
+    summaries = sorted(summaries)
+    blocking_job_summaries = filter(lambda s: s.name in BLOCKING_JOBS, summaries)
 
-
-def transpose(data):
-    """
-    Convert data from the format that gen_json creates (build-major)
-    to one that's more suitable for our processing (test-major).
-
-    Args:
-        data: dict {"buckets": {prefix: {job:
-            {build: {"tests": [{name, time, ...}]}}}}}
-
-    Returns:
-        dict {test-name: {job: [{build, time, ...}]}}
-    """
-    out = {}
-    names = data['test_names']
-    for prefix, jobs in data['buckets'].iteritems():
-        for job, builds in jobs.iteritems():
-            job = prefix_for(prefix) + job
-            for build_number, build in builds.items():
-                for test in build['tests']:
-                    if test.get('skipped'):
-                        continue
-                    out_test = out.setdefault(names[test['name']], {})
-                    out_test.setdefault(job, []).append(
-                        {'build': build_number,
-                         'time': test['time'],
-                         'failed': test.get('failed', False)})
-    return out
-
-def write_metadata(infile, outdir):
-    """Writes tests-*.html and suite-*.html files.
-
-    Args:
-      infile: the json file created by gen_json.py
-      outdir: a path to write the html files.
-    """
-    with open(infile) as buf:
-        data = json.load(buf)
-
-    data = transpose(data)
-
-    prefix_metadata = {}
-    prefixes = [
-        'kubernetes',
-        'kubernetes-e2e',
-        'kubernetes-soak',
-        'kubernetes-e2e-gce',
-        'kubernetes-e2e-gke',
-        'kubernetes-upgrade',
-    ]
-    for prefix in prefixes:
-        path = 'tests-%s.html' % prefix
-        html, metadata = gen_html(data, prefix, False)
-        write_html(outdir, path, html)
-        prefix_metadata[prefix or 'kubernetes', path] = metadata
-
-    suite_metadata = {}
-    suites = set()
-    for suite_names in data.values():
-        suites.update(suite_names.keys())
-    for suite in sorted(suites):
-        path = 'suite-%s.html' % suite
-        html, metadata = gen_html(data, suite, True)
-        write_html(outdir, path, html)
-        suite_metadata[suite, path] = metadata
-
-    blocking = {
-        'kubelet-gce-e2e-ci',
-        'kubernetes-build',
-        'kubernetes-e2e-gce',
-        'kubernetes-e2e-gce-scalability',
-        'kubernetes-e2e-gce-slow',
-        'kubernetes-e2e-gke',
-        'kubernetes-e2e-gke-slow',
-        'kubernetes-kubemark-5-gce',
-        'kubernetes-kubemark-500-gce',
-        'kubernetes-test-go',
-    }
-    blocking_suite_metadata = {
-        k: v for (k, v) in suite_metadata.items() if k[0] in blocking}
-
-    return prefix_metadata, suite_metadata, blocking_suite_metadata
-
-
-def write_index(outdir, prefixes, suites, blockers):
-    """Write the index.html with links to each view, including stat summaries.
-
-    Args:
-      outdir: the path to write the index.html file
-      prefixes: the {(prefix, path): TestMetadata} map
-      suites: the {(suite, path): TestMetadata} map
-      blockers: the {(suite, path): TestMetadata} map of blocking suites
-    """
-    html = html_header(title='Kubernetes Test Summary', script=False)
-    html.append('<h1>Kubernetes Tests</h1>')
-    html.append('Last updated %s' % time.strftime('%F %T %Z'))
-
-    html.append('<h2>Tests from suites starting with:</h2>')
-    html.extend(gen_metadata_links(prefixes))
-
-    html.append('<h2>Blocking suites:</h2>')
-    html.extend(gen_metadata_links(blockers))
-
-    html.append('<h2>All suites:</h2>')
-    html.extend(gen_metadata_links(suites))
-
-    html.extend(['</body>', '</html>'])
-    write_html(outdir, 'index.html', '\n'.join(html))
-
-
-def main(infile, buckets, outdir):
-    """Use infile to write test, suite and index html files to outdir."""
-    load_buckets_info(buckets)
-    prefixes, suites, blockers = write_metadata(infile, outdir)
-    write_index(outdir, prefixes, suites, blockers)
+    with open('{}/index.html'.format(templates_path)) as index_template_file:
+        index_template = jinja2.Template(index_template_file.read())
+    index_html = index_template.render({
+        'last_updated': time.ctime(),
+        'job_groups': [blocking_job_summaries, summaries],
+        'bad_tests': sorted(bad_tests.values(), key=lambda t: t['failed'], reverse=True),
+    })
+    with open('{}/index.html'.format(out_dir), 'w') as index_file:
+        index_file.write(index_html)
 
 
 def get_options(argv):
