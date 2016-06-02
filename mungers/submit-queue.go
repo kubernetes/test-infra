58fd99c91cbb54ff341762375d7884f4ee86e161
Daniel Smith <dbsmith@google.com>
2016-06-02 14:28:32 -0700

    Add serial back into the list

--- mungers/submit-queue.go
+++ mungers/submit-queue.go
@@ -378,6 +378,7 @@ func (sq *SubmitQueue) AddFlags(cmd *cobra.Command, config *github.Config) {
 		"kubernetes-test-go",
 		"kubernetes-e2e-gce",
 		"kubernetes-e2e-gce-slow",
+		"kubernetes-e2e-gce-serial",
 		"kubernetes-e2e-gke",
 		"kubernetes-e2e-gke-slow",
 		"kubernetes-e2e-gce-scalability",
