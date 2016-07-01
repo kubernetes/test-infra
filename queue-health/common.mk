1fc120c825a128ee059ad2dd56b0bdd318f9e662
Erick Fejta <fejta@google.com>
2016-07-01 15:16:02 -0700

    Use a service account to access gcs

--- queue-health/common.mk
+++ queue-health/common.mk
@@ -1,14 +1,15 @@
 # Please set IMG
 TAG = $(shell date +v%Y%m%d)
 
+all: push
+
 build:
 	docker build -t $(IMG):$(TAG) .
-	docker tag $(IMG):$(TAG) $(IMG):latest
+	docker tag -f $(IMG):$(TAG) $(IMG):latest
 	@echo Built $(IMG):$(TAG) and tagged with latest
 
-push:
+push: build
 	gcloud docker push $(IMG):$(TAG)
 	gcloud docker push $(IMG):latest
 	@echo Pushed $(IMG) with :latest and :$(TAG) tags
 
-all: build push
