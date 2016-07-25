5a749b5ebd2f96f3ef33a8cb84961d0fd0f9e994
Garrett Rodrigues <grod@google.com>
2016-07-25 08:49:36 -0700

    Create the Issue Label Triager
    Create a machine learning tool to triage the github issue labeling
    process.  Offline, we have used some validation techniques to select a
    good model and hyperparameters (regularization parameters) for the ml
    model.
    
    Then, we use the flask microframework to expose the machine
    learning model via a webapp that exposes two endpoints:
    	/ [POST]: ask the ml model to predict labels for an issue's
    		title and body
    	/update_models [PUT]: train or update the ml model
    We store the model in a 10GB persistent volume on GCP.
    
    The deploy.sh file controls the gke deployment. It builds th
    container, registers it (a cmdline param for the script), and exposes a
    a service on port 5000 so that other pods can query for webapp for
    labels.  It creates the deployment on gke and mounts the PV at /model/,
    and it starts the webapp.

---
+++ mungers/issue-categorizer.go
@@ -0,0 +1,104 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mungers
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+
+	"k8s.io/contrib/mungegithub/github"
+
+	"io/ioutil"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"k8s.io/contrib/mungegithub/features"
+)
+
+// LabelMunger will update a label on a PR based on how many lines are changed.
+// It will exclude certain files in it's calculations based on the config
+// file provided in --generated-files-config
+type LabelMunger struct {
+}
+
+// Initialize will initialize the munger
+func (LabelMunger) Initialize(config *github.Config, features *features.Features) error {
+	return nil
+}
+
+// Name is the name usable in --pr-mungers
+func (LabelMunger) Name() string { return "LabelMunger" }
+
+// RequiredFeatures is a slice of 'features' that must be provided
+func (LabelMunger) RequiredFeatures() []string { return []string{} }
+
+// AddFlags will add any request flags to the cobra `cmd`
+func (LabelMunger) AddFlags(cmd *cobra.Command, config *github.Config) {
+}
+
+func init() {
+	s := &LabelMunger{}
+	RegisterMungerOrDie(s)
+}
+
+// EachLoop is called at the start of every munge loop
+func (LabelMunger) EachLoop() error { return nil }
+
+// Munge is the workhorse the will actually make updates to the PR
+func (s *LabelMunger) Munge(obj *github.MungeObject) {
+	//this munger only works on issues
+	if obj.IsPR() {
+		return
+	}
+
+	issue := obj.Issue
+
+	if obj.HasLabel("kind/flake") {
+		return
+	}
+
+	tLabels := github.GetLabelsWithPrefix(issue.Labels, "team/")
+
+	if len(tLabels) != 0 {
+		//already labeled
+		return
+	}
+
+	cLabels := github.GetLabelsWithPrefix(issue.Labels, "components/")
+	if len(cLabels) != 0 {
+		//already labeled
+		return
+	}
+
+	routingLabelsToApply, err := http.PostForm("http://localhost:5000",
+		url.Values{"title": {*issue.Title}, "body": {*issue.Body}})
+
+	if err != nil {
+		//handle the error
+		glog.Error(err)
+		return
+	}
+	defer routingLabelsToApply.Body.Close()
+	response, err := ioutil.ReadAll(routingLabelsToApply.Body)
+	if routingLabelsToApply.StatusCode != 200 {
+		glog.Errorf("%d: %s", routingLabelsToApply.StatusCode, response)
+		return
+	}
+
+	obj.AddLabels(strings.Split(string(response), ";"))
+}
