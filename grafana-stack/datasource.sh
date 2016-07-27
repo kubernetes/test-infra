3570615c83487b1dfa8ec627af80a8d99d2d89b8
Antoine Pelisse <apelisse@google.com>
2016-07-27 09:14:57 -0700

    Create deployment for nginx/influxdb/grafana
    Create a deploy script to deploy an nginx server that reverse-proxies
    influxdb and grafana to serve graphs.
    
    Everything should be done automatically for easy set-up.

---
+++ grafana-stack/datasource.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright 2016 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+if [[ $# -ne 3 ]]; then
+  echo "usage: $0 server_hostname grafana_admin_password influxdb_root_password" >&2
+  exit 64
+fi
+
+server_hostname=$1
+grafana_admin_password=$2
+influxdb_root_password=$3
+
+curl -s --fail "http://admin:${grafana_admin_password}@${server_hostname}/api/datasources/name/github" ||
+env - server_hostname="${server_hostname}" influxdb_root_password="${influxdb_root_password}" envsubst <<EOF |
+{
+  "name": "github",
+  "type": "influxdb",
+  "access": "direct",
+  "url": "http://${server_hostname}/influxdb/",
+  "user": "root",
+  "password": "${influxdb_root_password}",
+  "database": "github",
+  "isDefault": true
+}
+EOF
+curl \
+  -X POST --fail \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  "http://admin:${grafana_admin_password}@${server_hostname}/api/datasources" \
+  --data-binary @-
