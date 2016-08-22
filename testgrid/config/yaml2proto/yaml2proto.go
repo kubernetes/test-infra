62dcd38974f32f701d854eb5dc8b95449ba22dfb
Sen Lu <senlu@google.com>
2016-08-22 14:43:42 -0700

    Updated proto from proto2 to proto3
    - Updated config.proto to proto3, and changed the column_header to take
      in repeated configuration_values
    - Fix some syntax in go code
    - Updated test code

---
+++ testgrid/config/yaml2proto/yaml2proto.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package yaml2proto
+
+import (
+	"fmt"
+	"errors"
+	"gopkg.in/yaml.v2"
+
+	"github.com/golang/protobuf/proto"
+	pb "k8s.io/test-infra/testgrid/config/pb"
+)
+
+func Yaml2Proto(yamlData []byte) ([]byte, error) {
+	
+	// Unmarshal yaml to config
+	config := &pb.Configuration{}
+	err := yaml.Unmarshal(yamlData,&config)
+	if err != nil {
+		fmt.Printf("Unmarshal Error : %v\n", err);
+		return nil, err
+	}
+
+	// Reject empty yaml ( No testgroups or dashboards )
+	if len(config.TestGroups) == 0 {
+		return nil, errors.New("Invalid Yaml : No Valid Testgroups")
+	}
+
+	if len(config.Dashboards) == 0 {
+		return nil, errors.New("Invalid Yaml : No Valid Dashboards")
+	}
+
+	// validating the yaml file:
+	// is_external and user_kubernetes_client should always be true
+	for _,testgroup := range config.TestGroups {
+		if !testgroup.IsExternal {
+			fmt.Printf("[Warning:] Testgroup %v : is_external should always be true\n",
+						testgroup.Name );
+			testgroup.IsExternal = true
+		}
+
+		if !testgroup.UseKubernetesClient {
+			fmt.Printf("[Warning:] Testgroup %v : use_kubernetes_client should always be true\n",
+						testgroup.Name );
+			testgroup.UseKubernetesClient = true
+		}
+	}
+	 
+	// Marshal config to protobuf
+	return proto.Marshal(config)
+}
---
+++ testgrid/config/yaml2proto/yaml2proto_test.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package yaml2proto
+
+import (
+	"testing"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/proto"
+	pb "k8s.io/test-infra/testgrid/config/pb"
+)
+
+func TestYaml2ProtoEmpty(t *testing.T) {
+	yamlData, err := ioutil.ReadFile("testdata/Empty.yaml")
+	if err != nil {
+		t.Errorf("IO Error : Cannot Open File Empty.yaml\n")
+	}
+
+	_, err = Yaml2Proto(yamlData)
+	
+	if err == nil || err.Error() != "Invalid Yaml : No Valid Testgroups" {
+		t.Errorf("Should Throw No Valid Testgroups Error\n")
+	}
+}
+
+func TestYaml2ProtoNoTestgroup(t *testing.T) {
+	yamlData, err := ioutil.ReadFile("testdata/NoTestgroup.yaml")
+	if err != nil {
+		t.Errorf("IO Error : Cannot Open File NoTestgroup.yaml\n")
+	}
+
+	_, err = Yaml2Proto(yamlData)
+	
+	if err == nil || err.Error() != "Invalid Yaml : No Valid Testgroups" {
+		t.Errorf("Should Throw No Valid Testgroups Error\n")
+	}
+}
+
+func TestYaml2ProtoNoDashboard(t *testing.T) {
+	yamlData, err := ioutil.ReadFile("testdata/NoDashboard.yaml")
+	if err != nil {
+		t.Errorf("IO Error : Cannot Open File NoDashboard.yaml\n")
+	}
+
+	_, err = Yaml2Proto(yamlData)
+	
+	if err == nil || err.Error() != "Invalid Yaml : No Valid Dashboards" {
+		t.Errorf("Should Throw No Valid Dashboards Error\n")
+	}
+}
+
+func TestYaml2ProtoIsExternalFalse(t *testing.T) {
+	yamlData, err := ioutil.ReadFile("testdata/IsExternalFalse.yaml")
+	if err != nil {
+		t.Errorf("IO Error : Cannot Open File IsExternalFalse.yaml\n")
+	}
+
+	protobufData, err := Yaml2Proto(yamlData)
+	
+	if err != nil {
+		t.Errorf("Convert Error: %v\n", err)
+	}
+
+	config := &pb.Configuration{}
+	if err := proto.Unmarshal(protobufData, config); err != nil {
+		t.Errorf("Failed to parse config: %v\n", err)
+	}
+
+	for _,testgroup := range config.TestGroups {
+		if !testgroup.IsExternal {
+			t.Errorf("IsExternal should always be true!")
+		}
+	}
+}
+
+func TestYaml2ProtoUseKuberClientFalse(t *testing.T) {
+	yamlData, err := ioutil.ReadFile("testdata/UseKuberClientFalse.yaml")
+	if err != nil {
+		t.Errorf("IO Error : Cannot Open File UseKuberClientFalse.yaml\n")
+	}
+
+	protobufData, err := Yaml2Proto(yamlData)
+	
+	if err != nil {
+		t.Errorf("Convert Error: %v\n", err)
+	}
+
+	config := &pb.Configuration{}
+	if err := proto.Unmarshal(protobufData, config); err != nil {
+		t.Errorf("Failed to parse config: %v\n", err)
+	}
+
+	for _,testgroup := range config.TestGroups {
+		if !testgroup.UseKubernetesClient {
+			t.Errorf("UseKubernetesClient should always be true!")
+		}
+	}
+}
+
+func TestYaml2ProtoLarge(t *testing.T) {
+	yamlData, err := ioutil.ReadFile("testdata/large.yaml")
+	if err != nil {
+		t.Errorf("IO Error : Cannot Open File large.yaml\n")
+	}
+
+	protobufData, err := Yaml2Proto(yamlData)
+	
+	if err != nil {
+		t.Errorf("Convert Error: %v\n", err)
+	}
+
+	config := &pb.Configuration{}
+	if err := proto.Unmarshal(protobufData, config); err != nil {
+		t.Errorf("Failed to parse config: %v\n", err)
+	}
+
+	if len(config.TestGroups) != 179 {
+		t.Errorf("TestGroup Count Not 179: %v\n", len(config.TestGroups))
+	}
+
+	if len(config.Dashboards) != 14 {
+		t.Errorf("Dashboard Count Not 14: %v\n", len(config.Dashboards))
+	}
+}
+
+
