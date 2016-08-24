kubernetes-retired/contrib

Add Pinger to nag people

mungegithub/mungers/matchers/comment/pinger.go
@@ -1,0 +1,118 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comment
+
+import (
+	"time"
+
+	"github.com/google/go-github/github"
+)
+
+// Pinger checks if it's time to send a ping.
+// You can build a pinger for a specific use-case and re-use it when you want.
+type Pinger struct {
+	keyword     string        // Short description for the ping
+	description string        // Long description for the ping
+	timePeriod  time.Duration // How often should we ping
+	maxCount    int           // Will stop pinging after that many occurences
+}
+
+// NewPinger creates a new pinger. `keyword` is the name of the notification.
+func NewPinger(keyword string) *Pinger {
+	return &Pinger{
+		keyword: keyword,
+	}
+}
+
+// SetDescription is the description that goes along the ping
+func (p *Pinger) SetDescription(description string) *Pinger {
+	p.description = description
+
+	return p
+}
+
+// SetTimePeriod is the time we wait between pings
+func (p *Pinger) SetTimePeriod(timePeriod time.Duration) *Pinger {
+	p.timePeriod = timePeriod
+
+	return p
+}
+
+// SetMaxCount will make the pinger fail when it reaches maximum
+func (p *Pinger) SetMaxCount(maxCount int) *Pinger {
+	p.maxCount = maxCount
+
+	return p
+}
+
+// PingNotification creates a new notification to ping `who`
+func (p *Pinger) PingNotification(comments []*github.IssueComment, who string, startDate *time.Time) *Notification {
+	if startDate == nil {
+		startDate = &time.Time{}
+	}
+
+	pings := FilterComments(
+		comments,
+		And([]Matcher{
+			CreatedAfter(*startDate),
+			MungerNotificationName(p.keyword),
+		}),
+	)
+
+	// We have pinged too many times, it's time to try something else
+	if p.isMaxReached(pings) {
+		return nil
+	}
+
+	if !p.shouldPingNow(pings, startDate) {
+		return nil
+	}
+
+	return &Notification{
+		Name:      p.keyword,
+		Arguments: who,
+		Context:   p.description,
+	}
+}
+
+// MaxReached tells you if you've reached the limit yet
+func (p *Pinger) MaxReached(comments []*github.IssueComment, startDate *time.Time) bool {
+	if startDate == nil {
+		startDate = &time.Time{}
+	}
+	return p.isMaxReached(FilterComments(
+		comments,
+		And([]Matcher{
+			CreatedAfter(*startDate),
+			MungerNotificationName(p.keyword),
+		}),
+	))
+}
+
+func (p *Pinger) isMaxReached(pings FilteredComments) bool {
+	return p.maxCount != 0 && len(pings) >= p.maxCount
+}
+
+func (p *Pinger) shouldPingNow(pings FilteredComments, startDate *time.Time) bool {
+	lastEvent := startDate
+
+	if len(pings) != 0 {
+		lastEvent = pings[len(pings)-1].CreatedAt
+	}
+
+	return time.Since(*lastEvent) > p.timePeriod
+}

mungegithub/mungers/matchers/comment/pinger_test.go
@@ -1,0 +1,136 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comment
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-github/github"
+)
+
+func timeAgo(d time.Duration) *time.Time {
+	t := time.Now().Add(-d)
+	return &t
+}
+
+func makeComment(body, author string, beforeNow time.Duration) *github.IssueComment {
+	return &github.IssueComment{
+		Body:      &body,
+		User:      &github.User{Login: &author},
+		CreatedAt: timeAgo(beforeNow),
+	}
+}
+
+func TestMaxReachNotReachedNoStart(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	pinger := NewPinger("NOTIF").SetMaxCount(2)
+
+	if pinger.MaxReached(comments, nil) {
+		t.Error("Should not have reached the maximum")
+	}
+}
+
+func TestMaxReachNotReachedWithStart(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 14*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 12*time.Hour),
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	pinger := NewPinger("NOTIF").SetMaxCount(2)
+
+	if pinger.MaxReached(comments, timeAgo(11*time.Hour)) {
+		t.Error("Should not have reached the maximum")
+	}
+}
+
+func TestMaxReachNoLimit(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 14*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 12*time.Hour),
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	pinger := NewPinger("NOTIF")
+
+	if pinger.MaxReached(comments, nil) {
+		t.Error("Should not have reached the non-existing maximum")
+	}
+}
+
+func TestNotification(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").SetDescription("Description").PingNotification(comments, "who", nil)
+	if notif == nil {
+		t.Error("PingNotification should have created a notif")
+	}
+	expectedNotif := Notification{Name: "NOTIF", Arguments: "who", Context: "Description"}
+	if *notif != expectedNotif {
+		t.Error(*notif, "doesn't match expectedNotif:", expectedNotif)
+	}
+}
+
+func TestNotificationNilTimePeriod(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 14*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 13*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 12*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 11*time.Hour),
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").PingNotification(comments, "who", nil)
+	if notif == nil {
+		t.Error("PingNotification should have created a notif")
+	}
+}
+
+func TestNotificationTimePeriodNotReached(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 5*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 3*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 1*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").SetTimePeriod(2*time.Hour).PingNotification(comments, "who", nil)
+	if notif != nil {
+		t.Error("PingNotification shouldn't have created a notif")
+	}
+}
+
+func TestNotificationTimePeriodReached(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 4*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 3*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 2*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").SetTimePeriod(time.Hour).PingNotification(comments, "who", nil)
+	if notif == nil {
+		t.Error("PingNotification should have created a notif")
+	}
+}

kubernetes/test-infra

mungers/matchers/comment/pinger.go
@@ -1,0 +1,118 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comment
+
+import (
+	"time"
+
+	"github.com/google/go-github/github"
+)
+
+// Pinger checks if it's time to send a ping.
+// You can build a pinger for a specific use-case and re-use it when you want.
+type Pinger struct {
+	keyword     string        // Short description for the ping
+	description string        // Long description for the ping
+	timePeriod  time.Duration // How often should we ping
+	maxCount    int           // Will stop pinging after that many occurences
+}
+
+// NewPinger creates a new pinger. `keyword` is the name of the notification.
+func NewPinger(keyword string) *Pinger {
+	return &Pinger{
+		keyword: keyword,
+	}
+}
+
+// SetDescription is the description that goes along the ping
+func (p *Pinger) SetDescription(description string) *Pinger {
+	p.description = description
+
+	return p
+}
+
+// SetTimePeriod is the time we wait between pings
+func (p *Pinger) SetTimePeriod(timePeriod time.Duration) *Pinger {
+	p.timePeriod = timePeriod
+
+	return p
+}
+
+// SetMaxCount will make the pinger fail when it reaches maximum
+func (p *Pinger) SetMaxCount(maxCount int) *Pinger {
+	p.maxCount = maxCount
+
+	return p
+}
+
+// PingNotification creates a new notification to ping `who`
+func (p *Pinger) PingNotification(comments []*github.IssueComment, who string, startDate *time.Time) *Notification {
+	if startDate == nil {
+		startDate = &time.Time{}
+	}
+
+	pings := FilterComments(
+		comments,
+		And([]Matcher{
+			CreatedAfter(*startDate),
+			MungerNotificationName(p.keyword),
+		}),
+	)
+
+	// We have pinged too many times, it's time to try something else
+	if p.isMaxReached(pings) {
+		return nil
+	}
+
+	if !p.shouldPingNow(pings, startDate) {
+		return nil
+	}
+
+	return &Notification{
+		Name:      p.keyword,
+		Arguments: who,
+		Context:   p.description,
+	}
+}
+
+// IsMaxReached tells you if you've reached the limit yet
+func (p *Pinger) IsMaxReached(comments []*github.IssueComment, startDate *time.Time) bool {
+	if startDate == nil {
+		startDate = &time.Time{}
+	}
+	return p.isMaxReached(FilterComments(
+		comments,
+		And([]Matcher{
+			CreatedAfter(*startDate),
+			MungerNotificationName(p.keyword),
+		}),
+	))
+}
+
+func (p *Pinger) isMaxReached(pings FilteredComments) bool {
+	return p.maxCount != 0 && len(pings) >= p.maxCount
+}
+
+func (p *Pinger) shouldPingNow(pings FilteredComments, startDate *time.Time) bool {
+	lastEvent := startDate
+
+	if len(pings) != 0 {
+		lastEvent = pings[len(pings)-1].CreatedAt
+	}
+
+	return time.Since(*lastEvent) > p.timePeriod
+}

mungers/matchers/comment/pinger_test.go
@@ -1,0 +1,136 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comment
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-github/github"
+)
+
+func timeAgo(d time.Duration) *time.Time {
+	t := time.Now().Add(-d)
+	return &t
+}
+
+func makeComment(body, author string, beforeNow time.Duration) *github.IssueComment {
+	return &github.IssueComment{
+		Body:      &body,
+		User:      &github.User{Login: &author},
+		CreatedAt: timeAgo(beforeNow),
+	}
+}
+
+func TestMaxReachNotReachedNoStart(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	pinger := NewPinger("NOTIF").SetMaxCount(2)
+
+	if pinger.IsMaxReached(comments, nil) {
+		t.Error("Should not have reached the maximum")
+	}
+}
+
+func TestMaxReachNotReachedWithStart(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 14*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 12*time.Hour),
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	pinger := NewPinger("NOTIF").SetMaxCount(2)
+
+	if pinger.IsMaxReached(comments, timeAgo(11*time.Hour)) {
+		t.Error("Should not have reached the maximum")
+	}
+}
+
+func TestMaxReachNoLimit(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 14*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 12*time.Hour),
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	pinger := NewPinger("NOTIF")
+
+	if pinger.IsMaxReached(comments, nil) {
+		t.Error("Should not have reached the non-existing maximum")
+	}
+}
+
+func TestNotification(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").SetDescription("Description").PingNotification(comments, "who", nil)
+	if notif == nil {
+		t.Error("PingNotification should have created a notif")
+	}
+	expectedNotif := Notification{Name: "NOTIF", Arguments: "who", Context: "Description"}
+	if *notif != expectedNotif {
+		t.Error(*notif, "doesn't match expectedNotif:", expectedNotif)
+	}
+}
+
+func TestNotificationNilTimePeriod(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 14*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 13*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 12*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 11*time.Hour),
+		makeComment("[SOMETHING] Something", "k8s-merge-robot", 10*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").PingNotification(comments, "who", nil)
+	if notif == nil {
+		t.Error("PingNotification should have created a notif")
+	}
+}
+
+func TestNotificationTimePeriodNotReached(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 5*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 3*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 1*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").SetTimePeriod(2*time.Hour).PingNotification(comments, "who", nil)
+	if notif != nil {
+		t.Error("PingNotification shouldn't have created a notif")
+	}
+}
+
+func TestNotificationTimePeriodReached(t *testing.T) {
+	comments := []*github.IssueComment{
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 4*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 3*time.Hour),
+		makeComment("[NOTIF] Notification", "k8s-merge-robot", 2*time.Hour),
+	}
+
+	notif := NewPinger("NOTIF").SetTimePeriod(time.Hour).PingNotification(comments, "who", nil)
+	if notif == nil {
+		t.Error("PingNotification should have created a notif")
+	}
+}