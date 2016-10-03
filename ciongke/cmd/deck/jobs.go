b6b4470b008d09d165f087e2100f93a59393df0b
Joe Finney <spxtr@google.com>
2016-10-03 12:17:49 -0700

    Add cmd/deck.

---
+++ ciongke/cmd/deck/jobs.go
@@ -0,0 +1,115 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+
+	"github.com/kubernetes/test-infra/ciongke/kube"
+)
+
+const (
+	period = time.Minute
+)
+
+type Job struct {
+	Repo     string `json:"repo"`
+	Number   int    `json:"number"`
+	Job      string `json:"job"`
+	Started  string `json:"started"`
+	Finished string `json:"finished"`
+	Duration string `json:"duration"`
+
+	st time.Time `json:"-"`
+	ft time.Time `json:"-"`
+}
+
+type JobAgent struct {
+	kc   *kube.Client
+	jobs []Job
+	mut  sync.Mutex
+}
+
+func (ja *JobAgent) Start() {
+	ja.tryUpdate()
+	go func() {
+		t := time.Tick(period)
+		for range t {
+			ja.tryUpdate()
+		}
+	}()
+}
+
+func (ja *JobAgent) Jobs() []Job {
+	ja.mut.Lock()
+	defer ja.mut.Unlock()
+	res := make([]Job, len(ja.jobs))
+	copy(res, ja.jobs)
+	return res
+}
+
+func (ja *JobAgent) tryUpdate() {
+	if err := ja.update(); err != nil {
+		logrus.WithError(err).Warning("Error updating job list.")
+	}
+}
+
+type byStartTime []Job
+
+func (a byStartTime) Len() int           { return len(a) }
+func (a byStartTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byStartTime) Less(i, j int) bool { return a[i].st.After(a[j].st) }
+
+func (ja *JobAgent) update() error {
+	js, err := ja.kc.ListJobs(nil)
+	if err != nil {
+		return err
+	}
+	var njs []Job
+	for _, j := range js {
+		nj := Job{
+			Repo:    fmt.Sprintf("%s/%s", j.Metadata.Labels["owner"], j.Metadata.Labels["repo"]),
+			Job:     j.Metadata.Labels["jenkins-job-name"],
+			Started: j.Status.StartTime.Format(time.Stamp),
+
+			st: j.Status.StartTime,
+			ft: j.Status.CompletionTime,
+		}
+		if !nj.ft.IsZero() {
+			nj.Finished = nj.ft.Format("15:04:05")
+			nj.Duration = nj.ft.Sub(nj.st).String()
+		}
+		if pr, err := strconv.Atoi(j.Metadata.Labels["pr"]); err == nil {
+			nj.Number = pr
+		} else {
+			return err
+		}
+		njs = append(njs, nj)
+	}
+	sort.Sort(byStartTime(njs))
+
+	ja.mut.Lock()
+	defer ja.mut.Unlock()
+	ja.jobs = njs
+	return nil
+}
---
+++ ciongke/cmd/deck/static/script.js
@@ -0,0 +1,75 @@
+var repos = {};
+var jobs = {};
+
+window.onload = function() {
+    for (var i = 0; i < allBuilds.length; i++) {
+        repos[allBuilds[i].repo] = true;
+        jobs[allBuilds[i].job] = true;
+    }
+
+    var rs = Array.from(Object.keys(repos)).sort();
+    for (var i = 0; i < rs.length; i++) {
+        var l = document.createElement("label");
+        var c = document.createElement("input");
+        c.setAttribute("type", "checkbox");
+        c.setAttribute("checked", true);
+        c.onclick = (function(repo) {
+            return function() {
+                repos[repo] = this.checked;
+                redraw();
+            }
+        })(rs[i]);
+        l.appendChild(c);
+        l.appendChild(document.createTextNode(rs[i]));
+        document.getElementById("repos").appendChild(l);
+        document.getElementById("repos").appendChild(document.createElement("br"));
+    }
+
+    var js = Array.from(Object.keys(jobs)).sort();
+    for (var i = 0; i < js.length; i++) {
+        var l = document.createElement("label");
+        var c = document.createElement("input");
+        c.setAttribute("type", "checkbox");
+        c.setAttribute("checked", true);
+        c.onclick = (function(job) {
+            return function() {
+                jobs[job] = this.checked;
+                redraw();
+            }
+        })(js[i]);
+        l.appendChild(c);
+        l.appendChild(document.createTextNode(js[i]));
+        document.getElementById("jobs").appendChild(l);
+        document.getElementById("jobs").appendChild(document.createElement("br"));
+    }
+
+    redraw();
+};
+
+function redraw() {
+    var builds = document.getElementById("builds").getElementsByTagName("tbody")[0];
+    while (builds.firstChild)
+        builds.removeChild(builds.firstChild);
+
+    for (var i = 0; i < allBuilds.length; i++) {
+        if (!repos[allBuilds[i].repo])
+            continue;
+        if (!jobs[allBuilds[i].job])
+            continue;
+
+        var r = document.createElement("tr");
+        r.appendChild(createCell(allBuilds[i].repo));
+        r.appendChild(createCell(allBuilds[i].number));
+        r.appendChild(createCell(allBuilds[i].job));
+        r.appendChild(createCell(allBuilds[i].started));
+        r.appendChild(createCell(allBuilds[i].finished));
+        r.appendChild(createCell(allBuilds[i].duration));
+        builds.appendChild(r);
+    }
+}
+
+function createCell(text) {
+    var c = document.createElement("td");
+    c.appendChild(document.createTextNode(text));
+    return c;
+}
