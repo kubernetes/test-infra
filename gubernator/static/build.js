98c9a4fc687ae6554c6196ba1d423654da9a5b6a
Ryan Hitchman <rmmh@google.com>
2016-10-04 19:12:30 -0700

    Convert ANSI escape codes in logs into color spans. (Fixes #660)

--- gubernator/static/build.js
+++ gubernator/static/build.js
@@ -85,6 +85,7 @@ function expand_skipped(els) {
 			}
 		}
 		parent.normalize();  // merge adjacent text nodes
+		fix_escape_codes();  // colorize new segments
 	});
 }
 
@@ -99,13 +100,65 @@ function expand_all(btn) {
 	btn.remove();
 }
 
+/* given a string containing ansi formatting directives, return a new one
+   with designated regions of text marked with the appropriate color directives,
+   and with all unknown directives stripped */
+function ansi_to_html(orig) {
+	// Given a cmd (like "32" or "0;97"), some enclosed body text, and the original string,
+	// either return the body wrapped in an element to achieve the desired result, or the
+	// original string if nothing works.
+	function annotate(cmd, body, orig) {
+		var code = +(cmd.replace('0;', ''));
+		if (code === 0) // reset
+			return body;
+		else if (code === 1) // bold
+			return '<em>' + body + '</em>';
+		else if (30 <= code && code <= 37) // foreground color
+			return '<span class="ansi-' + (code - 30) + '">' + body + '</span>'
+		else if (90 <= code && code <= 97) // foreground color, bright
+			return '<span class="ansi-' + (code - 90 + 8) + '">' + body + '</span>'
+		return orig;  // fallback: don't change anything
+	}
+	// Find commands, optionally followed by a bold command, with some content, then a reset command.
+	// Unpaired commands are *not* handled here, but they're very uncommon.
+	var filtered = orig.replace(/\033\[([0-9;]*)\w(\033\[1m)?([^\033]*?)\033\[0m/g, function(match, code, bold, body, offset, string) {
+		if (bold !== undefined)  // normal code + bold
+			return '<em>' + annotate(code, body, string) + '</em>';
+		return annotate(code, body, string);
+	})
+	// Strip out anything left over.
+	return filtered.replace(/\033\[([0-9;]*\w)/g, function(match, cmd, offset, string) {
+		console.log('unhandled ansi code: ', cmd, "context:", JSON.stringify(filtered.slice(offset-50,offset+50)));
+		return '';
+	});
+}
+
+function fix_escape_codes() {
+	var logs = document.querySelectorAll('pre[data-src]');
+	for (var i = 0; i < logs.length; i++) {
+		var orig = logs[i].innerHTML;
+		var newer = ansi_to_html(orig);
+		if (orig !== newer) {
+			logs[i].innerHTML = newer;
+		}
+	}
+}
+
 function init() {
 	fix_timestamps();
+	fix_escape_codes();
 	document.body.onclick = function(evt) {
 		var target = evt.target;
-		if (target.nodeName == 'SPAN' && target.className == 'skip') {
+		if (target.nodeName === 'SPAN' && target.className === 'skip') {
 			expand_skipped([target]);
 			evt.preventDefault();
 		}
+	}
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+	// enable node.js `require('./build')` to work for testing
+	module.exports = {
+		ansi_to_html: ansi_to_html
 	}
 }
