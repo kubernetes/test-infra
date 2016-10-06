faf19fcd6d33e4777cbcc40a56bf7505c2034ccb
Antoine Pelisse <apelisse@google.com>
2016-10-06 13:07:19 -0700

    mungegithub: Only fetch issue changed since last loop
    Use the "since" parameter from Github List Issue interface, so that we
    don't munge issues that haven't changed since the last loop.

--- mungegithub/github/github.go
+++ mungegithub/github/github.go
@@ -184,6 +184,9 @@ type Config struct {
 	// When we clear analytics we store the last values here
 	lastAnalytics analytics
 	analytics     analytics
+
+	// Last fetch
+	since time.Time
 }
 
 type analytic struct {
@@ -2079,11 +2082,39 @@ func (obj *MungeObject) MergedAt() (*time.Time, bool) {
 	return pr.MergedAt, true
 }
 
+func (config *Config) runMungeFunction(obj *MungeObject, fn MungeFunction) error {
+	if obj.Issue.Number == nil {
+		glog.Infof("Skipping issue with no number, very strange")
+		return nil
+	}
+	if obj.Issue.User == nil || obj.Issue.User.Login == nil {
+		glog.V(2).Infof("Skipping PR %d with no user info %#v.", *obj.Issue.Number, obj.Issue.User)
+		return nil
+	}
+	if *obj.Issue.Number < config.MinPRNumber {
+		glog.V(6).Infof("Dropping %d < %d", *obj.Issue.Number, config.MinPRNumber)
+		return nil
+	}
+	if *obj.Issue.Number > config.MaxPRNumber {
+		glog.V(6).Infof("Dropping %d > %d", *obj.Issue.Number, config.MaxPRNumber)
+		return nil
+	}
+
+	glog.V(2).Infof("----==== %d ====----", *obj.Issue.Number)
+	glog.V(8).Infof("Issue %d labels: %v isPR: %v", *obj.Issue.Number, obj.Issue.Labels, obj.Issue.PullRequestLinks != nil)
+	if err := fn(obj); err != nil {
+		return err
+	}
+	return nil
+}
+
 // ForEachIssueDo will run for each Issue in the project that matches:
 //   * pr.Number >= minPRNumber
 //   * pr.Number <= maxPRNumber
 func (config *Config) ForEachIssueDo(fn MungeFunction) error {
 	page := 1
+	since := time.Time{}
+
 	for {
 		glog.V(4).Infof("Fetching page %d of issues", page)
 		listOpts := &github.IssueListByRepoOptions{
@@ -2092,6 +2123,7 @@ func (config *Config) ForEachIssueDo(fn MungeFunction) error {
 			Labels:      config.Labels,
 			Direction:   "asc",
 			ListOptions: github.ListOptions{PerPage: 100, Page: page},
+			Since:       config.since,
 		}
 		issues, response, err := config.client.Issues.ListByRepo(config.Org, config.Project, listOpts)
 		config.analytics.ListIssues.Call(config, response)
@@ -2099,32 +2131,17 @@ func (config *Config) ForEachIssueDo(fn MungeFunction) error {
 			return err
 		}
 		for i := range issues {
-			issue := issues[i]
-			if issue.Number == nil {
-				glog.Infof("Skipping issue with no number, very strange")
-				continue
-			}
-			if issue.User == nil || issue.User.Login == nil {
-				glog.V(2).Infof("Skipping PR %d with no user info %#v.", *issue.Number, issue.User)
-				continue
-			}
-			if *issue.Number < config.MinPRNumber {
-				glog.V(6).Infof("Dropping %d < %d", *issue.Number, config.MinPRNumber)
-				continue
-			}
-			if *issue.Number > config.MaxPRNumber {
-				glog.V(6).Infof("Dropping %d > %d", *issue.Number, config.MaxPRNumber)
-				continue
-			}
-			glog.V(2).Infof("----==== %d ====----", *issue.Number)
-			glog.V(8).Infof("Issue %d labels: %v isPR: %v", *issue.Number, issue.Labels, issue.PullRequestLinks != nil)
-			obj := MungeObject{
+			obj := &MungeObject{
 				config:      config,
-				Issue:       issue,
+				Issue:       issues[i],
 				Annotations: map[string]string{},
 			}
-			if err := fn(&obj); err != nil {
-				continue
+			err := config.runMungeFunction(obj, fn)
+			if err != nil {
+				return err
+			}
+			if obj.Issue.UpdatedAt != nil && obj.Issue.UpdatedAt.After(since) {
+				since = *obj.Issue.UpdatedAt
 			}
 		}
 		if response.LastPage == 0 || response.LastPage <= page {
@@ -2132,6 +2149,7 @@ func (config *Config) ForEachIssueDo(fn MungeFunction) error {
 		}
 		page++
 	}
+	config.since = since
 	return nil
 }
 
