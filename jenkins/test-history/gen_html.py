b5993ed891d4ba899678addaa94bec2a56ac4435
Ryan Hitchman <rmmh@google.com>
2016-11-21 09:22:43 -0800

    Update blocking jobs lists to use ci- prefixes.

--- jenkins/test-history/gen_html.py
+++ jenkins/test-history/gen_html.py
@@ -33,16 +33,17 @@ import yaml
 
 
 BLOCKING_JOBS = [
-    'kubelet-gce-e2e-ci',
+    'ci-kubernetes-e2e-gce-etcd3',
+    'ci-kubernetes-e2e-gci-gce',
+    'ci-kubernetes-e2e-gci-gce-slow',
+    'ci-kubernetes-e2e-gci-gke',
+    'ci-kubernetes-e2e-gci-gke-slow',
+    'ci-kubernetes-kubemark-500-gce',
+    'ci-kubernetes-node-kubelet',
+    'ci-kubernetes-test-go',
+    'ci-kubernetes-verify-master',
     'kubernetes-build',
-    'kubernetes-e2e-gce',
-    'kubernetes-e2e-gce-slow',
-    'kubernetes-e2e-gke',
-    'kubernetes-e2e-gke-slow',
-    'kubernetes-kubemark-5-gce',
-    'kubernetes-kubemark-500-gce',
-    'kubernetes-test-go',
-    'kubernetes-verify-master',
+    'kubernetes-e2e-kops-aws',
 ]
 
 JobSummary = collections.namedtuple('JobSummary', [
