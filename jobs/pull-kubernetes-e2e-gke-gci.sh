d1dbd9b52e6ea4dbf50253ea915d681a8b961f7b
Jeff Grafton <jgrafton@google.com>
2016-12-19 13:18:19 -0800

    Remove references to build-tools/

--- jobs/pull-kubernetes-e2e-gke-gci.sh
+++ jobs/pull-kubernetes-e2e-gke-gci.sh
@@ -33,14 +33,9 @@ export JENKINS_USE_LOCAL_BINARIES=y
 export KUBE_FASTBUILD=true
 ./hack/jenkins/build.sh
 
-# TODO(spxtr): once https://github.com/kubernetes/kubernetes/pull/35453 is in,
-# remove the first branch here.
-if [[ -e build/util.sh ]]; then
-  version=$(source build/util.sh && echo $(kube::release::semantic_version))
-elif [[ -e build-tools/util.sh ]]; then
-  version=$(source build-tools/util.sh && echo $(kube::release::semantic_version))
-else
-  echo "Could not find build/util.sh or build-tools/util.sh." >&2
+readonly version=$(source build/util.sh && echo $(kube::release::semantic_version))
+if [[ -z "${version:-}" ]]; then
+  echo "Could not find build/util.sh, or kube::release::semantic_version failed." >&2
   exit 1
 fi
 gsutil -m rsync -r "gs://kubernetes-release-pull/ci/${JOB_NAME}/${version}" "gs://kubernetes-release-dev/ci/${version}-pull-gke-gci"
