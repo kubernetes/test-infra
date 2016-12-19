d0fb980234e905ad71597b1f559fda485c96202d
Joe Finney <spxtr@google.com>
2016-12-19 11:19:27 -0800

    Add verify/update for gazel.

---
+++ verify/verify-bazel.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Copyright 2016 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# TODO(spxtr): Pin to gazel version once mikedanese/gazel#14 is in.
+go get -u github.com/mikedanese/gazel/gazel
+if [[ $(gazel --root "${PWD}" --dry-run --print-diff 2>&1 | tee /dev/stderr | wc -l | tr -d '[:space:]') != 0 ]]; then
+  echo
+  echo "BUILD files are not up to date"
+  echo "Run ./verify/update-bazel.sh"
+  exit 1
+fi
