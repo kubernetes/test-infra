3362f69751bcd36fde143bdfef6d5d1c8acca364
Jeff Grafton <jgrafton@google.com>
2016-12-29 11:40:57 -0800

    Remove extra slash from SQ GitHub commit status URL

--- mungers/submit-queue.go
+++ mungers/submit-queue.go
@@ -755,7 +755,7 @@ func (sq *SubmitQueue) SetMergeStatus(obj *github.MungeObject, reason string) {
 	status, ok := obj.GetStatus(sqContext)
 	if !ok || status == nil || *status.Description != reason {
 		state := reasonToState(reason)
-		url := fmt.Sprintf("http://submit-queue.k8s.io/#/prs/?prDisplay=%d&historyDisplay=%d", *obj.Issue.Number, *obj.Issue.Number)
+		url := fmt.Sprintf("http://submit-queue.k8s.io/#/prs?prDisplay=%d&historyDisplay=%d", *obj.Issue.Number, *obj.Issue.Number)
 		_ = obj.SetStatus(state, url, reason, sqContext)
 	}
 
