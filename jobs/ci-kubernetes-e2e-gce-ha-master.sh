7001986110e2355309e327633dca94f8b7f079c7
Jerzy Szczepkowski <jsz@google.com>
2017-01-03 09:37:54 +0100

    Extended timeouts for HA-master tests. (#1467)
    Extended timeouts for HA-master tests.

--- jobs/ci-kubernetes-e2e-gce-ha-master.sh
+++ jobs/ci-kubernetes-e2e-gce-ha-master.sh
@@ -67,6 +67,6 @@ export PATH="${PATH}:/usr/local/go/bin"
 
 ### Runner
 readonly runner="${testinfra}/jenkins/dockerized-e2e-runner.sh"
-export DOCKER_TIMEOUT="170m"
-export KUBEKINS_TIMEOUT="150m"
+export DOCKER_TIMEOUT="240"
+export KUBEKINS_TIMEOUT="220"
 "${runner}"
