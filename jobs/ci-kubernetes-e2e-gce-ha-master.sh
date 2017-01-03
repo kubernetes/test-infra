b9771d9eccb6bb74632e33d82b23eac73632e596
Jerzy Szczepkowski <jsz@google.com>
2017-01-03 16:57:35 +0100

    Added missing 'm' to ha e2e test timeout.
    Added missing 'm' to ha e2e test timeout.

--- jobs/ci-kubernetes-e2e-gce-ha-master.sh
+++ jobs/ci-kubernetes-e2e-gce-ha-master.sh
@@ -67,6 +67,6 @@ export PATH="${PATH}:/usr/local/go/bin"
 
 ### Runner
 readonly runner="${testinfra}/jenkins/dockerized-e2e-runner.sh"
-export DOCKER_TIMEOUT="240"
-export KUBEKINS_TIMEOUT="220"
+export DOCKER_TIMEOUT="240m"
+export KUBEKINS_TIMEOUT="220m"
 "${runner}"
