59c00ad66e0f2c4c4e59a027e16c4c9d0a1398f5
Joe Finney <spxtr@google.com>
2017-01-06 10:15:44 -0800

    Add post-commit kubernetes job.

---
+++ jobs/ci-kubernetes-bazel.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright 2016 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Cache location.
+export TEST_TMPDIR="/root/.cache/bazel"
+
+make bazel-build && rc=$? || rc=$?
+
+# Clear test.xml so that we don't pick up old results.
+find -L bazel-testlogs -name 'test.xml' -type f -exec rm '{}' +
+
+if [[ "${rc}" == 0 ]]; then
+  make bazel-test && rc=$? || rc=$?
+fi
+
+if [[ "${rc}" == 0 ]]; then
+  bazel run //:ci-artifacts -- "gs://kubernetes-release-dev/bazel/$(git rev-parse HEAD)" && rc=$? || rc=$?
+fi
+
+# Coalesce test results into one file for upload.
+"$(dirname "${0}")/../images/pull-kubernetes-bazel/coalesce.py"
+
+exit "${rc}"
