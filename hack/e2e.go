kubernetes/kubernetes

Add kubernetes-anywhere as a new e2e deployment option.
The configuration in getConfig() comes mostly from the defaults in
kubernetes-anywhere.

hack/e2e.go
@@ -18,6 +18,9 @@ limitations under the License.
 package main
 
 import (
+	"github.com/renstrom/dedent"
+
+	"bytes"
 	"encoding/xml"
 	"flag"
 	"fmt"
@@ -30,6 +33,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"text/template"
 	"time"
 )
 
@@ -41,7 +45,7 @@ var (
 		"You can explicitly set to false if you're, e.g., testing client changes "+
 		"for which the server version doesn't make a difference.")
 	checkLeakedResources = flag.Bool("check_leaked_resources", false, "Ensure project ends with the same resources")
-	deployment           = flag.String("deployment", "bash", "up/down mechanism (defaults to cluster/kube-{up,down}.sh) (choices: bash/kops)")
+	deployment           = flag.String("deployment", "bash", "up/down mechanism (defaults to cluster/kube-{up,down}.sh) (choices: bash/kops/kubernetes-anywhere)")
 	down                 = flag.Bool("down", false, "If true, tear down the cluster before exiting.")
 	dump                 = flag.String("dump", "", "If set, dump cluster logs to this location on test or cluster-up failure")
 	kubemark             = flag.Bool("kubemark", false, "If true, run kubemark tests.")
@@ -61,6 +65,12 @@ var (
 	kopsZones       = flag.String("kops-zones", "us-west-2a", "(kops AWS only) AWS zones for kops deployment, comma delimited.")
 	kopsNodes       = flag.Int("kops-nodes", 2, "(kops only) Number of nodes to create.")
 	kopsUpTimeout   = flag.Duration("kops-up-timeout", 20*time.Minute, "(kops only) Time limit between 'kops config / kops update' and a response from the Kubernetes API.")
+
+	// kubernetes-anywhere specific flags.
+	kubernetesAnywherePath           = flag.String("kubernetes-anywhere-path", "", "(kubernetes-anywhere only) Path to the kubernetes-anywhere directory. Must be set for kubernetes-anywhere.")
+	kubernetesAnywherePhase2Provider = flag.String("kubernetes-anywhere-phase2-provider", "ignition", "(kubernetes-anywhere only) Provider for phase2 bootstrapping. (Defaults to ignition).")
+	kubernetesAnywhereCluster        = flag.String("kubernetes-anywhere-cluster", "", "(kubernetes-anywhere only) Cluster name. Must be set for kubernetes-anywhere.")
+	kubernetesAnywhereUpTimeout      = flag.Duration("kubernetes-anywhere-up-timeout", 20*time.Minute, "(kubernetes-anywhere only) Time limit between starting a cluster and making a successful call to the Kubernetes API.")
 
 	// Deprecated flags.
 	deprecatedPush   = flag.Bool("push", false, "Deprecated. Does nothing.")
@@ -409,6 +419,8 @@ func getDeployer() (deployer, error) {
 		return bash{}, nil
 	case "kops":
 		return NewKops()
+	case "kubernetes-anywhere":
+		return NewKubernetesAnywhere()
 	default:
 		return nil, fmt.Errorf("Unknown deployment strategy %q", *deployment)
 	}
@@ -525,30 +537,11 @@ func (k kops) Up() error {
 	// TODO(zmerlynn): More cluster validation. This should perhaps be
 	// added to kops and not here, but this is a fine place to loop
 	// for now.
-	for stop := time.Now().Add(*kopsUpTimeout); time.Now().Before(stop); time.Sleep(30 * time.Second) {
-		n, err := clusterSize(k)
-		if err != nil {
-			log.Printf("Can't get cluster size, sleeping: %v", err)
-			continue
-		}
-		if n < k.nodes+1 {
-			log.Printf("%d (current nodes) < %d (requested instances), sleeping", n, k.nodes+1)
-			continue
-		}
-		return nil
-	}
-	return fmt.Errorf("kops bringup timed out")
+	return waitForNodes(k, k.nodes+1, *kopsUpTimeout)
 }
 
 func (k kops) IsUp() error {
-	n, err := clusterSize(k)
-	if err != nil {
-		return err
-	}
-	if n <= 0 {
-		return fmt.Errorf("kops cluster found, but %d nodes reported", n)
-	}
-	return nil
+	return isUp(k)
 }
 
 func (k kops) SetupKubecfg() error {
@@ -578,6 +571,131 @@ func (k kops) Down() error {
 	return finishRunning("kops delete", exec.Command(k.path, "delete", "cluster", k.cluster, "--yes"))
 }
 
+type KubernetesAnywhere struct {
+	Path           string
+	Phase2Provider string
+	Project        string
+	Cluster        string
+}
+
+func NewKubernetesAnywhere() (*KubernetesAnywhere, error) {
+	if *kubernetesAnywherePath == "" {
+		return nil, fmt.Errorf("--kubernetes-anywhere-path is required")
+	}
+
+	if *kubernetesAnywhereCluster == "" {
+		return nil, fmt.Errorf("--kubernetes-anywhere-cluster is required")
+	}
+
+	// Set KUBERNETES_CONFORMANCE_TEST so the auth info is picked up
+	// from kubectl instead of bash inference.
+	if err := os.Setenv("KUBERNETES_CONFORMANCE_TEST", "yes"); err != nil {
+		return nil, err
+	}
+
+	k := &KubernetesAnywhere{
+		Path:           *kubernetesAnywherePath,
+		Phase2Provider: *kubernetesAnywherePhase2Provider,
+		Project:        os.Getenv("PROJECT"),
+		Cluster:        *kubernetesAnywhereCluster,
+	}
+
+	if err := k.writeConfig(); err != nil {
+		return nil, err
+	}
+	return k, nil
+}
+
+func (k KubernetesAnywhere) getConfig() (string, error) {
+	// As needed, plumb through more CLI options to replace these defaults
+	tmpl, err := template.New("kubernetes-anywhere-config").Parse(dedent.Dedent(`
+		.phase1.num_nodes=2
+		.phase1.cluster_name="{{.Cluster}}"
+		.phase1.cloud_provider="gce"
+
+		.phase1.gce.os_image="ubuntu-1604-xenial-v20160420c"
+		.phase1.gce.instance_type="n1-standard-2"
+		.phase1.gce.project="{{.Project}}"
+		.phase1.gce.region="us-central1"
+		.phase1.gce.zone="us-central1-b"
+		.phase1.gce.network="default"
+
+		.phase2.installer_container="docker.io/colemickens/k8s-ignition:latest"
+		.phase2.docker_registry="gcr.io/google-containers"
+		.phase2.kubernetes_version="v1.4.1"
+		.phase2.provider="{{.Phase2Provider}}"
+
+		.phase3.run_addons=y
+		.phase3.kube_proxy=y
+		.phase3.dashboard=y
+		.phase3.heapster=y
+		.phase3.kube_dns=y
+	`))
+
+	if err != nil {
+		return "", fmt.Errorf("Error creating template for KubernetesAnywhere config: %v", err)
+	}
+
+	var buf bytes.Buffer
+	if err = tmpl.Execute(&buf, k); err != nil {
+		return "", fmt.Errorf("Error executing template for KubernetesAnywhere config: %v", err)
+	}
+
+	return string(buf.Bytes()), nil
+}
+
+func (k KubernetesAnywhere) writeConfig() error {
+	config, err := k.getConfig()
+	if err != nil {
+		return fmt.Errorf("Could not generate config: %v", err)
+	}
+
+	f, err := os.Create(k.Path + "/.config")
+	if err != nil {
+		return fmt.Errorf("Could not create file: %v", err)
+	}
+	defer f.Close()
+
+	fmt.Fprint(f, config)
+	return nil
+}
+
+func (k KubernetesAnywhere) Up() error {
+	cmd := exec.Command("make", "-C", k.Path, "WAIT_FOR_KUBECONFIG=y", "deploy-cluster")
+	if err := finishRunning("deploy-cluster", cmd); err != nil {
+		return err
+	}
+
+	nodes := 2 // For now, this is hardcoded in the config
+	return waitForNodes(k, nodes, *kubernetesAnywhereUpTimeout)
+}
+
+func (k KubernetesAnywhere) IsUp() error {
+	return isUp(k)
+}
+
+func (k KubernetesAnywhere) SetupKubecfg() error {
+	output, err := exec.Command("make", "--silent", "-C", k.Path, "kubeconfig-path").Output()
+	if err != nil {
+		return fmt.Errorf("Could not get kubeconfig-path: %v", err)
+	}
+	kubecfg := strings.TrimSuffix(string(output), "\n")
+
+	if err = os.Setenv("KUBECONFIG", kubecfg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (k KubernetesAnywhere) Down() error {
+	err := finishRunning("get kubeconfig-path", exec.Command("make", "-C", k.Path, "kubeconfig-path"))
+	if err != nil {
+		// This is expected if the cluster doesn't exist.
+		return nil
+	}
+	return finishRunning("destroy-cluster", exec.Command("make", "-C", k.Path, "FORCE_DESTROY=y", "destroy-cluster"))
+}
+
 func clusterSize(deploy deployer) (int, error) {
 	if err := deploy.SetupKubecfg(); err != nil {
 		return -1, err
@@ -590,6 +708,33 @@ func clusterSize(deploy deployer) (int, error) {
 	stdout := strings.TrimSpace(string(o))
 	log.Printf("Cluster nodes:\n%s", stdout)
 	return len(strings.Split(stdout, "\n")), nil
+}
+
+func isUp(d deployer) error {
+	n, err := clusterSize(d)
+	if err != nil {
+		return err
+	}
+	if n <= 0 {
+		return fmt.Errorf("cluster found, but %d nodes reported", n)
+	}
+	return nil
+}
+
+func waitForNodes(d deployer, nodes int, timeout time.Duration) error {
+	for stop := time.Now().Add(timeout); time.Now().Before(stop); time.Sleep(30 * time.Second) {
+		n, err := clusterSize(d)
+		if err != nil {
+			log.Printf("Can't get cluster size, sleeping: %v", err)
+			continue
+		}
+		if n < nodes {
+			log.Printf("%d (current nodes) < %d (requested instances), sleeping", n, nodes)
+			continue
+		}
+		return nil
+	}
+	return fmt.Errorf("waiting for nodes timed out")
 }
 
 func DumpClusterLogs(location string) error {

kubernetes/test-infra

hack/e2e.go
@@ -18,6 +18,7 @@ limitations under the License.
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 	"flag"
 	"fmt"
@@ -30,6 +31,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"text/template"
 	"time"
 )
 
@@ -41,7 +43,7 @@ var (
 		"You can explicitly set to false if you're, e.g., testing client changes "+
 		"for which the server version doesn't make a difference.")
 	checkLeakedResources = flag.Bool("check_leaked_resources", false, "Ensure project ends with the same resources")
-	deployment           = flag.String("deployment", "bash", "up/down mechanism (defaults to cluster/kube-{up,down}.sh) (choices: bash/kops)")
+	deployment           = flag.String("deployment", "bash", "up/down mechanism (defaults to cluster/kube-{up,down}.sh) (choices: bash/kops/kubernetes-anywhere)")
 	down                 = flag.Bool("down", false, "If true, tear down the cluster before exiting.")
 	dump                 = flag.String("dump", "", "If set, dump cluster logs to this location on test or cluster-up failure")
 	kubemark             = flag.Bool("kubemark", false, "If true, run kubemark tests.")
@@ -62,11 +64,41 @@ var (
 	kopsNodes       = flag.Int("kops-nodes", 2, "(kops only) Number of nodes to create.")
 	kopsUpTimeout   = flag.Duration("kops-up-timeout", 20*time.Minute, "(kops only) Time limit between 'kops config / kops update' and a response from the Kubernetes API.")
 
+	// kubernetes-anywhere specific flags.
+	kubernetesAnywherePath           = flag.String("kubernetes-anywhere-path", "", "(kubernetes-anywhere only) Path to the kubernetes-anywhere directory. Must be set for kubernetes-anywhere.")
+	kubernetesAnywherePhase2Provider = flag.String("kubernetes-anywhere-phase2-provider", "ignition", "(kubernetes-anywhere only) Provider for phase2 bootstrapping. (Defaults to ignition).")
+	kubernetesAnywhereCluster        = flag.String("kubernetes-anywhere-cluster", "", "(kubernetes-anywhere only) Cluster name. Must be set for kubernetes-anywhere.")
+	kubernetesAnywhereUpTimeout      = flag.Duration("kubernetes-anywhere-up-timeout", 20*time.Minute, "(kubernetes-anywhere only) Time limit between starting a cluster and making a successful call to the Kubernetes API.")
+
 	// Deprecated flags.
 	deprecatedPush   = flag.Bool("push", false, "Deprecated. Does nothing.")
 	deprecatedPushup = flag.Bool("pushup", false, "Deprecated. Does nothing.")
 	deprecatedCtlCmd = flag.String("ctl", "", "Deprecated. Does nothing.")
 )
+
+const kubernetesAnywhereConfigTemplate = `
+.phase1.num_nodes=4
+.phase1.cluster_name="{{.Cluster}}"
+.phase1.cloud_provider="gce"
+
+.phase1.gce.os_image="ubuntu-1604-xenial-v20160420c"
+.phase1.gce.instance_type="n1-standard-2"
+.phase1.gce.project="{{.Project}}"
+.phase1.gce.region="us-central1"
+.phase1.gce.zone="us-central1-b"
+.phase1.gce.network="default"
+
+.phase2.installer_container="docker.io/colemickens/k8s-ignition:latest"
+.phase2.docker_registry="gcr.io/google-containers"
+.phase2.kubernetes_version="v1.4.1"
+.phase2.provider="{{.Phase2Provider}}"
+
+.phase3.run_addons=y
+.phase3.kube_proxy=y
+.phase3.dashboard=y
+.phase3.heapster=y
+.phase3.kube_dns=y
+`
 
 func appendError(errs []error, err error) []error {
 	if err != nil {
@@ -426,6 +458,8 @@ func getDeployer() (deployer, error) {
 		return bash{}, nil
 	case "kops":
 		return NewKops()
+	case "kubernetes-anywhere":
+		return NewKubernetesAnywhere()
 	default:
 		return nil, fmt.Errorf("Unknown deployment strategy %q", *deployment)
 	}
@@ -542,30 +576,11 @@ func (k kops) Up() error {
 	// TODO(zmerlynn): More cluster validation. This should perhaps be
 	// added to kops and not here, but this is a fine place to loop
 	// for now.
-	for stop := time.Now().Add(*kopsUpTimeout); time.Now().Before(stop); time.Sleep(30 * time.Second) {
-		n, err := clusterSize(k)
-		if err != nil {
-			log.Printf("Can't get cluster size, sleeping: %v", err)
-			continue
-		}
-		if n < k.nodes+1 {
-			log.Printf("%d (current nodes) < %d (requested instances), sleeping", n, k.nodes+1)
-			continue
-		}
-		return nil
-	}
-	return fmt.Errorf("kops bringup timed out")
+	return waitForNodes(k, k.nodes+1, *kopsUpTimeout)
 }
 
 func (k kops) IsUp() error {
-	n, err := clusterSize(k)
-	if err != nil {
-		return err
-	}
-	if n <= 0 {
-		return fmt.Errorf("kops cluster found, but %d nodes reported", n)
-	}
-	return nil
+	return isUp(k)
 }
 
 func (k kops) SetupKubecfg() error {
@@ -593,6 +608,115 @@ func (k kops) Down() error {
 		return nil
 	}
 	return finishRunning("kops delete", exec.Command(k.path, "delete", "cluster", k.cluster, "--yes"))
+}
+
+type kubernetesAnywhere struct {
+	path string
+	// These are exported only because their use in the config template requires it.
+	Phase2Provider string
+	Project        string
+	Cluster        string
+}
+
+func NewKubernetesAnywhere() (*kubernetesAnywhere, error) {
+	if *kubernetesAnywherePath == "" {
+		return nil, fmt.Errorf("--kubernetes-anywhere-path is required")
+	}
+
+	if *kubernetesAnywhereCluster == "" {
+		return nil, fmt.Errorf("--kubernetes-anywhere-cluster is required")
+	}
+
+	project, ok := os.LookupEnv("PROJECT")
+	if !ok {
+		return nil, fmt.Errorf("The PROJECT environment variable is required to be set for kubernetes-anywhere")
+	}
+
+	// Set KUBERNETES_CONFORMANCE_TEST so the auth info is picked up
+	// from kubectl instead of bash inference.
+	if err := os.Setenv("KUBERNETES_CONFORMANCE_TEST", "yes"); err != nil {
+		return nil, err
+	}
+
+	k := &kubernetesAnywhere{
+		path:           *kubernetesAnywherePath,
+		Phase2Provider: *kubernetesAnywherePhase2Provider,
+		Project:        project,
+		Cluster:        *kubernetesAnywhereCluster,
+	}
+
+	if err := k.writeConfig(); err != nil {
+		return nil, err
+	}
+	return k, nil
+}
+
+func (k kubernetesAnywhere) getConfig() (string, error) {
+	// As needed, plumb through more CLI options to replace these defaults
+	tmpl, err := template.New("kubernetes-anywhere-config").Parse(kubernetesAnywhereConfigTemplate)
+
+	if err != nil {
+		return "", fmt.Errorf("Error creating template for KubernetesAnywhere config: %v", err)
+	}
+
+	var buf bytes.Buffer
+	if err = tmpl.Execute(&buf, k); err != nil {
+		return "", fmt.Errorf("Error executing template for KubernetesAnywhere config: %v", err)
+	}
+
+	return buf.String(), nil
+}
+
+func (k kubernetesAnywhere) writeConfig() error {
+	config, err := k.getConfig()
+	if err != nil {
+		return fmt.Errorf("Could not generate config: %v", err)
+	}
+
+	f, err := os.Create(k.path + "/.config")
+	if err != nil {
+		return fmt.Errorf("Could not create file: %v", err)
+	}
+	defer f.Close()
+
+	fmt.Fprint(f, config)
+	return nil
+}
+
+func (k kubernetesAnywhere) Up() error {
+	cmd := exec.Command("make", "-C", k.path, "WAIT_FOR_KUBECONFIG=y", "deploy-cluster")
+	if err := finishRunning("deploy-cluster", cmd); err != nil {
+		return err
+	}
+
+	nodes := 4 // For now, this is hardcoded in the config
+	return waitForNodes(k, nodes+1, *kubernetesAnywhereUpTimeout)
+}
+
+func (k kubernetesAnywhere) IsUp() error {
+	return isUp(k)
+}
+
+func (k kubernetesAnywhere) SetupKubecfg() error {
+	output, err := exec.Command("make", "--silent", "-C", k.path, "kubeconfig-path").Output()
+	if err != nil {
+		return fmt.Errorf("Could not get kubeconfig-path: %v", err)
+	}
+	kubecfg := strings.TrimSuffix(string(output), "\n")
+
+	if err = os.Setenv("KUBECONFIG", kubecfg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (k kubernetesAnywhere) Down() error {
+	err := finishRunning("get kubeconfig-path", exec.Command("make", "-C", k.path, "kubeconfig-path"))
+	if err != nil {
+		// This is expected if the cluster doesn't exist.
+		return nil
+	}
+	return finishRunning("destroy-cluster", exec.Command("make", "-C", k.path, "FORCE_DESTROY=y", "destroy-cluster"))
 }
 
 func clusterSize(deploy deployer) (int, error) {
@@ -636,6 +760,33 @@ func (e *CommandError) Error() string {
 		stderr = string(stderr)
 	}
 	return fmt.Sprintf("%q: %q", exitErr.Error(), stderr)
+}
+
+func isUp(d deployer) error {
+	n, err := clusterSize(d)
+	if err != nil {
+		return err
+	}
+	if n <= 0 {
+		return fmt.Errorf("cluster found, but %d nodes reported", n)
+	}
+	return nil
+}
+
+func waitForNodes(d deployer, nodes int, timeout time.Duration) error {
+	for stop := time.Now().Add(timeout); time.Now().Before(stop); time.Sleep(30 * time.Second) {
+		n, err := clusterSize(d)
+		if err != nil {
+			log.Printf("Can't get cluster size, sleeping: %v", err)
+			continue
+		}
+		if n < nodes {
+			log.Printf("%d (current nodes) < %d (requested instances), sleeping", n, nodes)
+			continue
+		}
+		return nil
+	}
+	return fmt.Errorf("waiting for nodes timed out")
 }
 
 func DumpClusterLogs(location string) error {