285706f93a23efa3bf689de46f2196ebec08473b
Chao Xu <xuchao@google.com>
2017-02-02 00:07:31 -0800

    let the publishing robot publish k8s.io/apimachinery first, then publish k8s.io/client-go. In the meantime, the publishing robot lets client-go vendor the just published k8s.io/apimachinery.
    add the ability to create an issue if publish fails
    
    don't delete .travis.yml
    
    go build && go test before publish client-go

--- mungegithub/mungers/publish_scripts/clientgo_publish.sh
+++ mungegithub/mungers/publish_scripts/clientgo_publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2016 The Kubernetes Authors.
+# Copyright 2017 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,8 +18,8 @@ set -o nounset
 set -o pipefail
 
 echo $@
-if [ ! $# -eq 5 ]; then
-    echo "usage: publish.sh destination_dir destination_branch token netrc_dir commit_message. destination_dir and netrc_dir are expected to be absolute paths."
+if [ ! $# -eq 6 ]; then
+    echo "usage: publish.sh destination_dir destination_branch token netrc_dir commit_message gopath. destination_dir and netrc_dir are expected to be absolute paths."
     exit 1
 fi
 DST="${1}"
@@ -27,6 +27,7 @@ DST_BRANCH="${2}"
 TOKEN="${3}"
 NETRCDIR="${4}"
 MESSAGE="${5}"
+GOPATH="${6}"
 # set up github token
 echo "machine github.com login ${TOKEN}" > "${NETRCDIR}"/.netrc
 rm -f ~/.netrc
@@ -43,6 +44,23 @@ if git diff --cached --exit-code &>/dev/null; then
     exit 0
 fi
 git commit -m "${MESSAGE}"
+
+# Run "godep restore" to restore dependencies. Because entries for
+# k8s.io/apimachinery are removed from Godeps.json, so the just published code
+# for apimachinery will not be overwritten. Then we run "godep save" to save all
+# the dependencies, including the latest commit of k8s.io/apimachinery.
+godep restore
+godep save ./...
+git add --all
+if git diff --cached --exit-code &>/dev/null; then
+    echo "dependency has not changed!"
+else
+    git commit -m "pick up new dependencies on k8s.io repos"
+fi
+
+go build ./...
+go test ./...
+
 git push origin "${DST_BRANCH}"
 popd > /dev/null
 rm -f ~/.netrc
---
+++ mungegithub/mungers/publisher_issue.go
@@ -0,0 +1,116 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mungers
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/golang/glog"
+
+	githubapi "github.com/google/go-github/github"
+	"k8s.io/test-infra/mungegithub/github"
+)
+
+const Title = "Repo publisher failure"
+
+const owners = "@kubernetes-repo-publishing-cops"
+
+// publisherFailure includes the information to create an issue
+type publisherFailure struct {
+	err error
+	log string
+}
+
+func (p *publisherFailure) title() string {
+	return Title
+}
+
+func (p *publisherFailure) body() string {
+	body := p.err.Error()
+	body += fmt.Sprintf("\n")
+	body += fmt.Sprintf("cc %s", owners)
+	body += fmt.Sprintf("\n")
+	body += p.log
+	return body
+}
+
+// publisherIssueTracker tracks latest open issue regarding publisher failure,
+// can update the existing issue or create a new one.
+type publisherIssueTracker struct {
+	config             *github.Config
+	lastestOpenIssue   *githubapi.Issue
+	lastestIssueNumber int
+}
+
+func NewPublisherIssueTracker(config *github.Config) *publisherIssueTracker {
+	return &publisherIssueTracker{config: config}
+}
+
+func (p *publisherIssueTracker) Init() error {
+	issues, err := p.config.ListAllIssues(&githubapi.IssueListByRepoOptions{
+		State: "open",
+	})
+	if err != nil {
+		return err
+	}
+	for _, issue := range issues {
+		if issue.Title != nil && *issue.Title == Title &&
+			issue.Number != nil && *issue.Number > p.lastestIssueNumber {
+			p.lastestIssueNumber = *issue.Number
+			p.lastestOpenIssue = issue
+		}
+	}
+	return nil
+}
+
+func (p *publisherIssueTracker) FileIssue(failure publisherFailure) {
+	if p.lastestOpenIssue != nil {
+		obj, err := p.config.GetObject(*p.lastestOpenIssue.Number)
+		if err != nil {
+			glog.Errorf("failed to update issue %v: %v", obj, err)
+			return
+		}
+		err = obj.WriteComment(failure.body())
+		if err != nil {
+			glog.Errorf("failed to update issue %v: %v", obj, err)
+			return
+		}
+		glog.Infof("updated issue %v", obj)
+		return
+	}
+
+	obj, err := p.config.NewIssue(
+		failure.title(),
+		failure.body(),
+		[]string{},
+		"",
+	)
+	if err != nil {
+		glog.Errorf("failed to create issue %v: %v", obj, err)
+		return
+	}
+	glog.Infof("Created issue %v:\n%v", *obj.Issue.Number, failure.body())
+}
+
+func readLog(logFilePath string) (string, error) {
+	data, err := ioutil.ReadFile(logFilePath)
+	if err != nil {
+		return "", err
+	}
+	return string(data), err
+}
