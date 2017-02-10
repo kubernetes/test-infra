71925b9d24bd8565ea7846173c98abec84a7774e
Erick Fejta <fejta@google.com>
2017-02-09 23:48:20 -0800

    Make prow tell jokes

---
+++ prow/plugins/yuks/yuks.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package yuks
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+
+	"github.com/Sirupsen/logrus"
+
+	"k8s.io/test-infra/prow/github"
+	"k8s.io/test-infra/prow/plugins"
+)
+
+var (
+	match  = regexp.MustCompile(`(?m)^@k8s-(ci|ro)?bot tell me a joke[,.?!]*\r?$`)
+	simple = regexp.MustCompile(`^[\w?., ]+$`)
+)
+
+const (
+	jokeUrl    = realJoke("https://tambal.azurewebsites.net/joke/random")
+	pluginName = "yuks"
+)
+
+func init() {
+	plugins.RegisterIssueCommentHandler(pluginName, handleIssueComment)
+}
+
+type githubClient interface {
+	CreateComment(owner, repo string, number int, comment string) error
+}
+
+type joker interface {
+	readJoke() (string, error)
+}
+
+type realJoke string
+
+func (url realJoke) readJoke() (string, error) {
+	resp, err := http.Get(string(url))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	var a map[string]string
+	if err = json.NewDecoder(resp.Body).Decode(&a); err != nil {
+		return "", err
+	}
+	j, ok := a["joke"]
+	if !ok {
+		return "", fmt.Errorf("result does not contain a joke key: %v", a)
+	}
+	return j, nil
+}
+
+func handleIssueComment(pc plugins.PluginClient, ic github.IssueCommentEvent) error {
+	return handle(pc.GitHubClient, pc.Logger, ic, jokeUrl)
+}
+
+func handle(gc githubClient, log *logrus.Entry, ic github.IssueCommentEvent, j joker) error {
+	// Only consider new comments to open issues.
+	if ic.Issue.State != "open" || ic.Action != "created" {
+		return nil
+	}
+	// Make sure they are requesting a joke
+	if !match.MatchString(ic.Comment.Body) {
+		return nil
+	}
+
+	org := ic.Repo.Owner.Login
+	repo := ic.Repo.Name
+	number := ic.Issue.Number
+
+	resp, err := j.readJoke()
+	if err != nil {
+		return err
+	}
+	if !simple.MatchString(resp) {
+		return fmt.Errorf("joke contains invalid characters: %v", resp)
+	}
+	log.Infof("Commenting with \"%s\".", resp)
+	return gc.CreateComment(org, repo, number, plugins.FormatResponse(ic.Comment, resp))
+}
---
+++ prow/plugins/yuks/yuks_test.go
@@ -0,0 +1,159 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package yuks
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+
+	"k8s.io/test-infra/prow/github"
+	"k8s.io/test-infra/prow/github/fakegithub"
+)
+
+type fakeJoke string
+
+func (j fakeJoke) readJoke() (string, error) {
+	return string(j), nil
+}
+
+// Medium integration test (depends on ability to open a TCP port)
+func TestJokesMedium(t *testing.T) {
+	j := "What do you get when you cross a joke with a rhetorical question?"
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `{"joke": "%s"}`, j)
+	}))
+	defer ts.Close()
+	fc := &fakegithub.FakeClient{
+		IssueComments: make(map[int][]github.IssueComment),
+	}
+	ice := github.IssueCommentEvent{
+		Action: "created",
+		Comment: github.IssueComment{
+			Body: "@k8s-bot tell me a joke",
+		},
+		Issue: github.Issue{
+			Number: 5,
+			State:  "open",
+		},
+	}
+	if err := handle(fc, logrus.WithField("plugin", pluginName), ice, realJoke(ts.URL)); err != nil {
+		t.Errorf("didn't expect error: %v", err)
+		return
+	}
+	if len(fc.IssueComments[5]) != 1 {
+		t.Error("should have commented.")
+		return
+	}
+	if c := fc.IssueComments[5][0]; !strings.Contains(c.Body, j) {
+		t.Errorf("missing joke: %s from comment: %v", j, c)
+	}
+}
+
+// Small, unit tests
+func TestJokes(t *testing.T) {
+	var testcases = []struct {
+		name          string
+		action        string
+		body          string
+		state         string
+		joke          fakeJoke
+		pr            *struct{}
+		shouldComment bool
+		shouldError   bool
+	}{
+		{
+			name:          "ignore closed issue",
+			state:         "closed",
+			action:        "created",
+			body:          "@k8s-bot tell me a joke",
+			joke:          "this? that.",
+			shouldComment: false,
+			shouldError:   false,
+		},
+		{
+			name:          "ignore edited comment",
+			state:         "open",
+			action:        "edited",
+			body:          "@k8s-bot tell me a joke",
+			joke:          "this? that.",
+			shouldComment: false,
+			shouldError:   false,
+		},
+		{
+			name:          "leave joke on pr",
+			state:         "open",
+			action:        "created",
+			body:          "@k8s-bot tell me a joke",
+			joke:          "this? that.",
+			pr:            &struct{}{},
+			shouldComment: true,
+			shouldError:   false,
+		},
+		{
+			name:          "leave joke on issue",
+			state:         "open",
+			action:        "created",
+			body:          "@k8s-bot tell me a joke",
+			joke:          "this? that.",
+			shouldComment: true,
+			shouldError:   false,
+		},
+		{
+			name:          "reject bad joke chars",
+			state:         "open",
+			action:        "created",
+			body:          "@k8s-bot tell me a joke",
+			joke:          "[hello](url)",
+			shouldComment: false,
+			shouldError:   true,
+		},
+	}
+	for _, tc := range testcases {
+		fc := &fakegithub.FakeClient{
+			IssueComments: make(map[int][]github.IssueComment),
+		}
+		ice := github.IssueCommentEvent{
+			Action: tc.action,
+			Comment: github.IssueComment{
+				Body: tc.body,
+			},
+			Issue: github.Issue{
+				Number:      5,
+				State:       tc.state,
+				PullRequest: tc.pr,
+			},
+		}
+		err := handle(fc, logrus.WithField("plugin", pluginName), ice, tc.joke)
+		if !tc.shouldError && err != nil {
+			t.Errorf("For case %s, didn't expect error: %v", tc.name, err)
+			continue
+		} else if tc.shouldError && err == nil {
+			t.Errorf("For case %s, expected an error to occur", tc.name)
+			continue
+		}
+		if tc.shouldComment && len(fc.IssueComments[5]) != 1 {
+			t.Errorf("For case %s, should have commented.", tc.name)
+		} else if !tc.shouldComment && len(fc.IssueComments[5]) != 0 {
+			t.Errorf("For case %s, should not have commented.", tc.name)
+		}
+	}
+}
