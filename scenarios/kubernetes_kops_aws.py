2de767266b0e5e4233dc3eb4cef17a0ad63e05a0
Matthew Wong <mawong@redhat.com>
2017-02-24 14:24:32 -0500

    Add --kops-image to choose AMI

--- scenarios/kubernetes_kops_aws.py
+++ scenarios/kubernetes_kops_aws.py
@@ -149,8 +149,10 @@ def main(args):
       '-e', 'E2E_DOWN=%s' % args.down,
       # Kops
       '-e', ('E2E_OPT=--kops-cluster %s --kops-zones %s '
-             '--kops-state %s --kops-nodes=%s --kops-ssh-key=%s' %
-             (args.cluster, zones, args.state, args.nodes, aws_ssh)),
+             '--kops-state %s --kops-nodes=%s --kops-ssh-key=%s '
+             '--kops-image=%s' %
+             (args.cluster, zones, args.state, args.nodes, aws_ssh,
+              args.image)),
     ])
 
     # env blacklist.
@@ -219,6 +221,9 @@ if __name__ == '__main__':
         '--zones', default=None,
         help='Availability zones to start the cluster in. '
         'Defaults to a random zone.')
+    PARSER.add_argument(
+        '--image', default='',
+        help='Image (AMI) for nodes to use. Defaults to kops default.')
     ARGS = PARSER.parse_args()
 
     if not ARGS.cluster:
