e0486eaea7450a2234ab30c46d21aa111319e8ef
Jeff Grafton <jgrafton@google.com>
2017-02-28 16:39:58 -0800

    Bump e2e scenario to kubekins-test:v20170228-c2fc37ee

--- scenarios/kubernetes_e2e.py
+++ scenarios/kubernetes_e2e.py
@@ -334,7 +334,7 @@ if __name__ == '__main__':
     PARSER.add_argument(
         '--soak-test', action='store_true', help='If the test is a soak test job')
     PARSER.add_argument(
-        '--tag', default='v20170228-ebc41180', help='Use a specific kubekins-e2e tag if set')
+        '--tag', default='v20170228-c2fc37ee', help='Use a specific kubekins-e2e tag if set')
     PARSER.add_argument(
         '--test', default='true', help='If we need to set --test in e2e.go')
     PARSER.add_argument(
