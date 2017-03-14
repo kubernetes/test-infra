4b1a18ace985246f4332961b199276b2741182e9
Erick Fejta <fejta@google.com>
2017-03-14 10:19:30 -0700

    Update kubetest to v20170314-bb0669b0

--- scenarios/kubernetes_e2e.py
+++ scenarios/kubernetes_e2e.py
@@ -398,7 +398,7 @@ def create_parser():
     parser.add_argument(
         '--soak-test', action='store_true', help='If the test is a soak test job')
     parser.add_argument(
-        '--tag', default='v20170310-72232020', help='Use a specific kubekins-e2e tag if set')
+        '--tag', default='v20170314-bb0669b0', help='Use a specific kubekins-e2e tag if set')
     parser.add_argument(
         '--test', default='true', help='If we need to set --test in e2e.go')
     parser.add_argument(
