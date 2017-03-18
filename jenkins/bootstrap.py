118f9a0d6bc094d70c00f4bec51cbfa7ceb8fadc
Erick Fejta <fejta@google.com>
2017-03-18 15:40:39 -0700

    Use --ssh=SSH_KEY_PATH if set

--- jenkins/bootstrap.py
+++ jenkins/bootstrap.py
@@ -40,6 +40,7 @@ The contract with the runner is as follows:
 
 
 import argparse
+import contextlib
 import json
 import logging
 import os
@@ -51,6 +52,7 @@ import signal
 import socket
 import subprocess
 import sys
+import tempfile
 import time
 
 ORIG_CWD = os.getcwd()  # Checkout changes cwd
@@ -187,11 +189,11 @@ def branch_ref(branch):
         return [branch], ['FETCH_HEAD']
 
 
-def repository(repo, ssh=False):
+def repository(repo, ssh):
     """Return the url associated with the repo."""
     if repo.startswith('k8s.io/'):
         repo = 'github.com/kubernetes/%s' % (repo[len('k8s.io/'):])
-    if ssh is True:
+    if ssh:
         if ":" not in repo:
             parts = repo.split('/', 1)
             repo = '%s:%s' % (parts[0], parts[1])
@@ -204,7 +206,7 @@ def random_sleep(attempt):
     time.sleep(random.random() + attempt ** 2)
 
 
-def checkout(call, repo, branch, pull, ssh=False, git_cache='', clean=False):
+def checkout(call, repo, branch, pull, ssh='', git_cache='', clean=False):
     """Fetch and checkout the repository at the specified branch/pull."""
     # pylint: disable=too-many-locals
     if bool(branch) == bool(pull):
@@ -735,19 +737,47 @@ def gubernator_uri(paths):
     return job
 
 
+@contextlib.contextmanager
+def choose_ssh_key(ssh):
+    """Creates a script for GIT_SSH that uses -i ssh if set."""
+    if not ssh:  # Nothing to do
+        yield
+        return
+
+    # Create a script for use with GIT_SSH, which defines the program git uses
+    # during git fetch. In the future change this to GIT_SSH_COMMAND
+    # https://superuser.com/questions/232373/how-to-tell-git-which-private-key-to-use
+    with tempfile.NamedTemporaryFile(prefix='ssh', delete=False) as fp:
+        fp.write('#!/bin/sh\nssh -i \'%s\' -F /dev/null "${@}"\n' % ssh)
+    try:
+        os.chmod(fp.name, 0500)
+        had = 'GIT_SSH' in os.environ
+        old = os.getenv('GIT_SSH')
+        os.environ['GIT_SSH'] = fp.name
+
+        yield
+
+        del os.environ['GIT_SSH']
+        if had:
+            os.environ['GIT_SSH'] = old
+    finally:
+        os.unlink(fp.name)
+
+
 def setup_root(call, root, repos, ssh, git_cache, clean):
     """Create root dir, checkout repo and cd into resulting dir."""
     if not os.path.exists(root):
         os.makedirs(root)
     root_dir = os.path.realpath(root)
     logging.info('Root: %s', root_dir)
-    for repo, (branch, pull) in repos.items():
-        os.chdir(root_dir)
-        logging.info(
-            'Checkout: %s %s',
-            os.path.join(root_dir, repo),
-            pull and pull or branch)
-        checkout(call, repo, branch, pull, ssh, git_cache, clean)
+    with choose_ssh_key(ssh):
+        for repo, (branch, pull) in repos.items():
+            os.chdir(root_dir)
+            logging.info(
+                'Checkout: %s %s',
+                os.path.join(root_dir, repo),
+                pull and pull or branch)
+            checkout(call, repo, branch, pull, ssh, git_cache, clean)
     if len(repos) > 1:  # cd back into the primary repo
         os.chdir(root_dir)
         os.chdir(repos.main)
@@ -828,11 +858,6 @@ def bootstrap(args):
     exc_type = None
 
     try:
-        # TODO: find more graceful way to do ssh key
-        os.environ.setdefault(
-            'K8S_SECURITY_SSH_PRIVATE_KEY_FILE',
-            os.path.join(os.environ[HOME_ENV], '.ssh/id_ed25519'),
-        )
         setup_root(call, args.root, repos, args.ssh, args.git_cache, args.clean)
         logging.info('Configure environment...')
         if repos:
@@ -906,8 +931,7 @@ def parse_args(arguments=None):
         help='Activate and use path/to/service-account.json if set.')
     parser.add_argument(
         '--ssh',
-        action='store_true',
-        help='Use ssh to fetch the repository instead of https.')
+        help='Use the ssh key to fetch the repository instead of https if set.')
     parser.add_argument(
         '--git-cache',
         help='Location of the git cache.')
--- jenkins/bootstrap_test.py
+++ jenkins/bootstrap_test.py
@@ -258,6 +258,36 @@ class PullRefsTest(unittest.TestCase):
             (['master'], ['FETCH_HEAD']))
 
 
+class ChooseSshKeyTest(unittest.TestCase):
+    """Tests for choose_ssh_key()."""
+    def testEmpty(self):
+        """Do not change environ if no ssh key."""
+        fake_env = {}
+        with Stub(os, 'environ', fake_env):
+            with bootstrap.choose_ssh_key(''):
+                self.assertFalse(fake_env)
+
+    def testFull(self):
+        fake_env = {}
+        with Stub(os, 'environ', fake_env):
+            with bootstrap.choose_ssh_key('hello there'):
+                self.assertIn('GIT_SSH', fake_env)
+                with open(fake_env['GIT_SSH']) as fp:
+                    buf = fp.read()
+                self.assertIn('hello there', buf)
+                self.assertIn('ssh ', buf)
+                self.assertIn(' -i ', buf)
+            self.assertFalse(fake_env)  # Resets env
+
+    def testFullOldValue(self):
+        fake_env = {'GIT_SSH': 'random-value'}
+        old_env = dict(fake_env)
+        with Stub(os, 'environ', fake_env):
+            with bootstrap.choose_ssh_key('hello there'):
+                self.assertNotEqual(old_env, fake_env)
+            self.assertEquals(old_env, fake_env)
+
+
 class CheckoutTest(unittest.TestCase):
     """Tests for checkout()."""
 
@@ -1066,32 +1096,32 @@ class BootstrapTest(unittest.TestCase):
 class RepositoryTest(unittest.TestCase):
     def testKubernetesKubernetes(self):
         expected = 'https://github.com/kubernetes/kubernetes'
-        actual = bootstrap.repository('k8s.io/kubernetes')
+        actual = bootstrap.repository('k8s.io/kubernetes', '')
         self.assertEquals(expected, actual)
 
     def testKubernetesTestInfra(self):
         expected = 'https://github.com/kubernetes/test-infra'
-        actual = bootstrap.repository('k8s.io/test-infra')
+        actual = bootstrap.repository('k8s.io/test-infra', '')
         self.assertEquals(expected, actual)
 
     def testWhatever(self):
         expected = 'https://foo.com/bar'
-        actual = bootstrap.repository('foo.com/bar')
+        actual = bootstrap.repository('foo.com/bar', '')
         self.assertEquals(expected, actual)
 
     def testKubernetesKubernetesSSH(self):
         expected = 'git@github.com:kubernetes/kubernetes'
-        actual = bootstrap.repository('k8s.io/kubernetes', True)
+        actual = bootstrap.repository('k8s.io/kubernetes', 'path')
         self.assertEquals(expected, actual)
 
     def testKubernetesKubernetesSSHWithColon(self):
         expected = 'git@github.com:kubernetes/kubernetes'
-        actual = bootstrap.repository('github.com:kubernetes/kubernetes', True)
+        actual = bootstrap.repository('github.com:kubernetes/kubernetes', 'path')
         self.assertEquals(expected, actual)
 
     def testWhateverSSH(self):
         expected = 'git@foo.com:bar'
-        actual = bootstrap.repository('foo.com/bar', True)
+        actual = bootstrap.repository('foo.com/bar', 'path')
         self.assertEquals(expected, actual)
 
 
