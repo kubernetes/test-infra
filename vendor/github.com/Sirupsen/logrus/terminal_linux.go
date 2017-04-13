0ba8e90d7ac93b6481022bc2099d9b1741d2dbd0
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-04-13 07:32:39 +0200

    lukaszgryglicki-2196-commited-unneeded-file

--- vendor/github.com/Sirupsen/logrus/terminal_linux.go
+++ vendor/github.com/Sirupsen/logrus/terminal_linux.go
@@ -1,9 +1,12 @@
-// +build darwin freebsd openbsd netbsd dragonfly
+// Based on ssh/terminal:
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package logrus
 
 import "syscall"
 
-const ioctlReadTermios = syscall.TIOCGETA
+const ioctlReadTermios = syscall.TCGETS
 
 type Termios syscall.Termios
