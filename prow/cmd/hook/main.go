925f6979f5168a0a61df378f4f14f8aa8fa5e768
Dan Etter <dettervt@gmail.com>
2017-04-13 18:29:47 -0700

    Fix gofmt

--- prow/cmd/hook/main.go
+++ prow/cmd/hook/main.go
@@ -35,11 +35,11 @@ import (
 	_ "k8s.io/test-infra/prow/plugins/assign"
 	_ "k8s.io/test-infra/prow/plugins/cla"
 	_ "k8s.io/test-infra/prow/plugins/close"
-    _ "k8s.io/test-infra/prow/plugins/reopen"
 	_ "k8s.io/test-infra/prow/plugins/heart"
 	_ "k8s.io/test-infra/prow/plugins/label"
 	_ "k8s.io/test-infra/prow/plugins/lgtm"
 	_ "k8s.io/test-infra/prow/plugins/releasenote"
+	_ "k8s.io/test-infra/prow/plugins/reopen"
 	_ "k8s.io/test-infra/prow/plugins/trigger"
 	_ "k8s.io/test-infra/prow/plugins/yuks"
 )
