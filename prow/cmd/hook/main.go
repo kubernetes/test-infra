578cc0cdb1b9cd0fcfe7d6266f8a234adb0207c6
Dan Etter <dettervt@gmail.com>
2017-04-13 17:41:40 -0700

    Add import to hook/main.go, add reopen to plugins.yaml

--- prow/cmd/hook/main.go
+++ prow/cmd/hook/main.go
@@ -35,6 +35,7 @@ import (
 	_ "k8s.io/test-infra/prow/plugins/assign"
 	_ "k8s.io/test-infra/prow/plugins/cla"
 	_ "k8s.io/test-infra/prow/plugins/close"
+    _ "k8s.io/test-infra/prow/plugins/reopen"
 	_ "k8s.io/test-infra/prow/plugins/heart"
 	_ "k8s.io/test-infra/prow/plugins/label"
 	_ "k8s.io/test-infra/prow/plugins/lgtm"
