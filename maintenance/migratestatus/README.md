3bff6d31ca35253b159f733f7d0ee3f4546c92da
Cole Wagner <colew@google.com>
2017-04-28 17:32:32 -0700

    Created tool to migrate status contexts.

---
+++ maintenance/migratestatus/README.md
@@ -0,0 +1,30 @@
+# Status Context Migrator
+The migratestatus tool is a maintenance utility used to safely switch a repo from one status context to another.
+For example if there is a context named "CI tests" that needs to be moved by "CI tests v2" this tool can be used to copy every "CI tests" status into a "CI tests v2" status context and then mark every "CI tests" context as passing and retired. This ensures that no PRs are ever passing when they shouldn't be and doesn't block PRs that should be passing. The copy and retire phases can be run seperately or together at once in move mode.
+
+### Usage
+###### Modes
+This tool runs in one of three modes. Each mode is defined as a set of conditions on the statuses and an action to take if the conditions are met.
+- **copy** mode conditions on PRs that have the context to copy, but not the destination context. For each of these PRs, the 'copy' context's status is copied to the destination context.
+	- `$ ./migratestatus --copy="CI tests" --dest="CI tests v2" --tokenfile=$TOKEN_FILE --org=$ORG --repo=$REPO`
+- **retire** mode can be used to retire an old context with or without replacement. By setting the context's state to "success" the retired context can be ignored.
+	- With a replacement specified, the mode conditions on PRs that have the context to retire and the context that replaced the retired context. For each of these PRs, the context to retire has its state set to "success" and its description set to a message of the form "Context retired. Status moved to 'CI tests v2'.".
+	- Without a replacement specified, the mode conditions on PRs that have the context to retire. For each of these PRs, the context has its state set to "success" and its description set to "Context retired without replacement."
+	- `$ ./migratestatus --retire="CI tests" --dest="CI tests v2" --tokenfile=$TOKEN_FILE --org=$ORG --repo=$REPO`
+- **move** mode conditions on PRs that have the context to move, but not the destination context. For each of these PRs, a copy and retire action are performed. In other words, the status of the context to move is copied to the 'dest' context and then retired with 'dest' as the replacement.
+	- `$ ./migratestatus --move="CI tests" --dest="CI tests v2" --tokenfile=$TOKEN_FILE --org=$ORG --repo=$REPO`
+###### Flags
+The migratestatus binary is run locally and can be built by running `go build` from this directory. The binary accepts the following parameters:
+- `--tokenfile` The file containing the github authentication token to use.
+- `--org` The organization that owns the repository to migrate contexts in. (Kubernetes)
+- `--repo` The repository to migrate contexts in.
+- Exactly one of the following flags must appear. Each indicates a mode and specifies a context:
+	- `--copy` The context to copy.
+	- `--retire` The context to retire.
+	- `--move` The context to move (copy and retire).
+- `--dest` The destination context. This is the context that is copied **to** and/or the context that replaces the retired context. This flag may only be omitted if retire mode is specified and the old context is being retired without a replacement.
+
+The binary also accepts the following optional parameters:
+- `--dry-run` A bool indicating whether or not modifying actions should be disabled. Defaults to `true`.
+- `--continue-on-error` A bool indicating whether or not the migration should continue if context migration fails for an individual PR. Defaults to false.
+- `--alsologtostderr` A flag that sends logs to stderr in addition to temporary log files.
---
+++ maintenance/migratestatus/migrator/migrator_test.go
@@ -0,0 +1,373 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package migrator
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-github/github"
+)
+
+type modeTest struct {
+	name          string
+	start         []github.RepoStatus
+	expectedDiffs []*github.RepoStatus
+}
+
+// compareDiffs checks if a list of status updates matches an expected list of status updates.
+func compareDiffs(diffs []*github.RepoStatus, expectedDiffs []*github.RepoStatus) error {
+	if len(diffs) != len(expectedDiffs) {
+		return fmt.Errorf("failed because the returned diff had %d changes instead of %d.", len(diffs), len(expectedDiffs))
+	}
+	for _, diff := range diffs {
+		if diff == nil {
+			return fmt.Errorf("failed because the returned diff contained a nil RepoStatus.")
+		}
+		if diff.Context == nil {
+			return fmt.Errorf("failed because the returned diff contained a RepoStatus with a nil Context field.")
+		}
+		if diff.Description == nil {
+			return fmt.Errorf("failed because the returned diff contained a RepoStatus with a nil Description field.")
+		}
+		if diff.State == nil {
+			return fmt.Errorf("failed because the returned diff contained a RepoStatus with a nil State field.")
+		}
+		var match *github.RepoStatus
+		for _, expected := range expectedDiffs {
+			if *expected.Context == *diff.Context {
+				match = expected
+				break
+			}
+		}
+		if match == nil {
+			return fmt.Errorf("failed because the returned diff contained an unexpected change to context '%s'.", *diff.Context)
+		}
+		// Found a matching context. Make sure that fields are equal.
+		if *match.Description != *diff.Description {
+			return fmt.Errorf("failed because the returned diff for context '%s' had Description '%s' instead of '%s'.", *diff.Context, *diff.Description, *match.Description)
+		}
+		if *match.State != *diff.State {
+			return fmt.Errorf("failed because the returned diff for context '%s' had State '%s' instead of '%s'.", *diff.Context, *diff.State, *match.State)
+		}
+
+		if match.TargetURL == nil {
+			if diff.TargetURL != nil {
+				return fmt.Errorf("failed because the returned diff for context '%s' had a non-nil TargetURL.", *diff.Context)
+			}
+		} else if diff.TargetURL == nil {
+			return fmt.Errorf("failed because the returned diff for context '%s' had a nil TargetURL.", *diff.Context)
+		} else if *match.TargetURL != *diff.TargetURL {
+			return fmt.Errorf("failed because the returned diff for context '%s' had TargetURL '%s' instead of '%s'.", *diff.Context, *diff.TargetURL, *match.TargetURL)
+		}
+	}
+	return nil
+}
+
+func TestMoveMode(t *testing.T) {
+	contextA := "context A"
+	contextB := "context B"
+	desc := "Context retired. Status moved to \"context B\"."
+
+	tests := []*modeTest{
+		&modeTest{
+			name: "simple",
+			start: []github.RepoStatus{
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextA, "success", desc, ""),
+				makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+		},
+		&modeTest{
+			name: "unrelated contexts",
+			start: []github.RepoStatus{
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextA, "success", desc, ""),
+				makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+		},
+		&modeTest{
+			name: "unrelated contexts; missing context A",
+			start: []github.RepoStatus{
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "unrelated contexts; already have context A and B",
+			start: []github.RepoStatus{
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "unrelated contexts; already have context B; no context A",
+			start: []github.RepoStatus{
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name:          "no contexts",
+			start:         []github.RepoStatus{},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+	}
+
+	m := *MoveMode(contextA, contextB)
+	for _, test := range tests {
+		diff := m.ProcessStatuses(&github.CombinedStatus{Statuses: test.start})
+		if err := compareDiffs(diff, test.expectedDiffs); err != nil {
+			t.Errorf("MoveMode test '%s' %v\n", test.name, err)
+		}
+	}
+}
+
+func TestCopyMode(t *testing.T) {
+	contextA := "context A"
+	contextB := "context B"
+
+	tests := []*modeTest{
+		&modeTest{
+			name: "simple",
+			start: []github.RepoStatus{
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+		},
+		&modeTest{
+			name: "unrelated contexts",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+		},
+		&modeTest{
+			name: "already have context B",
+			start: []github.RepoStatus{
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "already have updated context B",
+			start: []github.RepoStatus{
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus(contextB, "success", "description 2", "url 2"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "unrelated contexts already have updated context B",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+				*makeStatus(contextB, "error", "description 3", "url 3"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "only have context B",
+			start: []github.RepoStatus{
+				*makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "unrelated contexts; context B but not A",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextB, "failure", "description 1", "url 1"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name:          "no contexts",
+			start:         []github.RepoStatus{},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+	}
+
+	m := *CopyMode(contextA, contextB)
+	for _, test := range tests {
+		diff := m.ProcessStatuses(&github.CombinedStatus{Statuses: test.start})
+		if err := compareDiffs(diff, test.expectedDiffs); err != nil {
+			t.Errorf("CopyMode test '%s' %v\n", test.name, err)
+		}
+	}
+}
+
+func TestRetireModeReplacement(t *testing.T) {
+	contextA := "context A"
+	contextB := "context B"
+	desc := "Context retired. Status moved to \"context B\"."
+
+	tests := []*modeTest{
+		&modeTest{
+			name: "simple",
+			start: []github.RepoStatus{
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextA, "success", desc, ""),
+			},
+		},
+		&modeTest{
+			name: "unrelated contexts;updated context B",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+				*makeStatus(contextB, "success", "description 3", "url 3"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextA, "success", desc, ""),
+			},
+		},
+		&modeTest{
+			name: "missing context B",
+			start: []github.RepoStatus{
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "unrelated contexts;missing context B",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "missing context A",
+			start: []github.RepoStatus{
+				*makeStatus(contextB, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "unrelated contexts;missing context A",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+				*makeStatus(contextB, "success", "description 3", "url 3"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name:          "no contexts",
+			start:         []github.RepoStatus{},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+	}
+
+	m := *RetireMode(contextA, contextB)
+	for _, test := range tests {
+		diff := m.ProcessStatuses(&github.CombinedStatus{Statuses: test.start})
+		if err := compareDiffs(diff, test.expectedDiffs); err != nil {
+			t.Errorf("RetireMode(Replacement) test '%s' %v\n", test.name, err)
+		}
+	}
+}
+
+func TestRetireModeNoReplacement(t *testing.T) {
+	contextA := "context A"
+	desc := "Context retired without replacement."
+
+	tests := []*modeTest{
+		&modeTest{
+			name: "simple",
+			start: []github.RepoStatus{
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextA, "success", desc, ""),
+			},
+		},
+		&modeTest{
+			name: "unrelated contexts",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus(contextA, "failure", "description 1", "url 1"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+			},
+			expectedDiffs: []*github.RepoStatus{
+				makeStatus(contextA, "success", desc, ""),
+			},
+		},
+		&modeTest{
+			name:          "missing context A",
+			start:         []github.RepoStatus{},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+		&modeTest{
+			name: "unrelated contexts;missing context A",
+			start: []github.RepoStatus{
+				*makeStatus("unrelated context", "success", "description 2", "url 2"),
+				*makeStatus("also not related", "error", "description 4", "url 4"),
+			},
+			expectedDiffs: []*github.RepoStatus{},
+		},
+	}
+
+	m := *RetireMode(contextA, "")
+	for _, test := range tests {
+		diff := m.ProcessStatuses(&github.CombinedStatus{Statuses: test.start})
+		if err := compareDiffs(diff, test.expectedDiffs); err != nil {
+			t.Errorf("RetireMode(NoReplace) test '%s' %v\n", test.name, err)
+		}
+	}
+}
+
+// makeStatus returns a new RepoStatus struct with the specified fields.
+// targetURL=="" means TargetURL==nil
+func makeStatus(context, state, description, targetURL string) *github.RepoStatus {
+	var url *string
+	if targetURL != "" {
+		url = &targetURL
+	}
+	return &github.RepoStatus{
+		Context:     &context,
+		State:       &state,
+		Description: &description,
+		TargetURL:   url,
+	}
+}
