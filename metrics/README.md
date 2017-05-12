daa9e718a2d8b6f469b5cab03640650d422e22f3
Erick Fejta <fejta@google.com>
2017-05-11 23:44:22 -0700

    Refactor bigquery scenario, include more than 100 rows

--- metrics/README.md
+++ metrics/README.md
@@ -6,22 +6,24 @@ database. The config files are consumed by the metrics-bigquery periodic prow jo
 ## Metrics
 
 * failures - find jobs that have been failing the longest
-    - Config: [failures-config.yaml](failures-config.yaml)
-    - [Latest results](http://storage.googleapis.com/k8s-metrics/failures/failures-latest.json)
+    - [Config](failures-config.yaml)
+    - [failures-latest.json](http://storage.googleapis.com/k8s-metrics/failures-latest.json)
 * flakes - find the flakiest jobs this week (and the flakiest tests in each job).
-    - Config: [flakes-config.yaml](flakes-config.yaml)
-    - [Latest results](http://storage.googleapis.com/k8s-metrics/flakes/flakes-latest.json)
+    - [Config](flakes-config.yaml)
+    - [flakes-latest.json](http://storage.googleapis.com/k8s-metrics/flakes-latest.json)
 * job-flakes - compute consistency of all jobs
-    - Config: [job-flakes-config.yaml](job-flakes-config.yaml)
-    - [Latest results](http://storage.googleapis.com/k8s-metrics/job-flakes/job-flakes-latest.json)
+    - [Config](job-flakes-config.yaml)
+    - [job-flakes-latest.json](http://storage.googleapis.com/k8s-metrics/job-flakes-latest.json)
 * weekly-consistency - compute overall weekly consistency for PRs
-    - Config: [weekly-consistency-config.yaml](weekly-consistency-config.yaml)
-    - [Latest results](http://storage.googleapis.com/k8s-metrics/weekly-consistency/weekly-consistency-latest.json)
+    - [Config](weekly-consistency-config.yaml)
+    - [weekly-consistency-latest.json](http://storage.googleapis.com/k8s-metrics/weekly-consistency-latest.json)
 
 ## Adding a new metric
 
-To add a new metric, create a PR that adds a new yaml config file specifying the metric name, the bigquery query to execute, and a jq filter to filter the data for the "METRICNAME-latest.json" file.
-Then add the path to the config file as a --config flag value under the args key of the metrics-bigquery job in [test-infra/jobs/config.json](../jobs/config.json). The new metric should have data available on GCS within 24 hours of the changes being merged into the test-infra repo.
+To add a new metric, create a PR that adds a new yaml config file
+specifying the metric name, the bigquery query to execute, and a
+jq filter to filter the data for the `METRICNAME-latest.json`
+file. Find the new metrics on GCS 24 hours after merging.
 
 ## Consistency
 
@@ -30,6 +32,3 @@ example suppose we run a build of a job 5 times at the same commit:
 * 5 passing runs, 0 failing runs: consistent
 * 0 passing runs, 5 failing runs: consistent
 * 1-4 passing runs, 1-4 failing runs: inconsistent aka flaked
-
-
-Future PRs will migrate other queries from [this spreadsheet](https://docs.google.com/spreadsheets/d/16nQPj_40xBgPLprj1DkKVTQ-BgQzO4A707q_JPsdxzY/edit).
--- scenarios/bigquery_test.py
+++ scenarios/bigquery_test.py
@@ -17,7 +17,6 @@
 """Tests for bigquery.py"""
 
 import argparse
-import glob
 import os
 import shutil
 import sys
@@ -32,35 +31,58 @@ class TestBigquery(unittest.TestCase):
     """Tests the bigquery scenario."""
 
     def test_configs(self):
-        """Test that the yaml config files in test-infra/metrics/ are valid metric config files."""
-        for path in glob.glob('metrics/*.yaml'):
-            with open(path) as config_file:
-                config = yaml.safe_load(config_file)
-                self.assertTrue(config)
-                self.assertTrue(config['metric'])
-                self.assertTrue(config['query'])
-                self.assertTrue(config['jqfilter'])
-                bigquery.validate_metric_name(config['metric'].strip())
+        """All yaml files in metrics are valid."""
+        start = os.path.join(os.path.dirname(__file__), '../metrics')
+        for root, _, names in os.walk(start):
+            for name in names:
+                if name in ['BUILD', 'README.md']:
+                    continue
+                if not name.endswith('.yaml'):
+                    self.fail('Only .yaml files allowed: %s' % name)
+
+                path = os.path.join(root, name)
+                with open(path) as config_file:
+                    config = yaml.safe_load(config_file)
+                    if not config:
+                        self.fail(path)
+                    self.assertIn('metric', config)
+                    self.assertIn('query', config)
+                    self.assertIn('jqfilter', config)
+                    bigquery.validate_metric_name(config['metric'].strip())
 
     def test_jq(self):
-        """Test that the do_jq function can execute a jq filter properly."""
+        """do_jq executes a jq filter properly."""
         # [filter, data, expected output]
-        tests = [['.', '{ "field": "value" }', '{"field":"value"}'],
-                 ['.field', '{ "field": "value" }', '"value"']]
+        tests = [
+            ['.', '{ "field": "value" }', '{"field":"value"}'],
+            ['.field', '{ "field": "value" }', '"value"'],
+        ]
         for test in tests:
             with open(self.data_filename, 'w') as data_file:
                 data_file.write(test[1])
-            bigquery.do_jq(test[0], self.data_filename, self.out_filename, jq_bin=ARGS.jq)
+            bigquery.do_jq(
+                test[0],
+                self.data_filename,
+                self.out_filename,
+                jq_bin=ARGS.jq,
+            )
             with open(self.out_filename) as out_file:
                 actual = out_file.read().replace(' ', '').replace('\n', '')
-                self.assertEqual(actual, test[2], msg='expected jq "{}" on data: {} to output {}'
-                                 ' but got {}'.format(test[0], test[1], test[2], actual))
+                self.assertEqual(actual, test[2])
 
     def test_validate_metric_name(self):
-        """Test the the validate_metric_name function rejects invalid metric names."""
-        tests = ['invalid#metric', 'invalid/metric', 'in\\valid', 'invalid?yes', '*invalid',
-                 '[metric]', 'metric\n', 'met\ric', 'metric& invalid']
-        for test in tests:
+        """validate_metric_name rejects invalid metric names."""
+        for test in [
+                'invalid#metric',
+                'invalid/metric',
+                'in\\valid',
+                'invalid?yes',
+                '*invalid',
+                '[metric]',
+                'metric\n',
+                'met\ric',
+                'metric& invalid',
+        ]:
             self.assertRaises(ValueError, bigquery.validate_metric_name, test)
 
     def setUp(self):
@@ -74,9 +96,7 @@ class TestBigquery(unittest.TestCase):
 
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser()
-    PARSER.add_argument('--jq',
-                        required=True,
-                        help='The path to the "jq" command.')
+    PARSER.add_argument('--jq', default='jq', help='path to jq binary')
     ARGS, _ = PARSER.parse_known_args()
 
     # Remove the --jq flag and its value so that unittest.main can parse the remaining args without
