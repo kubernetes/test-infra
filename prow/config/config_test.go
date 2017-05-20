469c4681bc544fc28559b0e0b64d511eb057bd93
Erick Fejta <fejta@google.com>
2017-05-20 00:45:57 -0700

    Require all contexts to match jobs

--- prow/config/config_test.go
+++ prow/config/config_test.go
@@ -18,9 +18,13 @@ package config
 
 import (
 	"fmt"
+	"io/ioutil"
 	"reflect"
+	"regexp"
 	"strings"
 	"testing"
+
+	"github.com/ghodss/yaml"
 )
 
 func TestConfigLoads(t *testing.T) {
@@ -51,6 +55,85 @@ func Replace(j *Presubmit, ks *Presubmit) error {
 	}
 
 	return nil
+}
+
+func CheckContext(t *testing.T, repo string, p Presubmit) {
+	if p.Name != p.Context {
+		t.Errorf("Context does not match job name: %s in %s", p.Name, repo)
+	}
+	for _, c := range p.RunAfterSuccess {
+		CheckContext(t, repo, c)
+	}
+}
+
+func TestContextMatches(t *testing.T) {
+	c, err := Load("../config.yaml")
+	if err != nil {
+		t.Fatalf("Could not load config: %v", err)
+	}
+
+	for repo, presubmits := range c.Presubmits {
+		for _, p := range presubmits {
+			CheckContext(t, repo, p)
+		}
+	}
+}
+
+type SubmitQueueConfig struct {
+	Data map[string]string `json:"data"`
+}
+
+func FindRequired(t *testing.T, presubmits []Presubmit) []string {
+	var required []string
+	for _, p := range presubmits {
+		if !p.AlwaysRun {
+			continue
+		}
+		for _, r := range FindRequired(t, p.RunAfterSuccess) {
+			required = append(required, r)
+		}
+		if p.SkipReport {
+			continue
+		}
+		required = append(required, p.Context)
+	}
+	return required
+}
+
+func TestRequiredRetestContextsMatch(t *testing.T) {
+	c, err := Load("../config.yaml")
+	if err != nil {
+		t.Fatalf("Could not load config: %v", err)
+	}
+	b, err := ioutil.ReadFile("../../mungegithub/submit-queue/deployment/kubernetes/configmap.yaml")
+	if err != nil {
+		t.Fatalf("Could not load submit queue configmap: %v", err)
+	}
+	sqc := &SubmitQueueConfig{}
+	if err = yaml.Unmarshal(b, sqc); err != nil {
+		t.Fatalf("Could not parse submit queue configmap: %v", err)
+	}
+	re := regexp.MustCompile(`"([^"]+)"`)
+	var required []string
+	for _, g := range re.FindAllStringSubmatch(sqc.Data["test-options.required-retest-contexts"], -1) {
+		required = append(required, g[1])
+	}
+
+	running := FindRequired(t, c.Presubmits["kubernetes/kubernetes"])
+
+	for _, r := range required {
+		found := false
+		for _, s := range running {
+			if s == r {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Required context: %s does not always run: %s", r, running)
+		}
+	}
+
 }
 
 func TestConfigSecurityJobsMatch(t *testing.T) {
