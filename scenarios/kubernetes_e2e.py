582e5c84dffa7e8c501b586796ac0d2784c1abbc
Sen Lu <senlu@google.com>
2017-07-20 10:53:25 -0700

    Move set project within kubetest

--- scenarios/kubernetes_e2e.py
+++ scenarios/kubernetes_e2e.py
@@ -184,15 +184,6 @@ class LocalMode(object):
         env.update(self.os_env)
         env.update(self.env_files)
         env.update(self.env)
-        # Do not interfere with the local project
-        project = env.get('PROJECT')
-        if project:
-            try:
-                check('gcloud', 'config', 'set', 'project', env['PROJECT'])
-            except subprocess.CalledProcessError:
-                print >>sys.stderr, 'Fail to set project %r', project
-        else:
-            print >>sys.stderr, 'PROJECT not set in job, will use local project'
         check_env(env, 'kubetest', *args)
 
 
