d774c95acf5568e917c8e1fe8982a42e4d9b20d1
Yang Guo <ygg@google.com>
2017-08-02 16:09:18 -0700

    Update prow jobs to use the new image

--- experiment/generate_tests.py
+++ experiment/generate_tests.py
@@ -56,7 +56,7 @@ PROW_CONFIG_TEMPLATE = """
           value: /etc/ssh-key-secret/ssh-private
         - name: JENKINS_GCE_SSH_PUBLIC_KEY_FILE
           value: /etc/ssh-key-secret/ssh-public
-        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170801-9babdb61
+        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170802-c4ea9089
         volumeMounts:
         - mountPath: /etc/service-account
           name: service
