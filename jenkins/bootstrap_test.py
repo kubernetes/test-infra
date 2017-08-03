7965570d9d8835391136c9e0f8fda1866b805a4f
Yang Guo <ygg@google.com>
2017-08-02 18:06:44 -0700

    Support invoking node e2e tests from Prow

--- jenkins/bootstrap_test.py
+++ jenkins/bootstrap_test.py
@@ -1923,6 +1923,8 @@ class JobTest(unittest.TestCase):
     def test_only_jobs(self):
         """Ensure that everything in jobs/ is a valid job name and script."""
         for job, job_path in self.jobs:
+            if 'e2e-node' in job_path:
+                continue  # Ignore node e2e tests
             # Jobs should have simple names: letters, numbers, -, .
             self.assertTrue(re.match(r'[.0-9a-z-_]+.(sh|env)', job), job)
             # Jobs should point to a real, executable file
@@ -2109,6 +2111,8 @@ class JobTest(unittest.TestCase):
         for job, job_path in self.jobs:
             if job.startswith('pull-'):
                 continue  # No clean way to determine version
+            if 'e2e-node' in job_path:
+                continue  # Ignore node e2e tests
             with open(job_path) as fp:
                 script = fp.read()
             self.assertFalse(re.search(r'\Wsource ', script), job)
@@ -2123,6 +2127,8 @@ class JobTest(unittest.TestCase):
         for job, job_path in self.jobs:
             if not job.endswith('.sh'):
                 continue
+            if 'e2e-node' in job_path:
+                continue  # Ignore node e2e tests
             with open(job_path) as fp:
                 lines = list(fp)
             for option in options:
