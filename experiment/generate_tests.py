423aab33df9948fa7e73d5de7be6c1f4d6c362b7
Zach Loafman <zml@google.com>
2017-08-07 11:57:13 -0700

    Bump e2e-prow to v20170807-fecc6833 (using generate_tests and manual)

--- experiment/generate_tests.py
+++ experiment/generate_tests.py
@@ -55,7 +55,7 @@ PROW_CONFIG_TEMPLATE = """
           value: /etc/ssh-key-secret/ssh-private
         - name: JENKINS_GCE_SSH_PUBLIC_KEY_FILE
           value: /etc/ssh-key-secret/ssh-public
-        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170802-c4ea9089
+        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170807-fecc6833
         volumeMounts:
         - mountPath: /etc/service-account
           name: service
