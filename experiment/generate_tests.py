a4fc3daca4375f2b46397e8578e65a196a9a9919
Zach Loafman <zml@google.com>
2017-08-08 17:44:57 -0700

    Bump to gcr.io/k8s-testimages/kubekins-e2e-prow:v20170808-562b55b5 (using generate_tests and manual)

--- experiment/generate_tests.py
+++ experiment/generate_tests.py
@@ -55,7 +55,7 @@ PROW_CONFIG_TEMPLATE = """
           value: /etc/ssh-key-secret/ssh-private
         - name: JENKINS_GCE_SSH_PUBLIC_KEY_FILE
           value: /etc/ssh-key-secret/ssh-public
-        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170808-8dda933a
+        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170808-562b55b5
         volumeMounts:
         - mountPath: /etc/service-account
           name: service
