4d6f51c04520c21099eec648d570b9a78d55ca4d
Shyam JVS <shyamjvs@google.com>
2017-08-26 01:46:21 +0200

    Revert "Bump kubekins and prow images"

--- experiment/generate_tests.py
+++ experiment/generate_tests.py
@@ -56,7 +56,7 @@ PROW_CONFIG_TEMPLATE = """
           value: /etc/ssh-key-secret/ssh-private
         - name: JENKINS_GCE_SSH_PUBLIC_KEY_FILE
           value: /etc/ssh-key-secret/ssh-public
-        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170825-886d8e5e
+        image: gcr.io/k8s-testimages/kubekins-e2e-prow:v20170821-8474e659
         volumeMounts:
         - mountPath: /etc/service-account
           name: service
