021d59175f5955bda4b72ef5a2bf767c7167f950
Sen Lu <senlu@google.com>
2017-08-30 14:46:22 -0700

    SHRUG!

--- prow/cmd/hook/main.go
+++ prow/cmd/hook/main.go
@@ -45,6 +45,7 @@ import (
 	_ "k8s.io/test-infra/prow/plugins/lgtm"
 	_ "k8s.io/test-infra/prow/plugins/releasenote"
 	_ "k8s.io/test-infra/prow/plugins/reopen"
+	_ "k8s.io/test-infra/prow/plugins/shrug"
 	_ "k8s.io/test-infra/prow/plugins/size"
 	_ "k8s.io/test-infra/prow/plugins/trigger"
 	_ "k8s.io/test-infra/prow/plugins/updateconfig"
