13353aaa40e493db718055044f17b69ff3a24fdf
xiangpengzhao <zhao.xiangpeng@zte.com.cn>
2017-08-31 16:59:08 +0800

    Add "/release-note-action-required" as a prow command.

--- prow/plugins/releasenote/releasenote.go
+++ prow/plugins/releasenote/releasenote.go
@@ -43,8 +43,9 @@ var (
 		releaseNote,
 	}
 
-	releaseNoteRe     = regexp.MustCompile(`(?mi)^/release-note\s*$`)
-	releaseNoteNoneRe = regexp.MustCompile(`(?mi)^/release-note-none\s*$`)
+	releaseNoteRe               = regexp.MustCompile(`(?mi)^/release-note\s*$`)
+	releaseNoteNoneRe           = regexp.MustCompile(`(?mi)^/release-note-none\s*$`)
+	releaseNoteActionRequiredRe = regexp.MustCompile(`(?mi)^/release-note-action-required\s*$`)
 )
 
 func init() {
@@ -74,11 +75,14 @@ func handle(gc githubClient, log *logrus.Entry, ic github.IssueCommentEvent) err
 
 	// Which label does the comment want us to add?
 	var nl string
-	if releaseNoteRe.MatchString(ic.Comment.Body) {
+	switch {
+	case releaseNoteRe.MatchString(ic.Comment.Body):
 		nl = releaseNote
-	} else if releaseNoteNoneRe.MatchString(ic.Comment.Body) {
+	case releaseNoteNoneRe.MatchString(ic.Comment.Body):
 		nl = releaseNoteNone
-	} else {
+	case releaseNoteActionRequiredRe.MatchString(ic.Comment.Body):
+		nl = releaseNoteActionRequired
+	default:
 		return nil
 	}
 
