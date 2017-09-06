02ac7667a5b4046b4e30f113004848a9a1e1ca89
Sen Lu <senlu@google.com>
2017-09-05 17:42:53 -0700

    Bump e2e-kubekins for docker mode to v20170905-80a93425

--- scenarios/kubernetes_e2e.py
+++ scenarios/kubernetes_e2e.py
@@ -36,7 +36,7 @@ import time
 ORIG_CWD = os.getcwd()  # Checkout changes cwd
 
 # Note: This variable is managed by experiment/bump_e2e_image.sh.
-DEFAULT_KUBEKINS_TAG = 'v20170905-a67c1bff'
+DEFAULT_KUBEKINS_TAG = 'v20170905-80a93425'
 
 def test_infra(*paths):
     """Return path relative to root of test-infra repo."""
