ed33682a2931629eab09315451d88ac4d8a845c0
Jakub Hadvig <jhadvig@redhat.com>
2017-09-25 11:28:37 +0200

    Add branding customization as an extension

--- prow/cmd/deck/static/extensions/script.js
+++ prow/cmd/deck/static/extensions/script.js
@@ -1,1 +1,9 @@
 // This file serves for custom javascript extensions
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var head = document.getElementsByTagName('head')[0];
+	head.insertAdjacentHTML('beforeend', '<link rel="shortcut icon" href="extensions/logo.png">');
+
+	var header = document.getElementsByTagName('header')[0];
+	header.insertAdjacentHTML('afterbegin', '<a class="logo" ng-href="https://github.com/openshift/origin"><img src="extensions/logo.png" alt="openshfit logo" class="titleLogo"></a>');
+});
