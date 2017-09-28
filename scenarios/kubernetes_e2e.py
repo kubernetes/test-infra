71ecbe7f3f4c97a2921f34b71a5ca81db5c2ac17
Sen Lu <senlu@google.com>
2017-09-27 17:42:09 -0700

    Bump to gcr.io/k8s-testimages/kubekins-e2e:v20170927-54229024

--- scenarios/kubernetes_e2e.py
+++ scenarios/kubernetes_e2e.py
@@ -36,7 +36,7 @@ import time
 ORIG_CWD = os.getcwd()  # Checkout changes cwd
 
 # Note: This variable is managed by experiment/bump_e2e_image.sh.
-DEFAULT_KUBEKINS_TAG = 'v20170925-f564687f'
+DEFAULT_KUBEKINS_TAG = 'v20170927-54229024'
 
 def test_infra(*paths):
     """Return path relative to root of test-infra repo."""
