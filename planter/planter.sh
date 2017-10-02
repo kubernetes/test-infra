6b8838b91e3c7f13265803a743fee3c9f81c8105
Benjamin Elder <bentheelder@google.com>
2017-10-02 13:47:33 -0700

    planter.sh add docker image pull

--- planter/planter.sh
+++ planter/planter.sh
@@ -27,4 +27,5 @@ REPO=${REPO:-${PWD}}
 VOLUMES="-v ${REPO}:${REPO} -v ${HOME}:${HOME} --tmpfs /tmp:exec,mode=777"
 GID="$(id -g ${USER})"
 ENV="-e USER=${USER} -e GID=${GID} -e UID=${UID} -e HOME=${HOME}"
+docker image pull ${IMAGE}
 docker run --rm ${VOLUMES} --user ${UID} -w ${PWD} ${ENV} ${IMAGE} ${@}
