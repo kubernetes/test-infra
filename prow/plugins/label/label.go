648858e5fc72c2d9a39c2e27204e3c6bea2f2bb1
Maru Newby <marun@redhat.com>
2017-10-05 14:45:41 -0700

    prow: Update status labeling to reflect issues process
    - remove status/in-review (not that useful in addition to in-progress)
    - allow addition of status/in-progress by anyone
    - support removal of status labels

--- prow/plugins/label/label.go
+++ prow/plugins/label/label.go
@@ -41,24 +41,23 @@ type assignEvent struct {
 }
 
 var (
-	labelRegex              = regexp.MustCompile(`(?m)^/(area|priority|kind|sig)\s*(.*)$`)
-	removeLabelRegex        = regexp.MustCompile(`(?m)^/remove-(area|priority|kind|sig)\s*(.*)$`)
-	statusRegex             = regexp.MustCompile(`(?m)^/status\s+(.+)$`)
+	labelRegex              = regexp.MustCompile(`(?m)^/(area|priority|kind|sig|status)\s*(.*)$`)
+	removeLabelRegex        = regexp.MustCompile(`(?m)^/remove-(area|priority|kind|sig|status)\s*(.*)$`)
 	sigMatcher              = regexp.MustCompile(`(?m)@kubernetes/sig-([\w-]*)-(misc|test-failures|bugs|feature-requests|proposals|pr-reviews|api-reviews)`)
 	chatBack                = "Reiterating the mentions to trigger a notification: \n%v"
 	nonExistentLabelOnIssue = "Those labels are not set on the issue: `%v`"
-	mustBeSigLead           = "You must be a member of the @kubernetes/kubernetes-milestone-maintainers github team to add status labels"
-	statusMap               = map[string]string{
-		"approved-for-milestone": "status/approved-for-milestone",
-		"in-progress":            "status/in-progress",
-		"in-review":              "status/in-review",
-	}
-	kindMap = map[string]string{
+	kindMap                 = map[string]string{
 		"bugs":             "kind/bug",
 		"feature-requests": "kind/feature",
 		"api-reviews":      "kind/api-change",
 		"proposals":        "kind/design",
 	}
+
+	// For status labeling
+	approvedForMilestoneLabel = "status/approved-for-milestone"
+	inProgressLabel           = "status/in-progress"
+	mustBeSigLead             = fmt.Sprintf("You must be a member of the @kubernetes/kubernetes-milestone-maintainers github team to add or remove the `%s` label.", approvedForMilestoneLabel)
+	approvedBeforeInProgress  = fmt.Sprintf("The `%s` label must be present before the `%s` label can be added.", approvedForMilestoneLabel, inProgressLabel)
 )
 
 func init() {
@@ -169,8 +168,7 @@ func handle(gc githubClient, log *logrus.Entry, ae assignEvent, sc slackClient, 
 	labelMatches := labelRegex.FindAllStringSubmatch(ae.body, -1)
 	removeLabelMatches := removeLabelRegex.FindAllStringSubmatch(ae.body, -1)
 	sigMatches := sigMatcher.FindAllStringSubmatch(ae.body, -1)
-	statusMatches := statusRegex.FindAllStringSubmatch(ae.body, -1)
-	if len(labelMatches) == 0 && len(sigMatches) == 0 && len(removeLabelMatches) == 0 && len(statusMatches) == 0 {
+	if len(labelMatches) == 0 && len(sigMatches) == 0 && len(removeLabelMatches) == 0 {
 		return nil
 	}
 
@@ -194,6 +192,14 @@ func handle(gc githubClient, log *logrus.Entry, ae assignEvent, sc slackClient, 
 	labelsToAdd = getLabelsFromREMatches(labelMatches)
 	labelsToRemove = getLabelsFromREMatches(removeLabelMatches)
 
+	// The status checker is used to determine whether a given status label can be added or removed.
+	statusChecker := statusLabelChecker{
+		gc:            gc,
+		log:           log,
+		ae:            ae,
+		maintainersID: maintainersID,
+	}
+
 	// Add labels
 	for _, labelToAdd := range labelsToAdd {
 		if ae.issue.HasLabel(labelToAdd) {
@@ -202,6 +208,10 @@ func handle(gc githubClient, log *logrus.Entry, ae assignEvent, sc slackClient, 
 
 		if _, ok := existingLabels[labelToAdd]; !ok {
 			nonexistent = append(nonexistent, labelToAdd)
+			continue
+		}
+
+		if !statusChecker.okToAdd(labelToAdd, labelsToAdd) {
 			continue
 		}
 
@@ -222,37 +232,12 @@ func handle(gc githubClient, log *logrus.Entry, ae assignEvent, sc slackClient, 
 			continue
 		}
 
+		if !statusChecker.okToRemove(labelToRemove) {
+			continue
+		}
+
 		if err := gc.RemoveLabel(ae.org, ae.repo, ae.number, labelToRemove); err != nil {
 			log.WithError(err).Errorf("Github failed to remove the following label: %s", labelToRemove)
-		}
-	}
-
-	maintainersMap := map[string]bool{}
-	milestoneMaintainers, err := gc.ListTeamMembers(maintainersID)
-	if err != nil {
-		log.WithError(err).Errorf("Failed to list the teammembers for the milestone maintainers team")
-	} else {
-		for _, person := range milestoneMaintainers {
-			maintainersMap[person.Login] = true
-		}
-	}
-
-	for _, statusMatch := range statusMatches {
-		status := strings.TrimSpace(statusMatch[1])
-		sLabel, validStatus := statusMap[status]
-		if validStatus {
-			_, ok := maintainersMap[ae.login]
-			if ok {
-				if err := gc.AddLabel(ae.org, ae.repo, ae.number, sLabel); err != nil {
-					log.WithError(err).Errorf("Github failed to add the following label: %s", sLabel)
-				}
-			} else {
-				// not in the milestone maintainers team
-				if err := gc.CreateComment(ae.org, ae.repo, ae.number, mustBeSigLead); err != nil {
-					log.WithError(err).Errorf("Could not create comment \"%s\".", mustBeSigLead)
-				}
-			}
-
 		}
 	}
 
@@ -307,4 +292,95 @@ func handle(gc githubClient, log *logrus.Entry, ae assignEvent, sc slackClient, 
 	}
 
 	return nil
+}
+
+// statusLabelChecker ensures that only maintainers can add and remove the
+// status/approved-for-milestone label, and that status/in-progress can only be added if
+// status/approved-for-milestone is present.
+type statusLabelChecker struct {
+	gc             githubClient
+	log            *logrus.Entry
+	ae             assignEvent
+	maintainersID  int
+	maintainersMap map[string]bool
+}
+
+// okToAdd indicates whether it's ok to add the given label
+func (s *statusLabelChecker) okToAdd(label string, labels []string) bool {
+	return s.checkApprovedForMilestone(label) && s.checkInProgress(label, labels)
+}
+
+// okToRemove indicates whether it's ok to remove the given label
+func (s *statusLabelChecker) okToRemove(label string) bool {
+	return s.checkApprovedForMilestone(label)
+}
+
+// checkInProgress indicates whether the given label should be added if it is
+// status/in-progress.
+func (s *statusLabelChecker) checkInProgress(label string, labels []string) bool {
+	if label == inProgressLabel {
+		// Issue is already approved for the milestone
+		if s.ae.issue.HasLabel(approvedForMilestoneLabel) {
+			return true
+		}
+		// User is a maintainer and is also attempting to apply the approved label
+		if stringSliceHasElement(labels, approvedForMilestoneLabel) && s.isMaintainer(s.ae.login) {
+			return true
+		}
+		s.addComment(approvedBeforeInProgress)
+		return false
+	}
+	return true
+}
+
+// checkApprovedForMilestone indicates whether the given label should
+// be added if it is status/approved-for-milestone.
+func (s *statusLabelChecker) checkApprovedForMilestone(label string) bool {
+	if label == approvedForMilestoneLabel && !s.isMaintainer(s.ae.login) {
+		s.addComment(mustBeSigLead)
+		return false
+	}
+	return true
+}
+
+// isMaintainer indicates whether the given login is for a milestone maintainer.
+func (s *statusLabelChecker) isMaintainer(login string) bool {
+	if s.maintainersMap == nil {
+		s.maintainersMap = loadMaintainersMap(s.gc, s.log, s.maintainersID)
+	}
+
+	_, ok := s.maintainersMap[login]
+	return ok
+}
+
+// addComment adds the given message as a comment on the object
+// indicated by the assign event.
+func (s *statusLabelChecker) addComment(msg string) {
+	if err := s.gc.CreateComment(s.ae.org, s.ae.repo, s.ae.number, msg); err != nil {
+		s.log.WithError(err).Errorf("Could not create comment \"%s\".", msg)
+	}
+}
+
+// loadMaintainersMap load the list of maintainers for the given id.
+func loadMaintainersMap(gc githubClient, log *logrus.Entry, maintainersID int) map[string]bool {
+	maintainersMap := map[string]bool{}
+	milestoneMaintainers, err := gc.ListTeamMembers(maintainersID)
+	if err != nil {
+		log.WithError(err).Errorf("Failed to list the teammembers for the milestone maintainers team")
+	} else {
+		for _, person := range milestoneMaintainers {
+			maintainersMap[person.Login] = true
+		}
+	}
+	return maintainersMap
+}
+
+// stringSliceHasElement indicates whether the given slice contains the given element.
+func stringSliceHasElement(slice []string, element string) bool {
+	for i := range slice {
+		if slice[i] == element {
+			return true
+		}
+	}
+	return false
 }
