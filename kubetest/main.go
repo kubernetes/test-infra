d04f35cf3b569b9aade4d752901e6ed77d8b323d
Justin Santa Barbara <justin@fathomdb.com>
2017-10-21 21:18:05 -0400

    kubetest kops: reuse cluster flag from GKE
    We can hopefully remove `--kops-cluster` once this is deployed.

--- kubetest/main.go
+++ kubetest/main.go
@@ -225,7 +225,7 @@ func getDeployer(o *options) (deployer, error) {
 	case "gke":
 		return newGKE(o.provider, o.gcpProject, o.gcpZone, o.gcpRegion, o.gcpNetwork, o.gcpNodeImage, o.cluster, &o.testArgs, &o.upgradeArgs)
 	case "kops":
-		return newKops(o.provider, o.gcpProject)
+		return newKops(o.provider, o.gcpProject, o.cluster)
 	case "kubernetes-anywhere":
 		if o.multiClusters.Enabled() {
 			return newKubernetesAnywhereMultiCluster(o.gcpProject, o.gcpZone, o.multiClusters)
