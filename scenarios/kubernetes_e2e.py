143b29e41faea93842d0ee5991ccb13693aa02a3
Justin Santa Barbara <justin@fathomdb.com>
2017-10-21 21:16:17 -0400

    kubernetes_e2e fixes for kops on GCE
    Map options even when not using `--aws`, map the correct key

--- scenarios/kubernetes_e2e.py
+++ scenarios/kubernetes_e2e.py
@@ -406,6 +406,31 @@ def build_kops(kops, mode):
     check('make', 'gcs-publish-ci', 'VERSION=%s' % version, 'GCS_LOCATION=%s' % gcs)
 
 
+def set_up_kops_gce(workspace, args, mode, cluster, runner_args):
+    """Set up kops on GCE envs."""
+    for path in [args.gce_ssh, args.gce_pub]:
+        if not os.path.isfile(os.path.expandvars(path)):
+            raise IOError(path, os.path.expandvars(path))
+    mode.add_gce_ssh(args.gce_ssh, args.gce_pub)
+
+    gce_ssh = os.path.join(workspace, '.ssh', 'google_compute_engine')
+
+    zones = args.kops_zones or random.choice([
+        'us-central1-a',
+        'us-central1-b',
+        'us-central1-c',
+        'us-central1-f',
+    ])
+
+    runner_args.extend([
+        '--kops-cluster=%s' % cluster,
+        '--kops-zones=%s' % zones,
+        '--kops-state=%s' % args.kops_state_gce,
+        '--kops-nodes=%s' % args.kops_nodes,
+        '--kops-ssh-key=%s' % gce_ssh,
+    ])
+
+
 def set_up_aws(workspace, args, mode, cluster, runner_args):
     """Set up aws related envs."""
     for path in [args.aws_ssh, args.aws_pub, args.aws_cred]:
@@ -594,6 +619,8 @@ def main(args):
 
     if args.aws:
         set_up_aws(mode.workspace, args, mode, cluster, runner_args)
+    elif args.kops:
+        set_up_kops_gce(mode.workspace, args, mode, cluster, runner_args)
     elif args.gce_ssh:
         mode.add_gce_ssh(args.gce_ssh, args.gce_pub)
 
@@ -689,7 +716,12 @@ def create_parser():
         default=[],
         help='Send unrecognized args directly to kubetest')
 
-    # aws
+    # kops & aws
+    # At the moment, the logic is that `--aws` => kops & aws,
+    # and `--kops` => kops & gce.
+    # TODO(justinsb): get this using a `--provider` flag, just like kubetest
+    parser.add_argument(
+        '--kops', action='store_true', help='E2E job runs using kops')
     parser.add_argument(
         '--aws', action='store_true', help='E2E job runs in aws')
     parser.add_argument(
@@ -723,6 +755,9 @@ def create_parser():
     parser.add_argument(
         '--kops-state', default='s3://k8s-kops-jenkins/',
         help='Name of the aws state storage')
+    parser.add_argument(
+        '--kops-state-gce', default='gs://k8s-kops-gce-jenkins/',
+        help='Name of the kops state storage for GCE')
     parser.add_argument(
         '--kops-zones', help='Comma-separated list of zones else random choice')
     parser.add_argument(
