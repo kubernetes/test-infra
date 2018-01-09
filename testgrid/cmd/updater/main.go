a5bfdcbc5ed770060d5ed0b5c94a5b1916f6e4f0
Erick Fejta <fejta@google.com>
2018-01-08 21:04:02 -0800

    Read junit results

--- testgrid/cmd/updater/main.go
+++ testgrid/cmd/updater/main.go
@@ -19,8 +19,9 @@ package main
 import (
 	"context"
 	"encoding/json"
-	// "encoding/xml"
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"regexp"
@@ -47,7 +48,7 @@ type Started struct {
 }
 
 type Finished struct {
-	Timestamp  int      `json:"timestamp"` // python time.time()
+	Timestamp  int64    `json:"timestamp"` // python time.time()
 	Passed     bool     `json:"passed"`
 	JobVersion string   `json:"job-version"`
 	Metadata   Metadata `json:"metadata"`
@@ -59,9 +60,26 @@ type Metadata struct {
 	InfraCommit string `json:"infra-commit"`
 }
 
+type JunitSuite struct {
+	XMLName  xml.Name      `xml:"testsuite"`
+	Time     float64       `xml:"time,attr"`
+	Failures int           `xml:"failures,attr"`
+	Tests    int           `xml:"tests,attr"`
+	Results  []JunitResult `xml:"testcase"`
+}
+
+type JunitResult struct {
+	Name      string  `xml:"name,attr"`
+	Time      float64 `xml:"time,attr"`
+	ClassName string  `xml:"classname,attr"`
+	Failure   *string `xml:"failure"`
+	Output    *string `xml:"system-out"`
+	Skipped   *string `xml:"skipped"`
+}
+
 type BuildResult struct {
-	Started  int
-	Finished int
+	Started  int64
+	Finished int64
 	Passed   bool
 	Results  map[string]state.Row_Result
 }
@@ -110,6 +128,54 @@ func ReadBuild(build Build) (*BuildResult, error) {
 			continue
 		}
 		artifacts = append(artifacts, a.Name)
+	}
+	for _, ap := range artifacts {
+		ar, err := build.Bucket.Object(ap).NewReader(build.Context)
+		if err != nil {
+			return nil, fmt.Errorf("could not read %s: %v", ap, err)
+		}
+		if r := ar.Remain(); r > 50e6 {
+			return nil, fmt.Errorf("too large: %s is %d > 50M", ap, r)
+		}
+		buf, err := ioutil.ReadAll(ar)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read all of %s: %v", ap, err)
+		}
+
+		var suite JunitSuite
+		err = xml.Unmarshal(buf, &suite)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse %s: %v", ap, err)
+		}
+		for _, s := range suite.Results {
+			if s.Skipped != nil && len(*s.Skipped) == 0 {
+				continue
+			}
+
+			n := s.Name
+			i := 0
+			if br.Results == nil {
+				br.Results = make(map[string]state.Row_Result)
+			}
+			for {
+				_, ok := br.Results[n]
+				if !ok {
+					break
+				}
+				i++
+				n = fmt.Sprintf("%s [%d]", s.Name, i)
+			}
+			switch {
+			case s.Failure != nil:
+				// TODO: extract failure reason
+				br.Results[n] = state.Row_FAIL
+			case s.Skipped != nil:
+				// TODO: extract skip reason
+				br.Results[n] = state.Row_PASS_WITH_SKIPS
+			default:
+				br.Results[n] = state.Row_PASS
+			}
+		}
 	}
 	return &br, nil
 }
@@ -161,17 +227,37 @@ func ListBuilds(client *storage.Client, ctx context.Context, path string, builds
 	return nil
 }
 
-func ReadBuilds(builds chan Build) {
+func ReadBuilds(builds chan Build, max int, dur time.Duration) {
 	log.Println("Reading builds...")
+	i := 0
+	var stop time.Time
+	if dur != 0 {
+		stop = time.Now().Add(-dur)
+	}
 	for b := range builds {
+		i++
+		if max > 0 && i > max {
+			log.Printf("Hit ceiling of %d results", max)
+			break
+		}
 		br, err := ReadBuild(b)
 		if err != nil {
 			log.Printf("FAIL %s: %v", b.Prefix, err)
 			continue
 		}
-		log.Printf("found! %v", br)
+		log.Printf("found! %+v", br)
+		if br.Started < stop.Unix() {
+			log.Printf("Latest result before %s", stop)
+			break
+		}
 	}
 	log.Println("Finished reading builds.")
+	for _ = range builds {
+	}
+}
+
+func Days(d int) time.Duration {
+	return 24 * time.Duration(d) * time.Hour // Close enough
 }
 
 func main() {
@@ -190,10 +276,11 @@ func main() {
 	g.Columns = append(g.Columns, &state.Column{Build: "first", Started: 1})
 	fmt.Println(g)
 	builds := make(chan Build)
-	go ReadBuilds(builds)
+	go ReadBuilds(builds, 10, Days(30))
 	if err = ListBuilds(client, ctx, "gs://kubernetes-jenkins/pr-logs/pull-ingress-gce-e2e", builds); err != nil {
 		log.Fatalf("Failed to list builds: %v", err)
 	}
 	close(builds)
+	log.Println("Sleep")
 	time.Sleep(10 * time.Second)
 }
