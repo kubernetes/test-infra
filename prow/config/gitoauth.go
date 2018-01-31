87395f06eb400d3a4f793363b93023ba8b1b7aa9
Quang Huynh <quanghuynh@google.com>
2018-01-30 16:00:07 -0800

    resolve lost stash

---
+++ prow/config/gitoauth.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
+type GitAuthConfig struct {
+	ClientId string `json:"client_id,omitempty"`
+	ClientSecret string `json:"client_secret,omitempty"`
+
+	RedirectURL string `json:"redirect_url,omitempty"`
+	Scope []string `json:"scope,omitempty"`
+
+	GitTokenSession string `json:"token_session,omitempty"`
+	GitTokenKey string `json:"token_key,omitempty"`
+
+	OAuthClient *oauth2.Config
+}
+
+func (gac *GitAuthConfig) ConfigureOAuthClient() {
+	gac.OAuthClient = &oauth2.Config{
+		ClientID: gac.ClientId,
+		ClientSecret: gac.ClientSecret,
+		RedirectURL: gac.RedirectURL,
+		Scopes: gac.Scope,
+		Endpoint: github.Endpoint,
+	}
+}
+
+
--- prow/userdashboard/userdashboard.go
+++ prow/userdashboard/userdashboard.go
@@ -17,28 +17,143 @@ limitations under the License.
 package userdashboard
 
 import (
+	"github.com/sirupsen/logrus"
 	"net/http"
+	"k8s.io/test-infra/prow/config"
+	"github.com/gorilla/sessions"
+	"golang.org/x/oauth2"
+	"context"
+	"k8s.io/test-infra/prow/github"
+	"github.com/shurcooL/githubql"
+	"encoding/json"
 	"fmt"
-	"k8s.io/test-infra/testgrid/config/pb"
 )
 
-type UserData struct {
+const githubEndpoint = "https://api.github.com"
 
+
+type githubClient interface {
+	Query(context.Context, interface{}, map[string]interface{}) error
+}
+
+type UserData struct {
+	Login bool
+	PullRequests []PullRequest
 }
 
 type DashboardAgent struct {
-	goaa *GitOAuthAgent
+	cookieStore *sessions.CookieStore
+	gitConfig *config.GitAuthConfig
+
+	log *logrus.Entry
 }
 
-func (da *DashboardAgent) NewDashboardAgent() (*DashboardAgent) {
-	return nil
+type Label struct {
+	Id githubql.ID
+	name githubql.String
 }
 
-func (da *DashboardAgent) generateUserData() (*UserData) {
-	da.goaa.PullRequest()
+type PullRequest struct{
+	Number githubql.Int
+	Author struct {
+		Login githubql.String
+	}
+	Repository struct {
+		Name          githubql.String
+		NameWithOwner githubql.String
+		Owner         struct {
+			Login githubql.String
+		}
+	}
+	Labels struct {
+		Nodes []struct {
+			Label Label
+		}
+	} `graphql:"labels(first: 100)"`
+}
+
+type PullRequestQuery struct {
+	User struct {
+		PullRequests struct {
+			PageInfo struct {
+				HasNextPage githubql.Boolean
+				EndCursor githubql.String
+			}
+			Nodes []struct {
+				PullRequest PullRequest
+			}
+		} `graphql:"pullRequests(first: 100, after: $prsCursor, state: $prsStates)"`
+	}
+}
+
+func NewDashboardAgent(cookieStore *sessions.CookieStore, config *config.GitAuthConfig, log *logrus.Entry) *DashboardAgent {
+	return &DashboardAgent{
+		gitConfig: config,
+		cookieStore: cookieStore,
+		log:  log,
+	}
 }
 
 func (da *DashboardAgent) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Cache-Control", "no-cache")
+	serverError := func(action string, err error) {
+		da.log.WithError(err).Errorf("Error %s.", action)
+		msg := fmt.Sprintf("500 Internal server error %s: %v", action, err)
+		http.Error(w, msg, http.StatusInternalServerError)
+	}
 
+	session, err := da.cookieStore.Get(r, da.gitConfig.GitTokenSession)
+	if err != nil {
+		serverError("Error with getting git token session.", err)
+		return
+	}
+	token, ok := session.Values[da.gitConfig.GitTokenKey].(*oauth2.Token)
+	data := UserData{}
+
+	if !ok || !token.Valid() {
+		data.Login = false
+	} else {
+		data.Login = true
+		ghc := github.NewClient(token.AccessToken, githubEndpoint)
+		pullRequests, err := search(context.Background(), ghc)
+		if err != nil {
+			serverError("Error with querying user data.", err)
+			return
+		} else {
+			data.PullRequests = pullRequests
+		}
+	}
+
+	marshaledData, err := json.Marshal(data)
+	if err != nil {
+		da.log.WithError(err).Error("Error with marshalling user data.")
+	}
+
+	w.Write(marshaledData)
 }
+
+func search(ctx context.Context, ghc githubClient) ([]PullRequest, error){
+	var prs = []PullRequest{}
+	vars := map[string]interface{}{
+		"$prsStates":         githubql.PullRequestStateOpen,
+		"$prsCursor": 				(*githubql.String)(nil),
+	}
+	for {
+		pq := PullRequestQuery{}
+		if err := ghc.Query(ctx, &pq, vars); err != nil {
+			return nil, err
+		}
+		for _, n := range pq.User.PullRequests.Nodes {
+			prs = append(prs, n.PullRequest)
+		}
+		if !pq.User.PullRequests.PageInfo.HasNextPage {
+			break
+		}
+		vars["prsCursor"] = githubql.NewString(pq.User.PullRequests.PageInfo.EndCursor)
+	}
+
+	return prs, nil
+}
+
+
+
+
