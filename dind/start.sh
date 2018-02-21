533d712a270523114de7d81f028f0a1922fe5cd1
Quintin Lee <qlee@google.com>
2018-02-20 21:05:21 -0800

    Correctly start the metrics-server addon

--- dind/start.sh
+++ dind/start.sh
@@ -83,7 +83,7 @@ start_master ()
   kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f http://docs.projectcalico.org/v2.4/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
 
   # Install the metrics server, and the HPA.
-  kubectl --kubeconfig=/var/kubernetes/admin.conf apply -f /cluster/addons/metrics-server/
+  kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f /addons/metrics-server/
 }
 
 start_cluster ()
