41ffd098dc31dced7dfb188a83dc6fbdf8f9bd4a
Jeff Grafton <jgrafton@google.com>
2018-02-26 12:14:50 -0800

    Update bazelbuild/rules_go and gazelle to latest

--- hack/update-bazel.sh
+++ hack/update-bazel.sh
@@ -34,7 +34,7 @@ rm -f ${TESTINFRA_ROOT}/vendor/k8s.io/apimachinery/pkg/util/sets/{BUILD,BUILD.ba
 
 "${TESTINFRA_ROOT}/hack/go_install_from_commit.sh" \
   github.com/bazelbuild/bazel-gazelle/cmd/gazelle \
-  eaa1e87d2a3ca716780ca6650ef5b9b9663b8773 \
+  a85b63b06c2e0c75931e57c4a1a18d4e566bb6f4 \
   "${TMP_GOPATH}"
 
 touch "${TESTINFRA_ROOT}/vendor/BUILD.bazel"
