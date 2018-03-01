8b9f83c43632e9abbdb6d6bc80ee3e924fd6ede2
Quintin Lee <qlee@google.com>
2018-02-28 23:39:57 -0800

    Start docker with systemd. Allow dind as well as dindind.

--- third_party/moby/dockerd-entrypoint.sh
+++ third_party/moby/dockerd-entrypoint.sh
@@ -9,7 +9,6 @@ if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
     --host=unix:///var/run/docker.sock \
     --host=tcp://0.0.0.0:2375 \
     --storage-driver=overlay \
-    --bip=10.0.0.1/8 \
     "$@"
 fi
 
