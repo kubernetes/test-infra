79410026fdb4c77dd6b187bcf99ba8c5243e5a8c
Steve Kuznetsov <skuznets@redhat.com>
2018-04-09 10:52:41 -0700

    Introduce a tool to create a Pod from a ProwJob
    When testing the behavior of `plank`, it is very useful to be able to
    run only the logic that transforms a `ProwJob` to a `Pod`. The `mkpod`
    tool allows for this; in concert with the `mkpj` tool, we now are able
    to create a `Pod` for a job in the Prow configuration without running
    any part of the larger system.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

---
+++ prow/cmd/mkpod/main.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/ghodss/yaml"
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
+
+	"k8s.io/test-infra/prow/kube"
+	"k8s.io/test-infra/prow/pod-utils/decorate"
+)
+
+type options struct {
+	prowJobPath string
+	buildId     string
+}
+
+func (o *options) Validate() error {
+	if o.prowJobPath == "" {
+		return errors.New("required flag --prow-job was unset")
+	}
+
+	return nil
+}
+
+func gatherOptions() options {
+	o := options{}
+	flag.StringVar(&o.prowJobPath, "prow-job", "", "ProwJob to decorate, - for stdin.")
+	flag.StringVar(&o.buildId, "build-id", "", "Build ID for the job run.")
+	flag.Parse()
+	return o
+}
+
+func main() {
+	o := gatherOptions()
+	if err := o.Validate(); err != nil {
+		logrus.Fatalf("Invalid options: %v", err)
+	}
+
+	var rawJob []byte
+	if o.prowJobPath == "-" {
+		raw, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			logrus.WithError(err).Fatal("Could not read ProwJob YAML from stdin.")
+		}
+		rawJob = raw
+	} else {
+		raw, err := ioutil.ReadFile(o.prowJobPath)
+		if err != nil {
+			logrus.WithError(err).Fatal("Could not open ProwJob YAML.")
+		}
+		rawJob = raw
+	}
+
+	var job kube.ProwJob
+	if err := yaml.Unmarshal(rawJob, &job); err != nil {
+		logrus.WithError(err).Fatal("Could not unmarshal ProwJob YAML.")
+	}
+
+	if o.buildId == "" && job.Status.BuildID != "" {
+		o.buildId = job.Status.BuildID
+	}
+
+	if o.buildId == "" {
+		logrus.Warning("No BuildID found in ProwJob status or given with --build-id, GCS interaction will be poor.")
+	}
+
+	pod, err := decorate.ProwJobToPod(job, o.buildId)
+	if err != nil {
+		logrus.WithError(err).Fatal("Could not decorate PodSpec.")
+	}
+
+	pod.GetObjectKind().SetGroupVersionKind(v1.SchemeGroupVersion.WithKind("Pod"))
+	podYAML, err := yaml.Marshal(pod)
+	if err != nil {
+		logrus.WithError(err).Fatal("Could not marshal Pod YAML.")
+	}
+	fmt.Println(string(podYAML))
+}
