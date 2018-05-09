9a6b4287d6eb7ca95d8596eb5f6484e963d75caf
Erick Fejta <erick@fejta.com>
2018-05-09 12:07:00 -0700

    Revert "Remove hack/prune-libraries.sh, instead relying solely on dep's internal pruning"

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -14,10 +14,10 @@
 # limitations under the License.
 
 
-# Run dep ensure and generate bazel rules.
+# Run dep ensure, generate bazel rules and do additional pruning.
 #
 # Usage:
-#   update-deps.sh <ARGS>
+#   update-dep.sh <ARGS>
 #
 # The args are sent to dep ensure -v <ARGS>
 
@@ -38,4 +38,6 @@ rm -rf vendor/github.com/golang/dep/internal/fs/testdata
 bazel run //:dep -- ensure -v "$@"
 rm -rf vendor/github.com/golang/dep/internal/fs/testdata
 hack/update-bazel.sh
+hack/prune-libraries.sh --fix
+hack/update-bazel.sh  # Update child :all-srcs in case parent was deleted
 echo SUCCESS
