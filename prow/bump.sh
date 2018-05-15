ff557e64c5022b81120015ece70be6679e3ab998
Ryan Hitchman <rmmh@google.com>
2018-05-14 17:24:38 -0700

    Fix shellcheck warnings in prow/bump.sh.

--- prow/bump.sh
+++ prow/bump.sh
@@ -18,23 +18,23 @@ set -o errexit
 # See https://misc.flogisoft.com/bash/tip_colors_and_formatting
 
 color-image() { # Bold magenta
-  echo -e "\x1B[1;35m${@}\x1B[0m"
+  echo -e "\\x1B[1;35m${*}\\x1B[0m"
 }
 
 color-version() { # Bold blue
-  echo -e "\x1B[1;34m${@}\x1B[0m"
+  echo -e "\\x1B[1;34m${*}\\x1B[0m"
 }
 
 color-error() { # Light red
-  echo -e "\x1B[91m${@}\x1B[0m"
+  echo -e "\\x1B[91m${*}\\x1B[0m"
 }
 
 color-target() { # Bold cyan
-  echo -e "\x1B[1;33m${@}\x1B[0m"
+  echo -e "\\x1B[1;33m${*}\\x1B[0m"
 }
 
 # darwin is great
-SED=sed
+SED="sed"
 if which gsed &>/dev/null; then
   SED=gsed
 fi
@@ -44,7 +44,7 @@ if ! ($SED --version 2>&1 | grep -q GNU); then
 fi
 
 new_version="v$(date -u '+%Y%m%d')-$(git describe --tags --always --dirty)"
-echo -e "version: $(color-version ${new_version})" >&2
+echo -e "version: $(color-version "${new_version}")" >&2
 if [[ "${new_version}" == *-dirty ]]; then
   echo -e "$(color-error ERROR): uncommitted changes to repo" >&2
   echo "  Fix with git commit" >&2
@@ -58,19 +58,19 @@ echo -n "images: " >&2
 images=("$@")
 if [[ "${#images[@]}" == 0 ]]; then
   echo "querying bazel for $(color-target :image) targets under $(color-target //prow/...) ..." >&2
-  images=($(bazel query 'filter(".*:image", //prow/...)' | cut -d : -f 1 | xargs -n 1 basename))
+  mapfile -t images < <(bazel query 'filter(".*:image", //prow/...)' | cut -d : -f 1 | xargs -n 1 basename)
   echo -n "images: " >&2
 fi
-echo -e "$(color-image ${images[@]})" >&2
+echo -e "$(color-image "${images[@]}")" >&2
 
-echo -e "Pushing $(color-version ${new_version}) via $(color-target //prow:release-push --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64) ..." >&2
+echo -e "Pushing $(color-version "${new_version}") via $(color-target //prow:release-push --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64) ..." >&2
 bazel run //prow:release-push --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64
 
-echo -e "Bumping: $(color-image ${images[@]}) to $(color-version ${new_version}) ..." >&2
+echo -e "Bumping: $(color-image "${images[@]}") to $(color-version "${new_version}") ..." >&2
 
 for i in "${images[@]}"; do
-  echo -e "  $(color-image $i): $(color-version $new_version)" >&2
-  $SED -i "s/\(${i}:\)v[a-f0-9-]\+/\1${new_version}/I" cluster/*.yaml
+  echo -e "  $(color-image "$i"): $(color-version "$new_version")" >&2
+  $SED -i "s/\\(${i}:\\)v[a-f0-9-]\\+/\\1${new_version}/I" cluster/*.yaml
 done
 
 echo "Deploy with:" >&2
