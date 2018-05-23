cb293073281961644490e78959951932886e39c7
Chris O'Haver <cohaver@infoblox.com>
2018-05-23 12:40:27 -0400

    add prow job and permit duplicate env

--- jobs/config_test.py
+++ jobs/config_test.py
@@ -534,6 +534,7 @@ class JobTest(unittest.TestCase):
             'pull-kubernetes-e2e-gce-100-performance': 'ci-kubernetes-scale-*',
             'pull-kubernetes-e2e-gce-big-performance': 'ci-kubernetes-scale-*',
             'pull-kubernetes-e2e-gce-large-performance': 'ci-kubernetes-scale-*',
+            'pull-kubernetes-e2e-gce-large-performance-coredns': 'ci-kubernetes-scale-*',
             'ci-kubernetes-e2e-gce-large-manual-up': 'ci-kubernetes-scale-*',
             'ci-kubernetes-e2e-gce-large-manual-down': 'ci-kubernetes-scale-*',
             'ci-kubernetes-e2e-gce-large-correctness': 'ci-kubernetes-scale-*',
