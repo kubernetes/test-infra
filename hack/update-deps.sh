0a958e1c93dcd342610e849bc34969f944fbb3c3
Benjamin Elder <bentheelder@google.com>
2018-08-08 09:00:35 -0700

    remove docker/docker/contrib in hack/update-deps.sh

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -37,8 +37,13 @@ trap 'echo "FAILED" >&2' ERR
 rm -rf vendor/github.com/golang/dep/internal/fs/testdata
 # go-bindata does too, and is not maintained ...
 rm -rf vendor/github.com/jteeuwen/go-bindata/testdata
+# docker has a contrib dir with nothing we use in it, dep will retain the licenses
+# which includes some GPL, so we manually prune this. 
+# See https://github.com/kubernetes/steering/issues/57
+rm -rf vendor/github.com/docker/docker/contrib
 bazel run //:dep -- ensure -v "$@"
 rm -rf vendor/github.com/golang/dep/internal/fs/testdata
 rm -rf vendor/github.com/jteeuwen/go-bindata/testdata
+rm -rf vendor/github.com/docker/docker/contrib
 hack/update-bazel.sh
 echo SUCCESS
