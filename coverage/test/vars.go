7ad365a6eddef4eee5f5c4dac6a62053b09204a4
steuhs <36011612+steuhs@users.noreply.github.com>
2018-08-21 17:34:24 -0700

    Remove unused vars & functions

--- coverage/test/vars.go
+++ coverage/test/vars.go
@@ -2,14 +2,12 @@ package test
 
 const (
 	ProfileName      = "cov-profile.txt"
-	KeyProfileName   = "key-cov-profile.txt"
 	StdoutName       = "stdout.txt"
 	CovTargetRootRel = "testTarget"
 	CovTargetRelPath = CovTargetRootRel + "/presubmit"
 )
 
 var (
-	//ArtifactsDir      = AbsPath("test_output/artifacts")
 	tmpArtsDir        = AbsPath("test_output/tmp_artifacts")
 	InputArtifactsDir = AbsPath("testdata/artifacts")
 	CovTargetDir      = AbsPath(CovTargetRelPath) + "/"
