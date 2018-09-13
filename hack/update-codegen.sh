7637489d4b1185630706bd79eae6c93b039177f7
Benjamin Elder <bentheelder@google.com>
2018-09-12 21:13:15 -0700

    fix update-codegen.sh lints

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -17,8 +17,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,defaulter,client,lister,informer" \
+SCRIPT_ROOT="$(dirname "${BASH_SOURCE[0]}")/.."
+CODEGEN_PKG="${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}"
+"${CODEGEN_PKG}"/generate-groups.sh "deepcopy,defaulter,client,lister,informer" \
   k8s.io/test-infra/prow/client k8s.io/test-infra/prow/apis \
   prowjobs:v1
