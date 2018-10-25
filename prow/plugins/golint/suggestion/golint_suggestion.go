b6e846458527a0556d7ff41f7b0b2a9ae76e77ae
Tara Gu <tara.weiqing@gmail.com>
2018-10-25 19:04:38 -0400

    Lint code suggestions for all caps names and names with underscore

---
+++ prow/plugins/golint/suggestion/golint_suggestion.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package suggestion
+
+import (
+	"github.com/golang/lint"
+	"regexp"
+	"strings"
+)
+
+var (
+	lintNamesUnderscoreRegex = regexp.MustCompile("don't use underscores in Go names; (.*) should be (.*)")
+	lintNamesAllCapsRegex    = regexp.MustCompile("don't use ALL_CAPS in Go names; use CamelCase")
+)
+
+var lintHandlers = [...]func(lint.Problem) string{
+	fixNameUnderscore,
+	fixNameAllCaps,
+}
+
+// SuggestCodeChange returns code suggestions for a given lint.Problem
+// Returns empty string if no suggestion can be given
+func SuggestCodeChange(p lint.Problem) string {
+	var suggestion = ""
+	for _, handler := range lintHandlers {
+		suggestion = handler(p)
+		if suggestion != "" {
+			return formatSuggestion(suggestion)
+		}
+	}
+	return ""
+}
+
+func fixNameUnderscore(p lint.Problem) string {
+	matches := lintNamesUnderscoreRegex.FindStringSubmatch(p.Text)
+	if len(matches) < 3 {
+		return ""
+	}
+	underscoreRe := regexp.MustCompile(`[A-Za-z]+(_[A-Za-z0-9]+)+`)
+	namesWithUnderscore := underscoreRe.FindStringSubmatch(matches[1])
+	suggestion := strings.Replace(p.LineText, namesWithUnderscore[0], matches[2], -1)
+	if suggestion == p.LineText {
+		return ""
+	}
+	return suggestion
+}
+
+func fixNameAllCaps(p lint.Problem) string {
+	result := ""
+	matches := lintNamesAllCapsRegex.FindStringSubmatch(p.Text)
+	if len(matches) == 0 {
+		return result
+	}
+	// Identify all caps names
+	reAllCaps := regexp.MustCompile(`[A-Z]+(_[A-Z0-9]+)*`)
+	result = reAllCaps.ReplaceAllStringFunc(p.LineText, func(oldName string) string {
+		return strings.Replace(strings.Title(strings.Replace(strings.ToLower(oldName), "_", " ", -1)), " ", "", -1)
+	})
+	if result == p.LineText {
+		return ""
+	}
+	return result
+}
+
+func formatSuggestion(s string) string {
+	return "```suggestion\n" + s + "```\n"
+}
---
+++ prow/plugins/golint/suggestion/golint_suggestion_test.go
@@ -0,0 +1,169 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package suggestion
+
+import (
+	"go/token"
+	"testing"
+
+	"github.com/golang/lint"
+)
+
+func TestLintNamesUnderscore(t *testing.T) {
+	var testcases = []struct {
+		problem            lint.Problem
+		expectedSuggestion string
+	}{
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use underscores in Go names; func Qux_1 should be Qux1",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func Qux_1() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "```suggestion\nfunc Qux1() error {```\n",
+		},
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use underscores in Go names; func Qux_Foo_Func should be QuxFooFunc",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func Qux_Foo_Func() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "```suggestion\nfunc QuxFooFunc() error {```\n",
+		},
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use underscores in Go names; func Qux_Foo_Func should be QuxFooFunc",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func QuxCorrectFunc() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "",
+		},
+	}
+	for _, test := range testcases {
+		suggestion := SuggestCodeChange(test.problem)
+		if suggestion != test.expectedSuggestion {
+			t.Errorf("Excepted code suggestion %s but got %s for LineText %s", test.expectedSuggestion, suggestion, test.problem.LineText)
+		}
+	}
+}
+
+func TestLintNamesAllCaps(t *testing.T) {
+	var testcases = []struct {
+		problem            lint.Problem
+		expectedSuggestion string
+	}{
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use ALL_CAPS in Go names; use CamelCase",
+				Link:       "",
+				Category:   "naming",
+				LineText:   "func QUX_FUNC() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "```suggestion\nfunc QuxFunc() error {```\n",
+		},
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use ALL_CAPS in Go names; use CamelCase",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func QUX() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "```suggestion\nfunc Qux() error {```\n",
+		},
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use ALL_CAPS in Go names; use CamelCase",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func QUX_FOO_FUNC() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "```suggestion\nfunc QuxFooFunc() error {```\n",
+		},
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use ALL_CAPS in Go names; use CamelCase",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func QUX_FOO_FUNC_1() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "```suggestion\nfunc QuxFooFunc1() error {```\n",
+		},
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use ALL_CAPS in Go names; use CamelCase",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func QuxCorrectFunc() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "",
+		},
+		{
+			problem: lint.Problem{
+				Position: token.Position{
+					Filename: "qux.go",
+				},
+				Text:       "don't use ALL_CAPS in Go names; use CamelCase",
+				Link:       "http://golang.org/doc/effective_go.html#mixed-caps",
+				Category:   "naming",
+				LineText:   "func Qux() error {",
+				Confidence: 100.00,
+			},
+			expectedSuggestion: "",
+		},
+	}
+	for _, test := range testcases {
+		suggestion := SuggestCodeChange(test.problem)
+		if suggestion != test.expectedSuggestion {
+			t.Errorf("Excepted code suggestion %s but got %s for LineText %s", test.expectedSuggestion, suggestion, test.problem.LineText)
+		}
+	}
+}
