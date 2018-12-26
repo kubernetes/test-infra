d08db3e4b4f6085e22c703cd03ebf3976ea83dc9
alisondy <alison@alisondowdney.com>
2018-12-27 00:05:46 +1300

    Make start on testing

--- prow/plugins/mergecommitblocker/mergecommitblocker_test.go
+++ prow/plugins/mergecommitblocker/mergecommitblocker_test.go
@@ -15,3 +15,62 @@ limitations under the License.
 */
 
 package mergecommitblocker
+
+import (
+	"k8s.io/test-infra/prow/github"
+	"testing"
+)
+const (
+	helpWanted = "help-wanted"
+	mergeCommit = "do-not-merge/contains-merge-commits"
+)
+type ghc struct {
+	*testing.T
+	labels    map[github.Label]bool
+	files     map[string][]byte
+	prChanges []github.PullRequestChange
+
+	addLabelErr, removeLabelErr, getIssueLabelsErr,
+	getFileErr, getPullRequestChangesErr error
+}
+
+func testHandlePR(t * testing.T) {
+	var testcases = []struct {
+		name string
+		pullRequestEvent github.PullRequestEvent
+		commits          []github.RepositoryCommit
+		initialLabels  []string
+
+	}{
+		{
+			name: "should label with do-not-merge/contains-merge-commits when merge commits present",
+			pullRequestEvent: github.PullRequestEvent{
+				Action:      github.PullRequestActionEdited,
+				PullRequest: github.PullRequest{Number: 3, Head: github.PullRequestBranch{SHA: "sha"}},
+			},
+			commits: []github.RepositoryCommit{
+				{SHA: "sha", Commit: github.GitCommit{Message: "One Commit"}},
+				{SHA: "sha2", Commit: github.GitCommit{Message: "Two commit"}, Parents: []github.Commit{
+					{
+						Added: []string{"three.commit"},
+					},
+					{
+						Added: []string{"merge.commit"},
+					},
+				}},
+			},
+			initialLabels: []string{helpWanted},
+		},
+		{
+			name: "should not label with do-not-merge/contains-merge-commits when there are no merge commits present",
+		},
+		{
+			name: "should remove label with do-not-merge/contains-merge-commits when merge commits have been removed",
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+
+		})
+	}
+}
