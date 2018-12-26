b5065a72bb2c48f5a3e84283bdfaea36ba49a3e0
alisondy <alison@alisondowdney.com>
2018-12-27 00:52:23 +1300

    Add test to check labels removed correctly

--- prow/plugins/mergecommitblocker/mergecommitblocker_test.go
+++ prow/plugins/mergecommitblocker/mergecommitblocker_test.go
@@ -45,6 +45,7 @@ func TestHandlePR(t *testing.T) {
 		commits          []github.RepositoryCommit
 		initialLabels  []string
 		addedLabel string
+		removedLabel   string
 	}{
 		{
 			name: "should label with do-not-merge/contains-merge-commits when merge commits present",
@@ -67,10 +68,20 @@ func TestHandlePR(t *testing.T) {
 			addedLabel: fmt.Sprintf("/#3:%s", mergeCommit),
 		},
 		{
-			name: "should not label with do-not-merge/contains-merge-commits when there are no merge commits present",
+			name: "should remove label with do-not-merge/contains-merge-commits when merge commits have been removed",
+			pullRequestEvent: github.PullRequestEvent{
+				Action:      github.PullRequestActionEdited,
+				PullRequest: github.PullRequest{Number: 3, Head: github.PullRequestBranch{SHA: "sha"}},
+			},
+			commits: []github.RepositoryCommit{
+				{SHA: "sha", Commit: github.GitCommit{Message: "One Commit"}},
+				{SHA: "sha2", Commit: github.GitCommit{Message: "Two commit"}},
+			},
+			initialLabels: []string{fmt.Sprintf("/#3:%s", mergeCommit)},
+			removedLabel: fmt.Sprintf("/#3:%s", mergeCommit),
 		},
 		{
-			name: "should remove label with do-not-merge/contains-merge-commits when merge commits have been removed",
+			name: "should not label with do-not-merge/contains-merge-commits when there are no merge commits present",
 		},
 	}
 	for _, tc := range testcases {
@@ -82,6 +93,7 @@ func TestHandlePR(t *testing.T) {
 				CommitMap: map[string][]github.RepositoryCommit{
 					"/#3": tc.commits,
 				},
+				IssueLabelsExisting: tc.initialLabels,
 			}
 			if err := handlePR(fc,logrus.WithField("plugin", pluginName),tc.pullRequestEvent); err != nil {
 				t.Errorf("For case %s, didn't expect error from mergecommitblocker	 plugin: %v", tc.name, err)
@@ -97,6 +109,18 @@ func TestHandlePR(t *testing.T) {
 			}
 			if !ok {
 				t.Errorf("Expected to add: %#v, Got %#v in case %s.", tc.addedLabel, fc.IssueLabelsAdded, tc.name)
+			}
+			ok = tc.removedLabel == ""
+			if !ok {
+				for _, label := range fc.IssueLabelsRemoved {
+					if reflect.DeepEqual(tc.removedLabel, label) {
+						ok = true
+						break
+					}
+				}
+			}
+			if !ok {
+				t.Errorf("Expected to remove: %#v, Got %#v in case %s.", tc.removedLabel, fc.IssueLabelsRemoved, tc.name)
 			}
 		})
 	}
