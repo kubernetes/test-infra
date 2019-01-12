0540bc722176b2f88d355a63baa6efb4e00108cc
Erick Fejta <fejta@google.com>
2019-01-12 03:09:51 -0800

    More unit tests for entrypoint, sidecar

--- prow/entrypoint/run_test.go
+++ prow/entrypoint/run_test.go
@@ -32,34 +32,42 @@ func TestOptions_Run(t *testing.T) {
 	var testCases = []struct {
 		name           string
 		args           []string
+		alwaysZero     bool
+		invalidMarker  bool
+		previousMarker string
 		timeout        time.Duration
 		gracePeriod    time.Duration
 		expectedLog    string
 		expectedMarker string
+		expectedCode   int
 	}{
 		{
 			name:           "successful command",
 			args:           []string{"sh", "-c", "exit 0"},
 			expectedLog:    "",
 			expectedMarker: "0",
+			expectedCode:   0,
 		},
 		{
 			name:           "successful command with output",
 			args:           []string{"echo", "test"},
 			expectedLog:    "test\n",
 			expectedMarker: "0",
+			expectedCode:   0,
 		},
 		{
 			name:           "unsuccessful command",
 			args:           []string{"sh", "-c", "exit 12"},
 			expectedLog:    "",
 			expectedMarker: "12",
+			expectedCode:   12,
 		},
 		{
 			name:           "unsuccessful command with output",
 			args:           []string{"sh", "-c", "echo test && exit 12"},
 			expectedLog:    "test\n",
 			expectedMarker: "12",
+			expectedCode:   12,
 		},
 		{
 			name:           "command times out",
@@ -68,6 +76,7 @@ func TestOptions_Run(t *testing.T) {
 			gracePeriod:    1 * time.Second,
 			expectedLog:    "level=error msg=\"Process did not finish before 1s timeout\" \nlevel=error msg=\"Process gracefully exited before 1s grace period\" \n",
 			expectedMarker: strconv.Itoa(InternalErrorCode),
+			expectedCode:   InternalErrorCode,
 		},
 		{
 			name:           "command times out and ignores interrupt",
@@ -76,6 +85,7 @@ func TestOptions_Run(t *testing.T) {
 			gracePeriod:    1 * time.Second,
 			expectedLog:    "level=error msg=\"Process did not finish before 1s timeout\" \nlevel=error msg=\"Process did not exit before 1s grace period\" \n",
 			expectedMarker: strconv.Itoa(InternalErrorCode),
+			expectedCode:   InternalErrorCode,
 		},
 		{
 			// Ensure that environment variables get passed through
@@ -83,6 +93,47 @@ func TestOptions_Run(t *testing.T) {
 			args:           []string{"sh", "-c", "echo $PATH"},
 			expectedLog:    os.Getenv("PATH") + "\n",
 			expectedMarker: "0",
+			expectedCode:   0,
+		},
+		{
+			name:           "failures return 0 when AlwaysZero is set",
+			alwaysZero:     true,
+			args:           []string{"sh", "-c", "exit 7"},
+			expectedMarker: "7",
+			expectedCode:   0,
+		},
+		{
+			name:           "return non-zero when writing marker fails even when AlwaysZero is set",
+			alwaysZero:     true,
+			timeout:        1 * time.Second,
+			gracePeriod:    1 * time.Second,
+			args:           []string{"echo", "test"},
+			invalidMarker:  true,
+			expectedLog:    "test\n",
+			expectedMarker: strconv.Itoa(InternalErrorCode),
+			expectedCode:   InternalErrorCode,
+		},
+		{
+			name:           "return PreviousErrorCode without running anything if previous marker failed",
+			previousMarker: "9",
+			args:           []string{"echo", "test"},
+			expectedLog:    "level=info msg=\"Skipping as previous step exited 9\" \n",
+			expectedCode:   PreviousErrorCode,
+			expectedMarker: strconv.Itoa(PreviousErrorCode),
+		},
+		{
+			name:           "run passing command as normal if previous marker passed",
+			previousMarker: "0",
+			args:           []string{"sh", "-c", "exit 0"},
+			expectedMarker: "0",
+			expectedCode:   0,
+		},
+		{
+			name:           "run failing command as normal if previous marker passed",
+			previousMarker: "0",
+			args:           []string{"sh", "-c", "exit 4"},
+			expectedMarker: "4",
+			expectedCode:   4,
 		},
 	}
 
@@ -103,6 +154,7 @@ func TestOptions_Run(t *testing.T) {
 			}()
 
 			options := Options{
+				AlwaysZero:  testCase.alwaysZero,
 				Timeout:     testCase.timeout,
 				GracePeriod: testCase.gracePeriod,
 				Options: &wrapper.Options{
@@ -112,12 +164,26 @@ func TestOptions_Run(t *testing.T) {
 				},
 			}
 
-			if code := strconv.Itoa(options.Run()); code != testCase.expectedMarker {
-				t.Errorf("%s: exit code %q does not match expected marker file contents %q", testCase.name, code, testCase.expectedMarker)
+			if testCase.previousMarker != "" {
+				p := path.Join(tmpDir, "previous-marker.txt")
+				options.PreviousMarker = p
+				if err := ioutil.WriteFile(p, []byte(testCase.previousMarker), 0600); err != nil {
+					t.Fatalf("could not create previous marker: %v", err)
+				}
+			}
+
+			if testCase.invalidMarker {
+				options.MarkerFile = "/this/had/better/not/be/a/real/file!@!#$%#$^#%&*&&*()*"
+			}
+
+			if code := options.Run(); code != testCase.expectedCode {
+				t.Errorf("%s: expected exit code %d != actual %d", testCase.name, testCase.expectedCode, code)
 			}
 
 			compareFileContents(testCase.name, options.ProcessLog, testCase.expectedLog, t)
-			compareFileContents(testCase.name, options.MarkerFile, testCase.expectedMarker, t)
+			if !testCase.invalidMarker {
+				compareFileContents(testCase.name, options.MarkerFile, testCase.expectedMarker, t)
+			}
 		})
 	}
 }
