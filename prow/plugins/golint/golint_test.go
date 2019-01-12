cd60e7b75bc18cf2bd04518ad698b12c0618127b
Tara Gu <tara.weiqing@gmail.com>
2019-01-12 13:54:17 -0500

    Create comment for lint error

--- prow/plugins/golint/golint_test.go
+++ prow/plugins/golint/golint_test.go
@@ -425,6 +425,158 @@ func TestLintCodeSuggestion(t *testing.T) {
 	}
 }
 
+func TestLintError(t *testing.T) {
+	var testcases = []struct {
+		name       string
+		codeChange string
+		pullFiles  map[string][]byte
+		comments   []github.DraftReviewComment
+	}{
+		{
+			name: "Golint error",
+			codeChange: "@@ -0,0 +1,15 @@\n+" +
+				"/*\n+" +
+				"Package bar comment\n+" +
+				"*/\n+" +
+				"package bar\n+" +
+				"\n+" +
+				"func f(m []string) {\n+" +
+				"      for _ = range m {\n+" +
+				"      }\n+" +
+				"}\n+" +
+				"\n+" +
+				"for b() error {\n+" +
+				"return nil\n+" +
+				"}\n+",
+			pullFiles: map[string][]byte{
+				"qux.go": []byte("/*\n" +
+					"Package bar comment\n" +
+					"*/\n" +
+					"package bar\n" +
+					"\n" +
+					"func f(m []string) {\n" +
+					"      for _ = range m {\n" +
+					"      }\n" +
+					"}\n" +
+					"\n" +
+					"for b() error {\n" +
+					"return nil\n" +
+					"}\n"),
+			},
+			comments: []github.DraftReviewComment{
+				{
+					Body:     "expected declaration, found 'for'",
+					Position: 11,
+					Path:     "qux.go",
+				},
+				{
+					Body: "computing added lines in qux.go: invalid patch: " +
+						"@@ -0,0 +1,15 @@\n+" +
+						"/*\n+" +
+						"Package bar comment\n+" +
+						"*/\n+" +
+						"package bar\n+" +
+						"\n+" +
+						"func f(m []string) {\n+" +
+						"      for _ = range m {\n+" +
+						"      }\n+" +
+						"}\n+" +
+						"\n+" +
+						"for b() error {\n+" +
+						"return nil\n+" +
+						"}\n+",
+					Position: 0,
+					Path:     "qux.go",
+				},
+			},
+		},
+		{
+			name: "No golint error",
+			codeChange: "@@ -0,0 +1,9 @@\n+" +
+				"/*\n+" +
+				"Package bar comment\n+" +
+				"*/\n+" +
+				"package bar\n+" +
+				"\n+" +
+				"func f() {\n+" +
+				"      var myZeroFlt float32 = 0.\n+" +
+				"}\n+",
+			pullFiles: map[string][]byte{
+				"qux.go": []byte("/*\n" +
+					"Package bar comment\n" +
+					"*/\n" +
+					"package bar\n" +
+					"\n" +
+					"func b() error {\n" +
+					"return nil\n" +
+					"}\n"),
+			},
+			comments: nil,
+		},
+	}
+
+	lg, c, err := localgit.New()
+	if err != nil {
+		t.Fatalf("Making localgit: %v", err)
+	}
+	defer func() {
+		if err := lg.Clean(); err != nil {
+			t.Errorf("Cleaning up localgit: %v", err)
+		}
+		if err := c.Clean(); err != nil {
+			t.Errorf("Cleaning up client: %v", err)
+		}
+	}()
+	if err := lg.MakeFakeRepo("foo", "bar"); err != nil {
+		t.Fatalf("Making fake repo: %v", err)
+	}
+	if err := lg.AddCommit("foo", "bar", initialFiles); err != nil {
+		t.Fatalf("Adding initial commit: %v", err)
+	}
+	if err := lg.CheckoutNewBranch("foo", "bar", "pull/42/head"); err != nil {
+		t.Fatalf("Checking out pull branch: %v", err)
+	}
+
+	for _, test := range testcases {
+		t.Logf("Running test case %q...", test.name)
+		if err := lg.AddCommit("foo", "bar", test.pullFiles); err != nil {
+			t.Fatalf("Adding PR commit: %v", err)
+		}
+		gh := &ghc{
+			changes: []github.PullRequestChange{
+				{
+					Filename: "qux.go",
+					Patch:    test.codeChange,
+				},
+			},
+		}
+		if err := handle(0, gh, c, logrus.NewEntry(logrus.New()), e); err != nil {
+			t.Fatalf("Got error from handle: %v", err)
+		}
+
+		if test.comments == nil {
+			if len(gh.comment.Comments) > 0 {
+				t.Fatalf("Expected no comment, got %d: %v.", len(gh.comment.Comments), gh.comment.Comments)
+			}
+		} else {
+			if len(gh.comment.Comments) != len(test.comments) {
+				t.Fatalf("Expected one comments, got %d: %v.", len(gh.comment.Comments), gh.comment.Comments)
+			}
+			for _, testComment := range test.comments {
+				exists := false
+				for _, actualComment := range gh.comment.Comments {
+					if actualComment == testComment {
+						exists = true
+					}
+				}
+				if !exists {
+					t.Fatalf("Did not get back expected comment %v, actual comments are %v", testComment, gh.comment.Comments)
+				}
+			}
+		}
+	}
+}
+
 func TestAddedLines(t *testing.T) {
 	var testcases = []struct {
 		patch string
