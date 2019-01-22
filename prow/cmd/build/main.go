d163c2f1ff3c034e58a2c9d947b3fe2c3ea01ce4
Steve Kuznetsov <skuznets@redhat.com>
2019-01-22 11:44:12 -0800

    Use generated Pod client from .kube/config in sinker
    The custom `Cluster` file containing a subset of auth methods (only
    certificate auth) is legacy and deprecated in this commit for `sinker`.
    A proper `.kube/config` file should be used and all contexts present in
    that file will be considered build cluster contexts that should be
    reaped by the tool. The `Cluster` file will continue to be supported for
    backwards compatibility until further notice.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- prow/cmd/build/main.go
+++ prow/cmd/build/main.go
@@ -19,8 +19,6 @@ package main
 import (
 	"errors"
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
@@ -39,11 +37,8 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
-
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // support gcp users in .kube/config
+	"k8s.io/client-go/rest"
 )
 
 type options struct {
@@ -105,100 +100,6 @@ func stopper() chan struct{} {
 	return stop
 }
 
-// contextConfigs returns a context => config mapping as well as the default context.
-//
-// Returns an error if kubeconfig is specified and invalid
-// Returns an error if no contexts are found.
-func contextConfigs(kubeconfig, buildCluster string) (map[string]rest.Config, string, error) {
-	logrus.Infof("Loading cluster contexts...")
-	configs := map[string]rest.Config{}
-	var defCtx *string
-	// This will work if we are running inside kubernetes
-	if localCfg, err := rest.InClusterConfig(); err != nil {
-		logrus.Warnf("Failed to create in-cluster config: %v", err)
-	} else {
-		defCtx = new(string)
-		logrus.Info("* in-cluster")
-		configs[*defCtx] = *localCfg
-	}
-
-	// Attempt to load external clusters too
-	var loader clientcmd.ClientConfigLoader
-	if kubeconfig != "" { // load from --kubeconfig
-		loader = &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig}
-	} else {
-		loader = clientcmd.NewDefaultClientConfigLoadingRules()
-	}
-
-	cfg, err := loader.Load()
-	switch {
-	case err != nil && kubeconfig != "":
-		return nil, "", fmt.Errorf("load %s kubecfg: %v", kubeconfig, err)
-	case err != nil:
-		logrus.Warnf("failed to load any kubecfg files: %v", err)
-	default:
-		// normally defCtx is in cluster (""), but we may be a dev running on their workstation
-		// in which case rest.InClusterConfig() will fail, so use the current context as default
-		// (which is where we look for prowjobs)
-		if defCtx == nil && cfg.CurrentContext != "" {
-			defCtx = &cfg.CurrentContext
-		}
-
-		for context := range cfg.Contexts {
-			logrus.Infof("* %s", context)
-			contextCfg, err := clientcmd.NewNonInteractiveClientConfig(*cfg, context, &clientcmd.ConfigOverrides{}, loader).ClientConfig()
-			if err != nil {
-				return nil, "", fmt.Errorf("create %s client: %v", context, err)
-			}
-			configs[context] = *contextCfg
-		}
-	}
-
-	if buildCluster != "" { // load from --build-cluster
-		data, err := ioutil.ReadFile(buildCluster)
-		if err != nil {
-			return nil, "", fmt.Errorf("read build clusters: %v", err)
-		}
-		raw, err := kube.UnmarshalClusterMap(data)
-		if err != nil {
-			return nil, "", fmt.Errorf("unmarshal build clusters: %v", err)
-		}
-		cfg = &clientcmdapi.Config{
-			Clusters:  map[string]*clientcmdapi.Cluster{},
-			AuthInfos: map[string]*clientcmdapi.AuthInfo{},
-			Contexts:  map[string]*clientcmdapi.Context{},
-		}
-		for alias, config := range raw {
-			cfg.Clusters[alias] = &clientcmdapi.Cluster{
-				Server:                   config.Endpoint,
-				CertificateAuthorityData: config.ClusterCACertificate,
-			}
-			cfg.AuthInfos[alias] = &clientcmdapi.AuthInfo{
-				ClientCertificateData: config.ClientCertificate,
-				ClientKeyData:         config.ClientKey,
-			}
-			cfg.Contexts[alias] = &clientcmdapi.Context{
-				Cluster:  alias,
-				AuthInfo: alias,
-				// TODO(fejta): Namespace?
-			}
-		}
-		for context := range cfg.Contexts {
-			logrus.Infof("* %s", context)
-			contextCfg, err := clientcmd.NewNonInteractiveClientConfig(*cfg, context, &clientcmd.ConfigOverrides{}, nil).ClientConfig()
-			if err != nil {
-				return nil, "", fmt.Errorf("create %s client: %v", context, err)
-			}
-			configs[context] = *contextCfg
-		}
-	}
-
-	if len(configs) == 0 {
-		return nil, "", errors.New("no clients found")
-	}
-	return configs, *defCtx, nil
-}
-
 type buildConfig struct {
 	client   buildset.Interface
 	informer buildinfov1alpha1.BuildInformer
@@ -240,7 +141,7 @@ func main() {
 		pjNamespace = pc.ProwJobNamespace
 	}
 
-	configs, defaultContext, err := contextConfigs(o.kubeconfig, o.buildCluster)
+	configs, defaultContext, err := kube.LoadClusterConfigs(o.kubeconfig, o.buildCluster)
 	if err != nil {
 		logrus.WithError(err).Fatal("Error building client configs")
 	}
