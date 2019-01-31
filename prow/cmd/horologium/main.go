0097f94347b00c7cdbed0b2b6b25a42c3d6de2c5
Steve Kuznetsov <skuznets@redhat.com>
2019-01-31 10:45:50 -0800

    Migrate horologium to use new k8s clients
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- prow/cmd/horologium/main.go
+++ prow/cmd/horologium/main.go
@@ -19,14 +19,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 
 	prowapi "k8s.io/test-infra/prow/apis/prowjobs/v1"
 	"k8s.io/test-infra/prow/config"
 	"k8s.io/test-infra/prow/cron"
-	"k8s.io/test-infra/prow/kube"
+	"k8s.io/test-infra/prow/flagutil"
 	"k8s.io/test-infra/prow/logrusutil"
 	"k8s.io/test-infra/prow/pjutil"
 )
@@ -34,14 +37,31 @@ import (
 type options struct {
 	configPath    string
 	jobConfigPath string
+
+	kubernetes flagutil.KubernetesOptions
+	dryRun     bool
 }
 
 func gatherOptions() options {
 	o := options{}
-	flag.StringVar(&o.configPath, "config-path", "/etc/config/config.yaml", "Path to config.yaml.")
-	flag.StringVar(&o.jobConfigPath, "job-config-path", "", "Path to prow job configs.")
-	flag.Parse()
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	fs.StringVar(&o.configPath, "config-path", "/etc/config/config.yaml", "Path to config.yaml.")
+	fs.StringVar(&o.jobConfigPath, "job-config-path", "", "Path to prow job configs.")
+
+	fs.BoolVar(&o.dryRun, "dry-run", true, "Whether or not to make mutating API calls to GitHub.")
+	o.kubernetes.AddFlags(fs)
+
+	fs.Parse(os.Args[1:])
 	return o
+}
+
+func (o *options) Validate() error {
+	if err := o.kubernetes.Validate(o.dryRun); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
@@ -55,9 +75,9 @@ func main() {
 		logrus.WithError(err).Fatal("Error starting config agent.")
 	}
 
-	kc, err := kube.NewClientInCluster(configAgent.Config().ProwJobNamespace)
+	prowJobClient, err := o.kubernetes.ProwJobClient(configAgent.Config().ProwJobNamespace, o.dryRun)
 	if err != nil {
-		logrus.WithError(err).Fatal("Error getting kube client.")
+		logrus.WithError(err).Fatal("Error getting Kubernetes client.")
 	}
 
 	// start a cron
@@ -66,16 +86,16 @@ func main() {
 
 	for now := range time.Tick(1 * time.Minute) {
 		start := time.Now()
-		if err := sync(kc, configAgent.Config(), cr, now); err != nil {
+		if err := sync(prowJobClient, configAgent.Config(), cr, now); err != nil {
 			logrus.WithError(err).Error("Error syncing periodic jobs.")
 		}
 		logrus.Infof("Sync time: %v", time.Since(start))
 	}
 }
 
-type kubeClient interface {
-	ListProwJobs(string) ([]prowapi.ProwJob, error)
-	CreateProwJob(prowapi.ProwJob) (prowapi.ProwJob, error)
+type prowJobClient interface {
+	Create(*prowapi.ProwJob) (*prowapi.ProwJob, error)
+	List(opts metav1.ListOptions) (*prowapi.ProwJobList, error)
 }
 
 type cronClient interface {
@@ -83,12 +103,12 @@ type cronClient interface {
 	QueuedJobs() []string
 }
 
-func sync(kc kubeClient, cfg *config.Config, cr cronClient, now time.Time) error {
-	jobs, err := kc.ListProwJobs(kube.EmptySelector)
+func sync(prowJobClient prowJobClient, cfg *config.Config, cr cronClient, now time.Time) error {
+	jobs, err := prowJobClient.List(metav1.ListOptions{LabelSelector: labels.Everything().String()})
 	if err != nil {
 		return fmt.Errorf("error listing prow jobs: %v", err)
 	}
-	latestJobs := pjutil.GetLatestProwJobs(jobs, prowapi.PeriodicJob)
+	latestJobs := pjutil.GetLatestProwJobs(jobs.Items, prowapi.PeriodicJob)
 
 	if err := cr.SyncConfig(cfg); err != nil {
 		logrus.WithError(err).Error("Error syncing cron jobs.")
@@ -99,19 +119,21 @@ func sync(kc kubeClient, cfg *config.Config, cr cronClient, now time.Time) error
 		cronTriggers[job] = true
 	}
 
-	errs := []error{}
+	var errs []error
 	for _, p := range cfg.Periodics {
 		j, ok := latestJobs[p.Name]
 
 		if p.Cron == "" {
 			if !ok || (j.Complete() && now.Sub(j.Status.StartTime.Time) > p.GetInterval()) {
-				if _, err := kc.CreateProwJob(pjutil.NewProwJob(pjutil.PeriodicSpec(p), p.Labels)); err != nil {
+				prowJob := pjutil.NewProwJob(pjutil.PeriodicSpec(p), p.Labels)
+				if _, err := prowJobClient.Create(&prowJob); err != nil {
 					errs = append(errs, err)
 				}
 			}
 		} else if _, exist := cronTriggers[p.Name]; exist {
 			if !ok || j.Complete() {
-				if _, err := kc.CreateProwJob(pjutil.NewProwJob(pjutil.PeriodicSpec(p), p.Labels)); err != nil {
+				prowJob := pjutil.NewProwJob(pjutil.PeriodicSpec(p), p.Labels)
+				if _, err := prowJobClient.Create(&prowJob); err != nil {
 					errs = append(errs, err)
 				}
 			}
