f6a8330da1f9c87b2f7966038007bfff936f6f69
Benjamin Elder <bentheelder@google.com>
2019-02-19 20:54:25 -0800

    correct package comment for kubetest2-kind

--- kubetest2/kubetest2-kind/deployer/doc.go
+++ kubetest2/kubetest2-kind/deployer/doc.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package app implements the kubetest2 kind deployer
+// Package deployer implements the kubetest2 kind deployer
 package deployer
 
 // TODO(bentheelder): actually implement!
