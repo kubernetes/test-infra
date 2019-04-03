465af472cf8d80bf74787cfe18aecfc2a2f516ab
Katharine Berry <ktbry@google.com>
2019-04-02 23:45:39 -0700

    Significantly improved images/builder.

--- images/builder/main.go
+++ images/builder/main.go
@@ -1,6 +1,23 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,8 +27,16 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/google/uuid"
 	"sigs.k8s.io/yaml"
 )
+
+func runCmd(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
 
 func getVersion() (string, error) {
 	cmd := exec.Command("git", "describe", "--tags", "--always", "--dirty")
@@ -23,6 +48,11 @@ func getVersion() (string, error) {
 }
 
 func cdToRootDir() error {
+	if bazelWorkspace := os.Getenv("BUILD_WORKSPACE_DIRECTORY"); bazelWorkspace != "" {
+		if err := os.Chdir(bazelWorkspace); err != nil {
+			return fmt.Errorf("failed to chdir to bazel workspace (%s): %v", bazelWorkspace, err)
+		}
+	}
 	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
 	output, err := cmd.Output()
 	if err != nil {
@@ -39,82 +69,159 @@ func uploadWorkingDir(targetBucket string) (string, error) {
 	name := f.Name()
 	_ = f.Close()
 	defer os.Remove(name)
-	cmd := exec.Command("tar", "--exclude", ".git", "-czf", name, ".")
-	if err := cmd.Run(); err != nil {
-		return "", fmt.Errorf("failed to tar files: %v", err)
+
+	log.Printf("Creating source tarball at %s...\n", name)
+	if err := runCmd("tar", "--exclude", ".git", "-czf", name, "."); err != nil {
+		return "", fmt.Errorf("failed to tar files: %s", err)
 	}
-	uploaded := targetBucket + path.Base(name) + ".tgz"
-	cmd = exec.Command("gsutil", "cp", name, uploaded)
-	if err := cmd.Run(); err != nil {
-		return "", fmt.Errorf("failed to upload files: %v", err)
+
+	u := uuid.New()
+	uploaded := fmt.Sprintf("%s/%s.tgz", targetBucket, u.String())
+	log.Printf("Uploading %s to %s...\n", name, uploaded)
+	if err := runCmd("gsutil", "cp", name, uploaded); err != nil {
+		return "", fmt.Errorf("failed to upload files: %s", err)
 	}
+
 	return uploaded, nil
 }
 
-type variants struct {
-	Variants []map[string]string `json:"variants"`
-}
-
-func runSingleJob(image, uploaded, version string, subs map[string]string) error {
+func runSingleJob(o options, jobName, uploaded, version string, subs map[string]string) error {
 	s := make([]string, 0, len(subs)+1)
 	for k, v := range subs {
 		s = append(s, fmt.Sprintf("_%s=%s", k, v))
 	}
 	s = append(s, "_GIT_TAG="+version)
-	cmd := exec.Command("gcloud", "builds", "submit", "--config", image+"/cloudbuild.yaml", "--substitutions", strings.Join(s, ","), uploaded)
-	return cmd.Run()
+	args := []string{"builds", "submit", "--config", path.Join(o.imageDirectory, "cloudbuild.yaml"), "--substitutions", strings.Join(s, ","), "--gcs-source-staging-dir", o.sourceBucket, uploaded}
+	if o.project != "" {
+		args = append(args, "--project", o.project)
+	}
+	cmd := exec.Command("gcloud", args...)
+
+	if o.logDir != "" {
+		p := path.Join(o.logDir, jobName+".log")
+		f, err := os.Create(p)
+		if err != nil {
+			return fmt.Errorf("couldn't create %s: %v", p, err)
+		}
+		defer f.Close()
+		cmd.Stdout = f
+		cmd.Stderr = f
+	} else {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error running %s: %v", cmd.Args, err)
+	}
+
+	return nil
 }
 
-func runBuildJobs(image, uploaded string) []error {
-	tag, err := getVersion()
-	if err != nil {
-		log.Fatalf("Failed to get current tag: %v\n", err)
-	}
+type variants map[string]map[string]string
 
-	if strings.HasSuffix(tag, "-dirty") {
-		log.Fatalf("The working copy is dirty!")
-	}
-
-	content, err := ioutil.ReadFile(image + "/variants.yaml")
+func getVariants(o options) (variants, error) {
+	content, err := ioutil.ReadFile(path.Join(o.imageDirectory, "variants.yaml"))
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return []error{err}
+			return nil, fmt.Errorf("failed to load variants.yaml: %v", err)
 		}
-		return []error{runSingleJob(image, uploaded, tag, nil)}
+		return nil, nil
 	}
-	var v variants
+	v := struct {
+		Variants variants `json:"variants"`
+	}{}
 	if err := yaml.UnmarshalStrict(content, &v); err != nil {
-		return []error{fmt.Errorf("failed to read variants.yaml: %v", err)}
+		return nil, fmt.Errorf("failed to read variants.yaml: %v", err)
+	}
+	return v.Variants, nil
+}
+
+func runBuildJobs(o options, uploaded string) []error {
+	log.Println("Running build jobs...")
+	tag, err := getVersion()
+	if err != nil {
+		return []error{fmt.Errorf("failed to get current tag: %v", err)}
 	}
 
+	if !o.allowDirty && strings.HasSuffix(tag, "-dirty") {
+		return []error{fmt.Errorf("the working copy is dirty")}
+	}
+
+	vs, err := getVariants(o)
+	if err != nil {
+		return []error{err}
+	}
+	if len(vs) == 0 {
+		log.Println("No variants.yaml, starting single build job...")
+		if err := runSingleJob(o, "build", uploaded, tag, nil); err != nil {
+			return []error{err}
+		}
+		return nil
+	}
+
+	log.Printf("Found variants.yaml, starting %d build jobs...\n", len(vs))
+
 	w := sync.WaitGroup{}
-	w.Add(len(v.Variants))
+	w.Add(len(vs))
 	var errors []error
-	for _, variant := range v.Variants {
-		go func(va map[string]string) {
+	for k, v := range vs {
+		go func(job string, vc map[string]string) {
 			defer w.Done()
-			if err := runSingleJob(image, uploaded, tag, va); err != nil {
-				errors = append(errors, err)
+			log.Printf("Starting job %q...\n", job)
+			if err := runSingleJob(o, job, uploaded, tag, vc); err != nil {
+				errors = append(errors, fmt.Errorf("job %q failed: %v", job, err))
+				log.Printf("Job %q failed: %v\n", job, err)
+			} else {
+				log.Printf("Job %q completed.\n", job)
 			}
-		}(variant)
+		}(k, v)
 	}
 	w.Wait()
 	return errors
 }
 
+type options struct {
+	logDir         string
+	sourceBucket   string
+	imageDirectory string
+	project        string
+	allowDirty     bool
+}
+
+func parseFlags() options {
+	o := options{}
+	flag.StringVar(&o.logDir, "log-dir", "", "If provided, build logs will be sent to files in this directory instead of to stdout/stderr")
+	flag.StringVar(&o.sourceBucket, "source-bucket", "", "The complete GCS path for source to be uploaded to (e.g. gs://temp-bucket/source)")
+	flag.StringVar(&o.project, "project", "", "If specified, use a non-default GCP project")
+	flag.BoolVar(&o.allowDirty, "allow-dirty", false, "If true, allow pushing dirty builds")
+	flag.Parse()
+	if flag.NArg() < 1 {
+		_, _ = fmt.Fprintln(os.Stderr, "expected an image directory to be provided")
+		os.Exit(1)
+	}
+	if o.sourceBucket == "" {
+		_, _ = fmt.Fprintln(os.Stderr, "--source-bucket is mandatory")
+		os.Exit(1)
+	}
+	o.imageDirectory = flag.Arg(0)
+	return o
+}
+
 func main() {
+	o := parseFlags()
 	if err := cdToRootDir(); err != nil {
 		log.Fatalf("Failed to cd to root: %v\n", err)
 	}
 
-	dir := os.Args[1]
-	uploadedFile, err := uploadWorkingDir("gs://ktbry-prow-dev_cloudbuild/source/")
+	uploadedFile, err := uploadWorkingDir(o.sourceBucket)
 	if err != nil {
 		log.Fatalf("Failed to upload source: %v", err)
 	}
 
-	errors := runBuildJobs(dir, uploadedFile)
+	errors := runBuildJobs(o, uploadedFile)
 	if len(errors) != 0 {
 		log.Fatalf("Failed to push some images: %v", errors)
 	}
+	log.Println("Finished.")
 }
