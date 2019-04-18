207ae07e3217860460bd1b9c2be98c5709818d82
Travis Rhoden <trhoden@vmware.com>
2019-04-18 17:48:36 -0600

    Add pre/post-submit jobs for vsphere-csi-driver
    All tests are added to the vmware dashboard

--- testgrid/cmd/configurator/config_test.go
+++ testgrid/cmd/configurator/config_test.go
@@ -359,6 +359,7 @@ func TestJobsTestgridEntryMatch(t *testing.T) {
 		"kubernetes-sigs/poseidon",
 		"kubernetes-sigs/kube-batch",
 		"kubernetes-sigs/structured-merge-diff",
+		"kubernetes-sigs/vsphere-csi-driver",
 		"tensorflow/minigo",
 	}) {
 		jobs[job.Name] = false
