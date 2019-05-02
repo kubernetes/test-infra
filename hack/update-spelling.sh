7a73ee172be3a9249073dc9c05117165e3bb7a8c
Erick Fejta <fejta@google.com>
2019-05-02 02:20:27 -0700

    Make {update,verify}-spelling bazel targets using vendored misspell

--- hack/update-spelling.sh
+++ hack/update-spelling.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2019 The Kubernetes Authors.
+# Copyright 2018 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,4 +17,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-"$(dirname "$0")"/verify-spelling.sh
+if [[ -n "${BUILD_WORKSPACE_DIRECTORY:-}" ]]; then
+  echo "Updating spelling..." >&2
+elif ! command -v bazel &>/dev/null; then
+  echo "Install bazel at https://bazel.build" >&2
+  exit 1
+else
+  (
+    set -o xtrace
+    bazel run --test_output=streamed @io_k8s_test_infra//hack:update-spelling
+  )
+  exit 0
+fi
+
+find -L . -type f -not \( \
+  \( \
+    -path '*/vendor/*' \
+    -o -path '*/static/*' \
+    -o -path '*/third_party/*' \
+    -o -path '*/node_modules/*' \
+    -o -path '*/localdata/*' \
+    \) -prune \
+  \) -exec "$@" '{}' +
