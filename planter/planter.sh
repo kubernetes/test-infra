2739d2866b9c4d6c45f1281a6fc4c501bde24695
Arash Deshmeh <adeshmeh@ca.ibm.com>
2019-05-14 13:54:30 -0400

    update planter tag to 0.23.0
    Signed-off-by: Arash Deshmeh <adeshmeh@ca.ibm.com>

--- planter/planter.sh
+++ planter/planter.sh
@@ -44,7 +44,7 @@ set -o nounset
 # these can be overridden but otherwise default to the current stable image
 # used to build kubernetes from the master branch
 IMAGE_NAME="${IMAGE_NAME:-gcr.io/k8s-testimages/planter}"
-TAG="${TAG:-0.22.0}"
+TAG="${TAG:-0.23.0}"
 IMAGE=${IMAGE:-${IMAGE_NAME}:${TAG}${CROSS+-cross}}
 
 # We want to mount our bazel workspace and the bazel cache
