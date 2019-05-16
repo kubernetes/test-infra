00a2b41128042019e430af2372f1c30cb621556a
Benjamin Elder <bentheelder@google.com>
2019-05-15 17:41:51 -0700

    ensure docker config dir exists

--- images/bootstrap/runner.sh
+++ images/bootstrap/runner.sh
@@ -59,6 +59,7 @@ export DOCKER_IN_DOCKER_IPV6_ENABLED=${DOCKER_IN_DOCKER_IPV6_ENABLED:-false}
 if [[ "${DOCKER_IN_DOCKER_IPV6_ENABLED}" == "true" ]]; then
     echo "Enabling IPV6 for Docker."
     # configure the daemon with ipv6
+    mkdir -p /etc/docker/
     cat /etc/docker/daemon.json <<EOF
 {
   "ipv6": true,
