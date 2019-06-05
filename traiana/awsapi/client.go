0aa71031f4eda26676630e0549b517bcca3fb959
Or Sela <orsela1@gmail.com>
2019-06-05 09:45:21 +0300

    avoid reading aws credentials from gcs credentials file

--- traiana/awsapi/client.go
+++ traiana/awsapi/client.go
@@ -1,12 +1,11 @@
 package awsapi
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/sirupsen/logrus"
 	"os"
 	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/sirupsen/logrus"
 )
 
 type Client struct {
@@ -14,45 +13,33 @@ type Client struct {
 }
 
 func NewClient(o ClientOption) (*Client, error) {
-	config := aws.Config{}
-	var sharedConfigFiles []string
 	sharedConfigState := session.SharedConfigDisable
+	var sharedConfigFiles []string
 
-	// use the gcs CredentialsFile for AWS and assume that the config file is next to it
+	// ignore the gcs CredentialsFile, but assume that the config file is next to it and use it to get the region
 	if o.CredentialsFile != "" {
-		config.Credentials = credentials.NewSharedCredentials(o.CredentialsFile, "default")
-
-		//k create secret generic aws-secret --from-file=service-account.json --from-file=config
 		dir, _ := filepath.Split(o.CredentialsFile)
 		configFile := filepath.Join(dir, "config")
 
 		if _, err := os.Stat(configFile); err != nil {
 			logrus.Warn("Config file not found: " + configFile)
 		} else {
-			sharedConfigFiles = append(sharedConfigFiles, configFile)
 			sharedConfigState = session.SharedConfigEnable
+			sharedConfigFiles = []string{configFile}
 		}
 	}
 
-	opts := session.Options {
-		Config: config,
-		SharedConfigFiles: sharedConfigFiles,
+	sess, err := session.NewSessionWithOptions(session.Options{
 		SharedConfigState: sharedConfigState,
-	}
-
-	session, err := session.NewSessionWithOptions(opts)
+		SharedConfigFiles: sharedConfigFiles,
+	})
 
 	if err != nil {
 		// Handle Session creation error
 		return nil, err
 	}
 
-	//sess.Handlers.Send.PushFront(func(r *request.Request) {
-	// Log every request made and its payload
-	//logger.Println("Request: %s/%s, Payload: %s",
-	//	r.ClientInfo.ServiceName, r.Operation, r.Params)
-
-	return &Client{session}, err
+	return &Client{sess}, err
 }
 
 func (c *Client) Bucket(name string) *BucketHandle {
