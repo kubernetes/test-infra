01a59bf004f27b03558a6b971dd9ab7b1e297139
Kubernetes Prow Robot <k8s.ci.robot@gmail.com>
2019-07-24 18:08:41 +0000

    Update prow to v20190724-1fcd9e27e, and other images as necessary.

--- experiment/generate_tests.py
+++ experiment/generate_tests.py
@@ -45,7 +45,7 @@ PROW_CONFIG_TEMPLATE = """
       containers:
       - args:
         env:
-        image: gcr.io/k8s-testimages/kubekins-e2e:v20190719-7cdf877-master
+        image: gcr.io/k8s-testimages/kubekins-e2e:v20190723-bf9fd63-master
 """
 
 
