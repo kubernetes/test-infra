ed96c4ec59616687861bfd5475ae85237ef3f94d
Michelle <michelle192837@users.noreply.github.com>
2019-07-25 11:19:15 -0700

    Revert "Update prow to v20190725-38ed02f62, and other images as necessary."

--- experiment/generate_tests.py
+++ experiment/generate_tests.py
@@ -45,7 +45,7 @@ PROW_CONFIG_TEMPLATE = """
       containers:
       - args:
         env:
-        image: gcr.io/k8s-testimages/kubekins-e2e:v20190725-6042c97-master
+        image: gcr.io/k8s-testimages/kubekins-e2e:v20190723-bf9fd63-master
 """
 
 
