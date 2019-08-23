f86dbe27e3ff51398011d828d4cfe1eb5017bc8e
Stefan Sakalik <stefan@improbable.io>
2019-08-23 13:18:24 +0100

    ENG-760: deploy tide and repocop from k8s-test-infra repo.

--- hack/improbapublish.sh
+++ hack/improbapublish.sh
@@ -7,4 +7,4 @@
 export PROW_REPO_OVERRIDE="eu.gcr.io/windy-oxide-102215"
 export DOCKER_REPO_OVERRIDE="eu.gcr.io/windy-oxide-102215"
 
-bazel run //prow:release-push-hook --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64
+bazel run //prow:release-push-improbable --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64
