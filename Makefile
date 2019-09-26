7191aa1be55dd778ee7565cc9796550ee813e491
morvencao <morvencao@gmail.com>
2019-09-26 16:05:33 +0800

    add more checks for test-infra.

---
+++ Makefile
@@ -0,0 +1,88 @@
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+GIT_HOST = github.com/IBM
+PWD := $(shell pwd)
+BASE_DIR := $(shell basename $(PWD))
+
+# Keep an existing GOPATH, make a private one if it is undefined
+GOPATH_DEFAULT := $(PWD)/.go
+export GOPATH ?= $(GOPATH_DEFAULT)
+GOBIN_DEFAULT := $(GOPATH)/bin
+export GOBIN ?= $(GOBIN_DEFAULT)
+TESTARGS_DEFAULT := "-v"
+export TESTARGS ?= $(TESTARGS_DEFAULT)
+DEST := $(GOPATH)/src/$(GIT_HOST)/$(BASE_DIR)
+VERSION ?= $(shell git describe --exact-match 2> /dev/null || \
+                 git describe --match=$(git rev-parse --short=8 HEAD) --always --dirty --abbrev=8)
+
+# Image URL to use all building/pushing image targets
+# IMG ?= xxx
+# REGISTRY ?= quay.io/multicloudlab
+
+ifneq ("$(realpath $(DEST))", "$(realpath $(PWD))")
+	$(error Please run 'make' from $(DEST). Current directory is $(PWD))
+endif
+
+include common/Makefile.common.mk
+
+all: check test build
+
+############################################################
+# work section
+############################################################
+$(GOBIN):
+	@echo "create gobin"
+	@mkdir -p $(GOBIN)
+
+work: $(GOBIN)
+
+############################################################
+# check section
+############################################################
+check: fmt lint
+
+fmt: format-go format-protos format-python
+
+lint: lint-all
+
+############################################################
+# test section
+############################################################
+
+test:
+	@go test ${TESTARGS} ./...
+
+############################################################
+# build section
+############################################################
+
+build:
+	@go build ./...
+
+############################################################
+# images section
+############################################################
+
+# images: build build-push-images
+
+# build-push-images: config-docker
+# 	@docker build . -f Dockerfile -t $(REGISTRY)/$(IMG):$(VERSION)
+# 	@docker push $(REGISTRY)/$(IMG):$(VERSION)
+
+############################################################
+# clean section
+############################################################
+clean:
+	rm -f xxx
---
+++ common/Makefile.common.mk
@@ -0,0 +1,82 @@
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+############################################################
+# GKE section
+############################################################
+PROJECT ?= oceanic-guard-191815
+ZONE    ?= us-west1-a
+CLUSTER ?= prow
+
+activate-serviceaccount:
+ifdef GOOGLE_APPLICATION_CREDENTIALS
+	gcloud auth activate-service-account --key-file="$(GOOGLE_APPLICATION_CREDENTIALS)"
+endif
+
+get-cluster-credentials: activate-serviceaccount
+	gcloud container clusters get-credentials "$(CLUSTER)" --project="$(PROJECT)" --zone="$(ZONE)"
+
+config-docker: get-cluster-credentials
+	@common/scripts/config_docker.sh
+
+FINDFILES=find . \( -path ./.git -o -path ./.github \) -prune -o -type f
+XARGS = xargs -0 -r
+
+lint-dockerfiles:
+	@${FINDFILES} -name 'Dockerfile*' -print0 | ${XARGS} hadolint -c ./common/config/.hadolint.yml
+
+lint-scripts:
+	@${FINDFILES} -name '*.sh' -print0 | ${XARGS} shellcheck
+
+lint-yaml:
+	@${FINDFILES} \( -name '*.yml' -o -name '*.yaml' \) -print0 | ${XARGS} grep -L -e "{{" | xargs -r yamllint -c ./common/config/.yamllint.yml
+
+lint-helm:
+	@${FINDFILES} -name 'Chart.yaml' -print0 | ${XARGS} -L 1 dirname | xargs -r helm lint --strict
+
+lint-copyright-banner:
+	@${FINDFILES} \( -name '*.go' -o -name '*.cc' -o -name '*.h' -o -name '*.proto' -o -name '*.py' -o -name '*.sh' \) \( ! \( -name '*.gen.go' -o -name '*.pb.go' -o -name '*_pb2.py' \) \) -print0 |\
+		${XARGS} common/scripts/lint_copyright_banner.sh
+
+lint-go:
+	@${FINDFILES} -name '*.go' \( ! \( -name '*.gen.go' -o -name '*.pb.go' \) \) -print0 | ${XARGS} common/scripts/lint_go.sh
+
+lint-python:
+	@${FINDFILES} -name '*.py' \( ! \( -name '*_pb2.py' \) \) -print0 | ${XARGS} autopep8 --max-line-length 160 --exit-code -d
+
+lint-markdown:
+	@${FINDFILES} -name '*.md' -print0 | ${XARGS} mdl --ignore-front-matter --style common/config/mdl.rb
+	@${FINDFILES} -name '*.md' -print0 | ${XARGS} awesome_bot --skip-save-results --allow_ssl --allow-timeout --allow-dupe --allow-redirect --white-list ${MARKDOWN_LINT_WHITELIST}
+
+lint-sass:
+	@${FINDFILES} -name '*.scss' -print0 | ${XARGS} sass-lint -c common/config/sass-lint.yml --verbose
+
+lint-typescript:
+	@${FINDFILES} -name '*.ts' -print0 | ${XARGS} tslint -c common/config/tslint.json
+
+lint-protos:
+	@$(FINDFILES) -name '*.proto' -print0 | $(XARGS) -L 1 prototool lint --protoc-bin-path=/usr/bin/protoc
+
+lint-all: lint-dockerfiles lint-scripts lint-yaml lint-helm lint-copyright-banner lint-go lint-python lint-markdown lint-sass lint-typescript lint-protos
+
+format-go:
+	@${FINDFILES} -name '*.go' \( ! \( -name '*.gen.go' -o -name '*.pb.go' \) \) -print0 | ${XARGS} goimports -w -local "github.com/IBM"
+
+format-python:
+	@${FINDFILES} -name '*.py' -print0 | ${XARGS} autopep8 --max-line-length 160 --aggressive --aggressive -i
+
+format-protos:
+	@$(FINDFILES) -name '*.proto' -print0 | $(XARGS) -L 1 prototool format -w
+
+.PHONY: lint-dockerfiles lint-scripts lint-yaml lint-copyright-banner lint-go lint-python lint-helm lint-markdown lint-sass lint-typescript lint-protos lint-all format-go format-python format-protos config-docker
