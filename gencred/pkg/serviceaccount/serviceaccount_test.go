7defa281ed481ba08553823212b1fa8a72bd4fc0
Travis Clarke <clarketm@google.com>
2019-10-17 22:18:30 -0700

    Add "gencred" support for generating kubeconfig

--- gencred/pkg/serviceaccount/serviceaccount_test.go
+++ gencred/pkg/serviceaccount/serviceaccount_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package gencred
+package serviceaccount
 
 import (
 	"testing"
@@ -24,47 +24,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	k8sFake "k8s.io/client-go/kubernetes/fake"
 )
-
-func TestCreateClusterCertificateCredentials(t *testing.T) {
-	tests := []struct {
-		name         string
-		createClient func() kubernetes.Interface
-		expected     bool
-	}{
-		{
-			name: "create cluster certificate success",
-			createClient: func() kubernetes.Interface {
-				return k8sFake.NewSimpleClientset(&corev1.Secret{
-					TypeMeta: metav1.TypeMeta{},
-					ObjectMeta: metav1.ObjectMeta{
-						Namespace: metav1.NamespaceSystem,
-					},
-					Data: map[string][]uint8{corev1.ServiceAccountRootCAKey: {1, 2, 3}},
-				})
-			},
-			expected: true,
-		},
-		{
-			name: "create cluster certificate fail",
-			createClient: func() kubernetes.Interface {
-				return k8sFake.NewSimpleClientset()
-			},
-			expected: false,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			client := test.createClient()
-			_, _, _, err := CreateClusterCertificateCredentials(client)
-			success := err == nil
-
-			if success != test.expected {
-				t.Fatalf("Expected %v, but got result %v: %v", test.expected, success, err)
-			}
-		})
-	}
-}
 
 func TestCreateClusterServiceAccountCredentials(t *testing.T) {
 	tests := []struct {
