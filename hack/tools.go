8988062288ef732181c6c65894a90040eac174a3
Erick Fejta <fejta@google.com>
2019-10-21 15:33:13 -0700

    Remove tools that repo-infra now handles

--- hack/tools.go
+++ hack/tools.go
@@ -20,11 +20,9 @@ package hack
 
 // Add tools that hack scripts depend on here, to ensure they are vendored.
 import (
-	_ "github.com/bazelbuild/bazel-gazelle/cmd/gazelle"
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/repo-infra/cmd/kazel"
 )
