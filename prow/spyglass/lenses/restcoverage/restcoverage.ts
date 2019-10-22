a4fc8c84d4304fcf3e3d70ab223190c0f42e1fd8
Marcin Franczyk <marcin0franczyk@gmail.com>
2019-10-22 11:22:42 +0200

    fix errors from tslint
    Signed-off-by: Marcin Franczyk <marcin0franczyk@gmail.com>

--- prow/spyglass/lenses/restcoverage/restcoverage.ts
+++ prow/spyglass/lenses/restcoverage/restcoverage.ts
@@ -3,32 +3,32 @@ function addReportExpander(): void {
     const report = document.querySelector<HTMLDivElement>('div#report')!;
     const icon = document.querySelector<HTMLElement>('div#report-brief i.material-icons')!;
     reportBtn.onclick = () => {
-        if(report.classList.toggle('hidden')) {
+        if (report.classList.toggle('hidden')) {
             icon.textContent = "unfold_more";
         } else {
             icon.textContent = "unfold_less";
         }
         spyglass.contentUpdated();
-    }
+    };
 }
 
 function addParamsExpander(): void {
     const methods = document.querySelectorAll<HTMLLIElement>('ul.methods li.method');
     for (const method of Array.from(methods)) {
         const icon = method.querySelector<HTMLElement>('i.material-icons')!;
-        if(icon == null) {
+        if (icon == null) {
             method.style.cursor = "default";
             continue;
         }
         const sibling = method.nextElementSibling!;
         method.onclick = () => {
-            if(sibling.classList.toggle('hidden')) {
-                icon.textContent = "arrow_drop_down"
+            if (sibling.classList.toggle('hidden')) {
+                icon.textContent = "arrow_drop_down";
             } else {
-                icon.textContent = "arrow_drop_up"
+                icon.textContent = "arrow_drop_up";
             }
             spyglass.contentUpdated();
-        }
+        };
     }
 }
 
@@ -36,5 +36,5 @@ function loaded(): void {
     addReportExpander();
     addParamsExpander();
 }
-  
+
 window.addEventListener('DOMContentLoaded', loaded);
