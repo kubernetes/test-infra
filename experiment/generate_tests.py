212248d732d708e021481dca07bd06467a91d61e
Christoph Blecker <admin@toph.ca>
2019-11-05 21:55:24 -0800

    Update kubekins/krte images

--- experiment/generate_tests.py
+++ experiment/generate_tests.py
@@ -45,7 +45,7 @@ PROW_CONFIG_TEMPLATE = """
       containers:
       - args:
         env:
-        image: gcr.io/k8s-testimages/kubekins-e2e:v20191105-e60677a-master
+        image: gcr.io/k8s-testimages/kubekins-e2e:v20191106-09649c7-master
 """
 
 
