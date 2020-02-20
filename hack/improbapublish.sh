9baeca38aa24a0ae213a75fee655687d0f824ac2
Sean Robertson <seanrobertson@improbable.io>
2020-02-20 15:02:54 +0000

    Add new components to the Improbable build targets, and update the build script to use the unified target.

--- hack/improbapublish.sh
+++ hack/improbapublish.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 ###
-# Builds Hook and pushes the container to GCR
+# Builds Hook and pushes the container to the Improbable GCR
 ###
 
 export PROW_REPO_OVERRIDE="eu.gcr.io/windy-oxide-102215"
 export DOCKER_REPO_OVERRIDE="eu.gcr.io/windy-oxide-102215"
 
-bazel run //prow:release-push-hook --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64
+# bazel run //prow:release-push-hook --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64
+bazel run \
+  --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 \
+  //prow:improbable-push
