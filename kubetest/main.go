8d1b174cfb5c27ff8e644edfbbc337458eb7dc5d
Kir Kolyshkin <kolyshkin@gmail.com>
2020-02-21 17:14:26 -0800

    kubetest --help: fix description of --skew
    Since PR https://github.com/kubernetes/kubernetes/pull/84696
    hack/e2e.go is no longer part of kubernetes/kubernetes, so
    let's not mention it here.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- kubetest/main.go
+++ kubetest/main.go
@@ -170,7 +170,7 @@ func defineFlags() *options {
 	flag.StringVar(&o.runtimeConfig, "runtime-config", "batch/v2alpha1=true", "If set, API versions can be turned on or off while bringing up the API server.")
 	flag.StringVar(&o.stage.dockerRegistry, "registry", "", "Push images to the specified docker registry (e.g. gcr.io/a-test-project)")
 	flag.StringVar(&o.save, "save", "", "Save credentials to gs:// path on --up if set (or load from there if not --up)")
-	flag.BoolVar(&o.skew, "skew", false, "If true, run tests in another version at ../kubernetes/hack/e2e.go")
+	flag.BoolVar(&o.skew, "skew", false, "If true, run tests in another version at ../kubernetes/kubernetes_skew")
 	flag.BoolVar(&o.soak, "soak", false, "If true, job runs in soak mode")
 	flag.DurationVar(&o.soakDuration, "soak-duration", 7*24*time.Hour, "Maximum age of a soak cluster before it gets recycled")
 	flag.Var(&o.stage, "stage", "Upload binaries to gs://bucket/devel/job-suffix if set")
