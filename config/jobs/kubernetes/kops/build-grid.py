87132cfbece922dbabcbdcf11dace49e99a02ab6
Ciprian Hacman <ciprian.hacman@sematext.com>
2020-04-28 00:16:09 +0300

    Skip known failed test for Calico

--- config/jobs/kubernetes/kops/build-grid.py
+++ config/jobs/kubernetes/kops/build-grid.py
@@ -156,7 +156,7 @@ def build_test(cloud='aws', distro=None, networking=None):
 
     kops_args = kops_args.strip()
 
-    test_args = r'--ginkgo.skip=\[Slow\]|\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]|\[HPA\]|Dashboard|Services.*functioning.*NodePort' # pylint: disable=line-too-long
+    test_args = r'--ginkgo.skip=\[Slow\]|\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]|\[HPA\]|Dashboard|Services.*functioning.*NodePort|Services.*rejected.*endpoints' # pylint: disable=line-too-long
 
     suffix = ""
     if cloud:
