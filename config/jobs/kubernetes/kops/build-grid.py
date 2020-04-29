a4c76f733501646130e9b4ca83407d08700fdf7c
Justin Santa Barbara <justinsb@google.com>
2020-04-29 10:58:01 -0400

    Run kops grid tests against supported k8s versions
    Reduce the frequency of jobs, defaulting to weekly, so that we don't
    have many more jobs.

--- config/jobs/kubernetes/kops/build-grid.py
+++ config/jobs/kubernetes/kops/build-grid.py
@@ -57,12 +57,12 @@ template = """
 # This should be used for temporary tests we are evaluating,
 # and ideally linked to a bug, and removed once the bug is fixed
 run_hourly = [
-    # flannel networking issues: https://github.com/kubernetes/kops/pull/8381#issuecomment-616689498
-    'kops-grid-aws-flannel-centos7',
-    'kops-grid-aws-flannel-rhel7',
 ]
 
 run_daily = [
+    # flannel networking issues: https://github.com/kubernetes/kops/pull/8381#issuecomment-616689498
+    'kops-grid-aws-flannel-centos7',
+    'kops-grid-aws-flannel-rhel7',
 ]
 
 def simple_hash(s):
@@ -78,7 +78,7 @@ def build_cron(key):
 
     minute = simple_hash("minutes:" + key) % 60
     hour = simple_hash("hours:" + key) % 24
-    #day_of_week = simple_hash("day_of_week:" + key) % 7
+    day_of_week = simple_hash("day_of_week:" + key) % 7
 
     job_count += 1
 
@@ -91,11 +91,8 @@ def build_cron(key):
         runs_per_week += 7
         return "%d %d * * *" % (minute, hour)
 
-    # other jobs will run once per week, but for now let's backfill with once per day
-    #runs_per_week += 1
-    #return "%d %d * * %d" % (minute, hour, day_of_week)
-    runs_per_week += 7
-    return "%d %d * * *" % (minute, hour)
+    runs_per_week += 1
+    return "%d %d * * %d" % (minute, hour, day_of_week)
 
 def remove_line_with_prefix(s, prefix):
     keep = []
@@ -121,22 +118,22 @@ def build_test(cloud='aws', distro=None, networking=None, k8s_version=None):
     elif distro == 'centos7':
         kops_ssh_user = 'centos'
         kops_image = "679593333241/CentOS Linux 7 x86_64 HVM EBS ENA 1901_01-b7ee8a69-ee97-4a49-9e68-afaee216db2e-ami-05713873c6794f575.4" # pylint: disable=line-too-long
-    elif distro == 'debian9':
+    elif distro == 'deb9':
         kops_ssh_user = 'admin'
         kops_image = '379101102735/debian-stretch-hvm-x86_64-gp2-2019-11-13-63558'
-    elif distro == 'debian10':
+    elif distro == 'deb10':
         kops_ssh_user = 'admin'
         kops_image = '136693071363/debian-10-amd64-20200210-166'
     elif distro == 'flatcar':
         kops_ssh_user = 'core'
         kops_image = '075585003325/Flatcar-stable-2303.3.1-hvm'
-    elif distro == 'ubuntu1604':
+    elif distro == 'u1604':
         kops_ssh_user = 'ubuntu'
         kops_image = '099720109477/ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20200407'
-    elif distro == 'ubuntu1804':
+    elif distro == 'u1804':
         kops_ssh_user = 'ubuntu'
         kops_image = '099720109477/ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200408'
-    elif distro == 'ubuntu2004':
+    elif distro == 'u2004':
         kops_ssh_user = 'ubuntu'
         kops_image = '099720109477/ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20200423'
     elif distro == 'rhel7':
@@ -179,7 +176,11 @@ def build_test(cloud='aws', distro=None, networking=None, k8s_version=None):
     if distro:
         suffix += "-" + distro
     if k8s_version:
-        suffix += "-k8s" + k8s_version
+        suffix += "-k" + k8s_version.replace("1.", "")
+
+    # We current have an issue with long cluster names; let's warn if we encounter them
+    if len(suffix) > 24:
+        raise Exception("suffix name %s is probably too long" % (suffix))
 
     # We current have an issue with long cluster names; let's warn if we encounter them
     if len(suffix) > 24:
@@ -229,21 +230,21 @@ distro_options = [
     None,
     'amzn2',
     'centos7',
-    'debian9',
-    'debian10',
+    'deb9',
+    'deb10',
     'flatcar',
     'rhel7',
     'rhel8',
-    'ubuntu1604',
-    'ubuntu1804',
-    'ubuntu2004',
+    'u1604',
+    'u1804',
+    'u2004',
 ]
 
 k8s_versions = [
     None,
-    # "1.16",
-    # "1.17",
-    # "1.18",
+    "1.16",
+    "1.17",
+    "1.18",
 ]
 
 def generate():
