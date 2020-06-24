ae987332b40dfdb4b4b5ec432d8b89181ea55deb
Nikolaos Moraitis <nmoraiti@redhat.com>
2020-06-24 11:44:39 +0200

    introduce the github event server package
    Githubeventserver package handles most of the github
    events. It holds the ServeMuxHandler struct that implements the
    ServeHTTP function, which can be used to initialize a http.ServeMux.
    
    It can be bind options to specify the main endpoint and the port
    in which the http server will listen to.
    
    The package provices a series of registration functions to allow
    each plugin to register its own event handlers
    
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

--- prow/hook/hook_test.go
+++ prow/hook/hook_test.go
@@ -24,6 +24,7 @@ import (
 
 	"k8s.io/test-infra/prow/config"
 	"k8s.io/test-infra/prow/github"
+	"k8s.io/test-infra/prow/githubeventserver"
 	"k8s.io/test-infra/prow/phony"
 	"k8s.io/test-infra/prow/plugins"
 	"k8s.io/test-infra/prow/repoowners"
@@ -107,7 +108,7 @@ func TestHook(t *testing.T) {
 		GitHubClient: github.NewFakeClient(),
 		OwnersClient: repoowners.NewClient(nil, nil, func(org, repo string) bool { return false }, func(org, repo string) bool { return false }, func() config.OwnersDirBlacklist { return config.OwnersDirBlacklist{} }),
 	}
-	metrics := NewMetrics()
+	metrics := githubeventserver.NewMetrics()
 	var testcases = []struct {
 		name           string
 		secret         []byte
