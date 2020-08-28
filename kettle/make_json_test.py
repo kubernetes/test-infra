17445e523213380ec2606c1c0c5ee8de549fd0d9
Grant Mccloskey <gmccloskey@google.com>
2020-08-27 21:41:23 -0700

    Move update attr out of check

--- kettle/make_json_test.py
+++ kettle/make_json_test.py
@@ -153,14 +153,15 @@ class BuildObjectTests(unittest.TestCase):
     ])
     def test_populate_start(self, started, updates):
         build = make_json.Build("gs://kubernetes-jenkins/pr-logs/path", [])
-        base_attrs = {"path":"gs://kubernetes-jenkins/pr-logs/path",
+        attrs = {"path":"gs://kubernetes-jenkins/pr-logs/path",
              "test": [],
              "tests_run": 0,
              "tests_failed": 0,
              "job": "pr:pr-logs",
             }
+        attrs.update(updates)
         build.populate_finish(started)
-        self.assertEqual(build.as_dict(), base_attrs.update(updates))
+        self.assertEqual(build.as_dict(), attrs)
 
 
 class GenerateBuilds(unittest.TestCase):
