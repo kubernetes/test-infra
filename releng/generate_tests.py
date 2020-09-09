02d491891b6157d718c1bfaf4edb7dd01576c334
Chao Dai <chaodai@google.com>
2020-09-09 07:52:30 -0700

    Revert "Update prow to v20200908-94d8b4dfcd, and other images as necessary."
    This reverts commit 7bf5e377a57c206f4a06088988ba9eb86198c816.

--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -45,7 +45,7 @@ PROW_CONFIG_TEMPLATE = """
       containers:
       - args:
         env:
-        image: gcr.io/k8s-testimages/kubekins-e2e:v20200903-fd282bc-master
+        image: gcr.io/k8s-testimages/kubekins-e2e:v20200901-eeeadc5-master
         resources:
           requests:
             cpu: 1000m
