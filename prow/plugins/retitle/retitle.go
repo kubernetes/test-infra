bf30e1e7ec00761fb7342f1d9e1d2e356968da3f
Nikhita Raghunath <nikitaraghunath@gmail.com>
2020-10-08 17:43:02 +0530

    prow/plugins: don't allow invalid keywords in PR titles
    When a PR is merged, the merge commit created by GitHub contains the PR
    title.
    
    So if a PR title contains invalid keywords like @ mentions or a keyword
    which can automatically close issues, its corresponding merge commit
    would also contain these keywords.
    
    If this merge commit is then pulled into another PR, prow would consider
    it invalid.
    
    To avoid problems in the future, this commit disallows such keywords in
    PR titles. The change is made in two plugins:
    
    * invalidcommitmsg - if a PR title contains invalid keywords, the
    `do-not-merge/invalid-commit-message` label is added along with a
    comment.
    
    * retitle - if the new PR title proposed with the /retitle command
    contains invalid keywords, it is rejected and a comment is added.

--- prow/plugins/retitle/retitle.go
+++ prow/plugins/retitle/retitle.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/test-infra/prow/github"
 	"k8s.io/test-infra/prow/pluginhelp"
 	"k8s.io/test-infra/prow/plugins"
+	"k8s.io/test-infra/prow/plugins/invalidcommitmsg"
 	"k8s.io/test-infra/prow/plugins/trigger"
 )
 
@@ -128,6 +129,10 @@ func handleGenericComment(gc githubClient, isTrusted func(string) (bool, error),
 	newTitle := strings.TrimSpace(matches[1])
 	if newTitle == "" {
 		return gc.CreateComment(org, repo, number, plugins.FormatResponseRaw(gce.Body, gce.HTMLURL, user, `Titles may not be empty.`))
+	}
+
+	if invalidcommitmsg.AtMentionRegex.MatchString(newTitle) || invalidcommitmsg.CloseIssueRegex.MatchString(newTitle) {
+		return gc.CreateComment(org, repo, number, plugins.FormatResponseRaw(gce.Body, gce.HTMLURL, user, `Titles may not contain [keywords](https://help.github.com/articles/closing-issues-using-keywords) which can automatically close issues and at(@) mentions.`))
 	}
 
 	if gce.IsPR {
