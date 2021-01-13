37a45bba8ca1acbef74f635da3f0c5d4d34a477f
mpherman2 <mpherman@google.com>
2021-01-12 17:42:02 -0800

    Fix bash script autobumper to use new branch based EnsurePR

--- prow/cmd/autobump/autobump.sh
+++ prow/cmd/autobump/autobump.sh
@@ -122,7 +122,7 @@ create-gh-pr() {
 	/pr-creator \
 		--github-token-path="${token}" \
 		--org="${GH_ORG}" --repo="${GH_REPO}" --branch=master \
-		--title="${title}" --match-title="Bump ${PROW_INSTANCE_NAME} from" \
+		--title="${title}" --head-branch="autobump-${PROW_INSTANCE_NAME}" \
 		--body="${body}" \
 		--source="${user}:autobump-${PROW_INSTANCE_NAME}" \
 		--confirm
