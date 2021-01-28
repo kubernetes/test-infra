ad7fc0f49482e9985d62f0ee50fdece04941f1a2
Chao <chaodai@google.com>
2021-01-28 06:35:17 -0800

    [TESTONLY] Try bazel 4.0.0

--- prow/test/integration/test.sh
+++ prow/test/integration/test.sh
@@ -35,12 +35,12 @@ if ! which kind >/dev/null 2>&1; then
 fi
 
 # TODO(chaodaiG): remove this once bazel is installed in test image
-if ! which bazel >/dev/null 2>&1 || [[ "$(bazel --version)" != "bazel 3.0.0" ]]; then
+if ! which bazel >/dev/null 2>&1 || [[ "$(bazel --version)" != "bazel 4.0.0" ]]; then
   echo "Install bazel for prow"
   mkdir -p "/usr/local/lib/bazel/bin"
   pushd "/usr/local/lib/bazel/bin" >/dev/null
-  curl -LO https://releases.bazel.build/3.0.0/release/bazel-3.0.0-linux-x86_64
-  chmod +x bazel-3.0.0-linux-x86_64
+  curl -LO https://releases.bazel.build/3.0.0/release/bazel-4.0.0-linux-x86_64
+  chmod +x bazel-4.0.0-linux-x86_64
   popd
 fi
 
