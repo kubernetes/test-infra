81c5adb48839d6af0e96b07ba2956f7b727f8c94
Erick Fejta <fejta@google.com>
2021-01-28 13:14:27 -0800

    Update to repo-infra v0.2.0 and bazel 3.4.1

--- prow/plugins/buildifier/buildifier.go
+++ prow/plugins/buildifier/buildifier.go
@@ -125,8 +125,8 @@ func uniqProblems(problems []string) []string {
 
 // problemsInFiles runs buildifier on the files. It returns a map from the file to
 // a list of problems with that file.
-func problemsInFiles(r git.RepoClient, files map[string]string) (map[string][]string, error) {
-	problems := make(map[string][]string)
+func problemsInFiles(r git.RepoClient, files map[string]string) (map[string]bool, error) {
+	problems := map[string]bool{}
 	for f := range files {
 		src, err := ioutil.ReadFile(filepath.Join(r.Directory(), f))
 		if err != nil {
@@ -138,13 +138,10 @@ func problemsInFiles(r git.RepoClient, files map[string]string) (map[string][]st
 		if err != nil {
 			return nil, fmt.Errorf("parsing as Bazel file %v", err)
 		}
-		beforeRewrite := build.Format(content)
-		var info build.RewriteInfo
-		build.Rewrite(content, &info)
+		beforeRewrite := build.FormatWithoutRewriting(content)
 		ndata := build.Format(content)
 		if !bytes.Equal(src, ndata) && !bytes.Equal(src, beforeRewrite) {
-			// TODO(mattmoor): This always seems to be empty?
-			problems[f] = uniqProblems(info.Log)
+			problems[f] = true
 		}
 	}
 	return problems, nil
