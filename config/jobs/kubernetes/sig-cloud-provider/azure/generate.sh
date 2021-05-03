42e1f6f8f1a459c571652aaaf115589f8ca800af
Ernest Wong <chuwon@microsoft.com>
2021-05-03 13:46:04 -0700

    azure: remove --aksengine-location and some aks-engine jobs
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

--- config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
+++ config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
@@ -37,6 +37,8 @@ generate_presubmit_annotations() {
 EOF
 }
 
+readonly kubekins_e2e_image="gcr.io/k8s-testimages/kubekins-e2e:v20210428-a1a20d1"
+
 for release in "$@"; do
   output="${dir}/release-${release}.yaml"
   orchestrator_release="${release}"
@@ -68,7 +70,7 @@ presubmits:
       preset-dind-enabled: "true"
     spec:
       containers:
-      - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
+      - image: ${kubekins_e2e_image}-${release}
         command:
         - runner.sh
         - kubetest
@@ -89,7 +91,6 @@ presubmits:
         - --aksengine-mastervmsize=Standard_DS2_v2
         - --aksengine-agentvmsize=Standard_DS2_v2
         - --aksengine-deploy-custom-k8s
-        - --aksengine-location=westus2
         - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
         - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/cloud-provider-azure/master/tests/k8s-azure/manifest/kubernetes.json
         - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
@@ -118,7 +119,7 @@ $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-confor
       path_alias: sigs.k8s.io/azuredisk-csi-driver
     spec:
       containers:
-      - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
+      - image: ${kubekins_e2e_image}-${release}
         command:
         - runner.sh
         - kubetest
@@ -136,7 +137,6 @@ $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-confor
         - --aksengine-creds=\$(AZURE_CREDENTIALS)
         - --aksengine-orchestratorRelease=${orchestrator_release}
         - --aksengine-deploy-custom-k8s
-        - --aksengine-location=westus2
         - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
         - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/azuredisk-csi-driver/master/test/e2e/manifest/in-tree.json
         - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
@@ -167,7 +167,7 @@ $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-azure-
       path_alias: sigs.k8s.io/azuredisk-csi-driver
     spec:
       containers:
-      - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
+      - image: ${kubekins_e2e_image}-${release}
         command:
         - runner.sh
         - kubetest
@@ -185,7 +185,6 @@ $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-azure-
         - --aksengine-creds=\$(AZURE_CREDENTIALS)
         - --aksengine-orchestratorRelease=${orchestrator_release}
         - --aksengine-deploy-custom-k8s
-        - --aksengine-location=westus2
         - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
         - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/azuredisk-csi-driver/master/test/e2e/manifest/in-tree-vmss.json
         - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
@@ -218,7 +217,7 @@ $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-azure-
       path_alias: sigs.k8s.io/azurefile-csi-driver
     spec:
       containers:
-      - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
+      - image: ${kubekins_e2e_image}-${release}
         command:
         - runner.sh
         - kubetest
@@ -239,7 +238,6 @@ $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-azure-
         - --aksengine-mastervmsize=Standard_DS2_v2
         - --aksengine-agentvmsize=Standard_DS2_v2
         - --aksengine-deploy-custom-k8s
-        - --aksengine-location=westus2
         - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
         - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/test/e2e/manifest/in-tree.json
         - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
@@ -253,222 +251,6 @@ $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-azure-
 $(generate_presubmit_annotations ${branch} pull-kubernetes-e2e-aks-engine-azure-file)
 periodics:
 - interval: 3h
-  name: aks-engine-conformance-${release/./-}
-  decorate: true
-  decoration_config:
-    timeout: 3h
-  labels:
-    preset-service-account: "true"
-    preset-azure-cred: "true"
-    preset-dind-enabled: "true"
-  extra_refs:
-  - org: kubernetes
-    repo: kubernetes
-    base_ref: ${branch}
-    path_alias: k8s.io/kubernetes
-  spec:
-    containers:
-    - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
-      command:
-      - runner.sh
-      - kubetest
-      args:
-      # Generic e2e test args
-      - --test
-      - --up
-      - --down
-      - --build=quick
-      - --dump=\$(ARTIFACTS)
-      # Azure-specific test args
-      - --provider=skeleton
-      - --deployment=aksengine
-      - --aksengine-agentpoolcount=2
-      - --aksengine-admin-username=azureuser
-      - --aksengine-creds=\$(AZURE_CREDENTIALS)
-      - --aksengine-orchestratorRelease=${orchestrator_release}
-      - --aksengine-mastervmsize=Standard_DS2_v2
-      - --aksengine-agentvmsize=Standard_DS2_v2
-      - --aksengine-deploy-custom-k8s
-      - --aksengine-location=westus2
-      - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
-      - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/cloud-provider-azure/master/tests/k8s-azure/manifest/kubernetes.json
-      - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
-      # Specific test args
-      - --test_args=--ginkgo.focus=\[Conformance\]
-      - --ginkgo-parallel=1
-      securityContext:
-        privileged: true
-  annotations:
-    testgrid-dashboards: provider-azure-${release}-signal
-    testgrid-tab-name: aks-engine-conformance
-    testgrid-alert-email: kubernetes-provider-azure@googlegroups.com
-    testgrid-num-columns-recent: '30'
-
-- interval: 24h
-  name: aks-engine-azure-disk-vmas-${release/./-}
-  decorate: true
-  labels:
-    preset-service-account: "true"
-    preset-azure-cred: "true"
-    preset-dind-enabled: "true"
-  extra_refs:
-  - org: kubernetes
-    repo: kubernetes
-    base_ref: ${branch}
-    path_alias: k8s.io/kubernetes
-  - org: kubernetes-sigs
-    repo: azuredisk-csi-driver
-    base_ref: master
-    path_alias: sigs.k8s.io/azuredisk-csi-driver
-  spec:
-    containers:
-    - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
-      command:
-      - runner.sh
-      - kubetest
-      args:
-      # Generic e2e test args
-      - --test
-      - --up
-      - --down
-      - --build=quick
-      - --dump=\$(ARTIFACTS)
-      # Azure-specific test args
-      - --provider=skeleton
-      - --deployment=aksengine
-      - --aksengine-admin-username=azureuser
-      - --aksengine-creds=\$(AZURE_CREDENTIALS)
-      - --aksengine-orchestratorRelease=${orchestrator_release}
-      - --aksengine-deploy-custom-k8s
-      - --aksengine-location=westus2
-      - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
-      - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/azuredisk-csi-driver/master/test/e2e/manifest/in-tree.json
-      - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
-      # Specific test args
-      - --test-azure-disk-csi-driver
-      securityContext:
-        privileged: true
-      env:
-      - name: AZURE_STORAGE_DRIVER
-        value: "kubernetes.io/azure-disk" # In-tree Azure disk storage class
-  annotations:
-    testgrid-dashboards: provider-azure-${release}-signal
-    testgrid-tab-name: aks-engine-azure-disk-vmas
-    testgrid-alert-email: kubernetes-provider-azure@googlegroups.com
-    testgrid-num-columns-recent: '30'
-
-- interval: 24h
-  name: aks-engine-azure-disk-vmss-${release/./-}
-  decorate: true
-  labels:
-    preset-service-account: "true"
-    preset-azure-cred: "true"
-    preset-dind-enabled: "true"
-  extra_refs:
-  - org: kubernetes
-    repo: kubernetes
-    base_ref: ${branch}
-    path_alias: k8s.io/kubernetes
-  - org: kubernetes-sigs
-    repo: azuredisk-csi-driver
-    base_ref: master
-    path_alias: sigs.k8s.io/azuredisk-csi-driver
-  spec:
-    containers:
-    - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
-      command:
-      - runner.sh
-      - kubetest
-      args:
-      # Generic e2e test args
-      - --test
-      - --up
-      - --down
-      - --build=quick
-      - --dump=\$(ARTIFACTS)
-      # Azure-specific test args
-      - --provider=skeleton
-      - --deployment=aksengine
-      - --aksengine-admin-username=azureuser
-      - --aksengine-creds=\$(AZURE_CREDENTIALS)
-      - --aksengine-orchestratorRelease=${orchestrator_release}
-      - --aksengine-deploy-custom-k8s
-      - --aksengine-location=westus2
-      - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
-      - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/azuredisk-csi-driver/master/test/e2e/manifest/in-tree-vmss.json
-      - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
-      # Specific test args
-      - --test-azure-disk-csi-driver
-      securityContext:
-        privileged: true
-      env:
-      - name: AZURE_STORAGE_DRIVER
-        value: "kubernetes.io/azure-disk" # In-tree Azure disk storage class
-      - name: ENABLE_TOPOLOGY
-        value: "true"
-  annotations:
-    testgrid-dashboards: provider-azure-${release}-signal
-    testgrid-tab-name: aks-engine-azure-disk-vmss
-    testgrid-alert-email: kubernetes-provider-azure@googlegroups.com
-    testgrid-num-columns-recent: '30'
-
-- interval: 24h
-  name: aks-engine-azure-file-${release/./-}
-  decorate: true
-  labels:
-    preset-service-account: "true"
-    preset-azure-cred: "true"
-    preset-dind-enabled: "true"
-  extra_refs:
-  - org: kubernetes
-    repo: kubernetes
-    base_ref: ${branch}
-    path_alias: k8s.io/kubernetes
-  - org: kubernetes-sigs
-    repo: azurefile-csi-driver
-    base_ref: master
-    path_alias: sigs.k8s.io/azurefile-csi-driver
-  spec:
-    containers:
-    - image: gcr.io/k8s-testimages/kubekins-e2e:v20171002-6e8d729f-${release}
-      command:
-      - runner.sh
-      - kubetest
-      args:
-      # Generic e2e test args
-      - --test
-      - --up
-      - --down
-      - --build=quick
-      - --dump=\$(ARTIFACTS)
-      # Azure-specific test args
-      - --provider=skeleton
-      - --deployment=aksengine
-      - --aksengine-agentpoolcount=2
-      - --aksengine-admin-username=azureuser
-      - --aksengine-creds=\$(AZURE_CREDENTIALS)
-      - --aksengine-orchestratorRelease=${orchestrator_release}
-      - --aksengine-mastervmsize=Standard_DS2_v2
-      - --aksengine-agentvmsize=Standard_DS2_v2
-      - --aksengine-deploy-custom-k8s
-      - --aksengine-location=westus2
-      - --aksengine-public-key=\$(AZURE_SSH_PUBLIC_KEY_FILE)
-      - --aksengine-template-url=https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/master/test/e2e/manifest/in-tree.json
-      - --aksengine-download-url=https://github.com/Azure/aks-engine/releases/download/nightly/aks-engine-nightly-linux-amd64.tar.gz
-      # Specific test args
-      - --test-azure-file-csi-driver
-      securityContext:
-        privileged: true
-      env:
-      - name: AZURE_STORAGE_DRIVER
-        value: kubernetes.io/azure-file # In-tree Azure file storage class
-  annotations:
-    testgrid-dashboards: provider-azure-${release}-signal
-    testgrid-tab-name: aks-engine-azure-file
-    testgrid-alert-email: kubernetes-provider-azure@googlegroups.com
-    testgrid-num-columns-recent: '30'
-
-- interval: 3h
   name: capz-conformance-${release/./-}
   decorate: true
   decoration_config:
@@ -476,7 +258,6 @@ periodics:
   labels:
     preset-dind-enabled: "true"
     preset-kind-volume-mounts: "true"
-    preset-azure-cred: "true"
     preset-azure-cred-only: "true"
   extra_refs:
   - org: kubernetes-sigs
