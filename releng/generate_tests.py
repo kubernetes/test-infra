74105bd34dc13442a71d8bb618f403a3fc4ce3a9
Kubernetes Prow Robot <k8s.ci.robot@gmail.com>
2021-06-21 14:06:48 +0000

    Bumping K8s-Test-Images
    Multiple distinct gcr.io/k8s-testimages/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/00de27f...c973edd | 2021&#x2011;05&#x2011;09&nbsp;&#x2192;&nbsp;2021&#x2011;06&#x2011;15 | kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/ea6aa4e...c973edd | 2021&#x2011;06&#x2011;01&nbsp;&#x2192;&nbsp;2021&#x2011;06&#x2011;15 | kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/c72cf72...c973edd | 2021&#x2011;06&#x2011;10&nbsp;&#x2192;&nbsp;2021&#x2011;06&#x2011;15 | krte(1.18), krte(1.19), krte(1.20), krte(1.21), krte(experimental), krte(master), kubekins-e2e(1.18), kubekins-e2e(1.19), kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)

--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -45,7 +45,7 @@ PROW_CONFIG_TEMPLATE = """
       containers:
       - args:
         env:
-        image: gcr.io/k8s-testimages/kubekins-e2e:v20210610-c72cf72-master
+        image: gcr.io/k8s-testimages/kubekins-e2e:v20210615-c973edd-master
         resources:
           requests:
             cpu: 1000m
