a1b0b12af7d87e66a916c1465e0691386895055d
Chao Dai <chaodai@google.com>
2021-07-26 11:15:37 -0700

    Revert "Revert "skip_report flag in prowjob only used by crier reporters that report back to CI system""
    This reverts commit 35e82f62e00372f56eab25f40b47aed433c42c33.

--- prow/crier/reporters/github/reporter.go
+++ prow/crier/reporters/github/reporter.go
@@ -120,6 +120,9 @@ func (c *Client) GetName() string {
 
 // ShouldReport returns if this prowjob should be reported by the github reporter
 func (c *Client) ShouldReport(_ context.Context, _ *logrus.Entry, pj *v1.ProwJob) bool {
+	if !pj.Spec.Report {
+		return false
+	}
 
 	switch {
 	case pj.Labels[client.GerritReportLabel] != "":
