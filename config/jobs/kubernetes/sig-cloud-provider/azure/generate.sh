db414c109972d5a8627006a3fa677379d2bb0b62
Kubernetes Prow Robot <k8s.ci.robot@gmail.com>
2021-08-09 18:07:41 +0000

    Bumping K8s-Test-Images
    Multiple distinct gcr.io/k8s-testimages/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/b06ec78...38e1be0 | 2021&#x2011;02&#x2011;04&nbsp;&#x2192;&nbsp;2021&#x2011;08&#x2011;06 | launcher.gcr.io/google/bazel(kubernetes-master), launcher.gcr.io/google/bazel(org), launcher.gcr.io/google/bazel(test-infra)
    https://github.com/kubernetes/test-infra/compare/0f9c540...7bef894 | 2021&#x2011;07&#x2011;07&nbsp;&#x2192;&nbsp;2021&#x2011;08&#x2011;06 | bazelbuild(test-infra)
    https://github.com/kubernetes/test-infra/compare/2b77449...1eaeec7 | 2021&#x2011;07&#x2011;21&nbsp;&#x2192;&nbsp;2021&#x2011;08&#x2011;08 | krte(1.18), krte(1.19), krte(1.20), krte(1.21), krte(1.22), krte(experimental), krte(master), kubekins-e2e(1.18), kubekins-e2e(1.19), kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)

--- config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
+++ config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
@@ -38,7 +38,7 @@ EOF
 }
 
 # we need to define the full image URL so it can be autobumped
-tmp="gcr.io/k8s-testimages/kubekins-e2e:v20210721-2b77449-master"
+tmp="gcr.io/k8s-testimages/kubekins-e2e:v20210808-1eaeec7-master"
 kubekins_e2e_image="${tmp/\-master/}"
 
 for release in "$@"; do
