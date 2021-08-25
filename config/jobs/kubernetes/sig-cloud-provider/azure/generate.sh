90a5d63b426d3611e94ea8c3963e494eb5027e50
Kubernetes Prow Robot <k8s.ci.robot@gmail.com>
2021-08-25 20:07:21 +0000

    Bumping K8s-Test-Images
    Multiple distinct gcr.io/k8s-testimages/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/bb6d84a...f1955d1 | 2021&#x2011;08&#x2011;25&nbsp;&#x2192;&nbsp;2021&#x2011;08&#x2011;25 | kubekins-e2e(1.18), kubekins-e2e(1.19), kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)

--- config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
+++ config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
@@ -38,7 +38,7 @@ EOF
 }
 
 # we need to define the full image URL so it can be autobumped
-tmp="gcr.io/k8s-testimages/kubekins-e2e:v20210825-bb6d84a-master"
+tmp="gcr.io/k8s-testimages/kubekins-e2e:v20210825-f1955d1-master"
 kubekins_e2e_image="${tmp/\-master/}"
 
 for release in "$@"; do
