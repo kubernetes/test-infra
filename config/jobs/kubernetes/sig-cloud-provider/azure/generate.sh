b72f1db5e4e686c30cf74bc923626c16c064afb7
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2021-10-04 15:06:38 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/f2ebda117d...9bdcb71308 | 2021&#x2011;10&#x2011;01&nbsp;&#x2192;&nbsp;2021&#x2011;10&#x2011;01 | kubekins-e2e(1.18), kubekins-e2e(1.19), kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
+++ config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
@@ -38,7 +38,7 @@ EOF
 }
 
 # we need to define the full image URL so it can be autobumped
-tmp="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211001-f2ebda117d-master"
+tmp="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211001-9bdcb71308-master"
 kubekins_e2e_image="${tmp/\-master/}"
 
 for release in "$@"; do
