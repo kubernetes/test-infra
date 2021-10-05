d08a28a37baaafbbe5311fb7260c689a12a061a9
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2021-10-05 14:07:20 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/9bdcb71308...06df33eb71 | 2021&#x2011;10&#x2011;01&nbsp;&#x2192;&nbsp;2021&#x2011;10&#x2011;05 | kubekins-e2e(1.18), kubekins-e2e(1.19), kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -33,7 +33,7 @@ from helpers import ( # pylint: disable=import-error, no-name-in-module
 skip_jobs = [
 ]
 
-image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211001-9bdcb71308-master"
+image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211005-06df33eb71-master"
 
 ##############
 # Build Test #
