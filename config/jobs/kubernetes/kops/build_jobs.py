413311f322dbc31eea4dd2aea6bd4271e55eafb3
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2021-10-15 14:07:32 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/471c27b6e3...7ca1952a94 | 2021&#x2011;09&#x2011;27&nbsp;&#x2192;&nbsp;2021&#x2011;10&#x2011;14 | image-builder
    https://github.com/kubernetes/test-infra/compare/794b7e0910...7ca1952a94 | 2021&#x2011;10&#x2011;14&nbsp;&#x2192;&nbsp;2021&#x2011;10&#x2011;14 | kubekins-e2e(1.18), kubekins-e2e(1.19), kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    https://github.com/kubernetes/test-infra/compare/794b7e0910...75b15aa220 | 2021&#x2011;10&#x2011;14&nbsp;&#x2192;&nbsp;2021&#x2011;10&#x2011;14 | krte(1.18), krte(1.19), krte(1.20), krte(1.21), krte(1.22), krte(experimental), krte(master)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -33,7 +33,7 @@ from helpers import ( # pylint: disable=import-error, no-name-in-module
 skip_jobs = [
 ]
 
-image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211014-794b7e0910-master"
+image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211014-7ca1952a94-master"
 
 ##############
 # Build Test #
