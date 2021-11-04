3be17c9d177d99e0ecfadcfe12b83ee857ee2ef9
Khalid Ali <khalludi123@gmail.com>
2021-11-04 05:05:06 -0500

    Add Docker image for pytest

---
+++ metrics/get_jq.sh
@@ -0,0 +1,7 @@
+
+mkdir jq-1.5
+wget -O - https://github.com/stedolan/jq/releases/download/jq-1.5/jq-osx-amd64 > jq-1.5/jq-osx-amd64
+chmod a+x jq-1.5/jq-osx-amd64
+
+wget -O - https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 > jq-1.5/jq-linux64
+chmod a+x jq-1.5/jq-linux64
