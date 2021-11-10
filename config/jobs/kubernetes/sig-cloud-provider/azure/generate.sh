9e90da5e1692b8238caeaf957b360b6c79b0a31f
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2021-11-10 15:07:33 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/d0d3c6d7a8...cdea54775d | 2021&#x2011;11&#x2011;08&nbsp;&#x2192;&nbsp;2021&#x2011;11&#x2011;10 | krte(1.19), krte(1.20), krte(1.21), krte(1.22), krte(experimental), krte(master)
    https://github.com/kubernetes/test-infra/compare/db1707d3de...cdea54775d | 2021&#x2011;11&#x2011;09&nbsp;&#x2192;&nbsp;2021&#x2011;11&#x2011;10 | kubekins-e2e(1.18), kubekins-e2e(1.19), kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
+++ config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
@@ -38,7 +38,7 @@ EOF
 }
 
 # we need to define the full image URL so it can be autobumped
-tmp="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211109-db1707d3de-master"
+tmp="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20211110-cdea54775d-master"
 kubekins_e2e_image="${tmp/\-master/}"
 
 for release in "$@"; do
