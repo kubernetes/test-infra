fb822fab4d4cb041f407fdec12ec0051de6b5b3f
Chao Dai <chaodai@google.com>
2022-01-14 09:42:08 -0800

    Crier: collects prometheus metrics for times spent reporting

---
+++ prow/crier/metrics.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package crier reports finished prowjob status to git providers.
+package crier
+
+import "github.com/prometheus/client_golang/prometheus"
+
+// Prometheus Metrics
+var (
+	crierMetrics = struct {
+		latency *prometheus.HistogramVec
+	}{
+		latency: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    "crier_report_latency",
+			Help:    "Histogram of time spent reporting, calculated by the time difference between job completion and end of reporting.",
+			Buckets: []float64{1, 10, 20, 30, 60, 120, 300, 600, 1800, 3600},
+		}, []string{
+			"reporter",
+		}),
+	}
+)
+
+func init() {
+	prometheus.MustRegister(crierMetrics.latency)
+}
