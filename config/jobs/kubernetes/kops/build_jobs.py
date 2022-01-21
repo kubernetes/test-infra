802fa645f173c637230d5e947246832279d55466
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2022-01-21 15:07:33 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/c920b4c552...bb77a9cb93 | 2021&#x2011;10&#x2011;01&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;21 | bigquery
    https://github.com/kubernetes/test-infra/compare/e0e089cfce...bb77a9cb93 | 2021&#x2011;10&#x2011;08&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;21 | bootstrap
    https://github.com/kubernetes/test-infra/compare/cac87ea594...bb77a9cb93 | 2022&#x2011;01&#x2011;19&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;21 | krte(1.20), krte(1.21), krte(1.22), krte(1.23), krte(experimental), krte(master)
    https://github.com/kubernetes/test-infra/compare/50581feb62...bb77a9cb93 | 2022&#x2011;01&#x2011;20&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;21 | kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(1.23), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -33,7 +33,7 @@ from helpers import ( # pylint: disable=import-error, no-name-in-module
 skip_jobs = [
 ]
 
-image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220120-50581feb62-master"
+image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220121-bb77a9cb93-master"
 
 loader = jinja2.FileSystemLoader(searchpath="./templates")
 
