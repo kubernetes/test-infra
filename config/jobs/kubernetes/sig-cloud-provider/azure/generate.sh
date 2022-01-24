e9fb0e8cba0209a9e747f778e2f34c1bd73bef4f
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2022-01-24 20:07:06 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/7b86cde1b8...681f3531ec | 2022&#x2011;01&#x2011;21&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;24 | kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(1.23), kubekins-e2e(experimental), kubekins-e2e(master), kubekins-e2e(test-infra)
    https://github.com/kubernetes/test-infra/compare/354980456e...681f3531ec | 2022&#x2011;01&#x2011;21&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;24 | kubekins-e2e(master)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
+++ config/jobs/kubernetes/sig-cloud-provider/azure/generate.sh
@@ -38,7 +38,7 @@ EOF
 }
 
 # we need to define the full image URL so it can be autobumped
-tmp="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220121-7b86cde1b8-master"
+tmp="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220124-681f3531ec-master"
 kubekins_e2e_image="${tmp/\-master/}"
 installCSIdrivers=""
 
