0a8d90af01f4a317982d9956a91807a63396f38b
Chao Dai <chaodai@google.com>
2022-02-08 10:11:34 -0800

    Pushing prow pod utils images using ko
    Local testing looks promising.
    
    Depends on: https://github.com/kubernetes/test-infra/pull/25154

--- prow/prow-images.sh
+++ prow/prow-images.sh
@@ -21,7 +21,38 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 cd $REPO_ROOT
 source hack/build/setup-go.sh
 
+readonly DEFAULT_ARCH="linux/amd64"
 readonly PROW_IMAGES_DEF_FILE="prow/.prow-images"
+GIT_TAG="$(date +v%Y%m%d)-$(git describe --always --dirty)"
+TAG_SET=(
+  "latest"
+  "latest-root"
+  "${GIT_TAG}"
+)
+ALL_ARCHES=("arm64" "s390x" "ppc64le")
+
+# push or local tar?
+PUSH="${PUSH:-false}"
+
+# takes comma separated list of arch
+# returns space separated list of tags basesd on arch
+tags-arg() {
+  local tags=(${TAG_SET[@]})
+  local arches="$1"
+  if [[ "${arches}" == "all" ]]; then
+    for arch in "${ALL_ARCHES[@]}"; do
+      tags+=("${arch}")
+      for base in "${TAG_SET[@]}"; do
+        tags+=("${arch}-${base}")
+      done
+    done
+  fi
+
+  for tag in "${tags[@]}"; do
+    echo "--tags=${tag}"
+  done
+}
+
 IMAGES=()
 while IFS= read -r image; do
   IMAGES+=($image)
@@ -35,11 +66,6 @@ if [[ -z "${KO_DOCKER_REPO}" ]]; then
 fi
 export KO_DOCKER_REPO
 
-# push or local tar?
-PUSH="${PUSH:-false}"
-# overridable auto-tag
-TAG="${TAG:-"$(date +v%Y%m%d)-$(git describe --always --dirty)"}"
-
 # build ko
 cd 'hack/tools'
 go build -o "${REPO_ROOT}/_bin/ko" github.com/google/ko
@@ -49,9 +75,15 @@ echo "Images: ${IMAGES[@]}"
 
 for image in "${IMAGES[@]}"; do
     echo "Building $image"
-    name="$(basename "${image}")"
+    parts=(${image//;/ })
+    image_dir="${parts[0]}"
+    arch="${DEFAULT_ARCH}"
+    if [[ "${#parts[@]}" -gt 1 ]]; then
+      arch="${parts[1]}"
+    fi
+    name="$(basename "${image_dir}")"
     # gather static files if there is any
-    gather_static_file_script="${image}/gather-static.sh"
+    gather_static_file_script="${image_dir}/gather-static.sh"
     if [[ -f $gather_static_file_script ]]; then
       source $gather_static_file_script
     fi
@@ -61,15 +93,16 @@ for image in "${IMAGES[@]}"; do
         publish_args=(--push=true)
     fi
     # specify tag
-    publish_args+=(--base-import-paths --tags="${TAG}" --tags="latest" --tags="latest-root" --platform=linux/amd64)
+    push_tags="$(tags-arg $arch)"
+    publish_args+=(--base-import-paths ${push_tags} --platform="${arch}")
     # actually build
     failed=0
-    (set -x; _bin/ko publish "${publish_args[@]}" ./"${image}") || failed=1
+    (set -x; _bin/ko publish "${publish_args[@]}" ./"${image_dir}") || failed=1
     if [[ -f $gather_static_file_script ]]; then
       CLEAN=true $gather_static_file_script
     fi
     if (( failed )); then
-      echo "Failed building image: ${image}"
+      echo "Failed building image: ${image_dir}"
       exit 1
     fi
 done
