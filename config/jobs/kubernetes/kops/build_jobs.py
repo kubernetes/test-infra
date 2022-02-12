381df1b838dbe76e6c4b6b539db1c0e577184060
Ciprian Hacman <ciprian@hakman.dev>
2022-02-12 12:26:47 +0200

    Add presubmit for Amazon Linux 2

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -988,6 +988,15 @@ def generate_presubmits_e2e():
             always_run=True,
         ),
         presubmit_test(
+            distro="amzn2",
+            networking='calico',
+            k8s_version='stable',
+            kops_channel='alpha',
+            name='pull-kops-e2e-k8s-amzn2',
+            tab_name='e2e-amzn2',
+            always_run=False,
+        ),
+        presubmit_test(
             distro="u2110",
             networking='calico',
             k8s_version='stable',
