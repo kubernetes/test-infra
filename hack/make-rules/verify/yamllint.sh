dd7fcb8faaa504b8322ea684cd43c4841778f055
Quique Llorente <ellorent@redhat.com>
2022-03-15 11:25:35 +0100

    yamllint: Do bind mount with :z
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- hack/make-rules/verify/yamllint.sh
+++ hack/make-rules/verify/yamllint.sh
@@ -33,7 +33,7 @@ LINT_COMMAND=("yamllint" "-c" "config/jobs/.yamllint.conf" "config/jobs" "config
 
 "${DOCKER[@]}" run \
     --rm -i \
-    -v "${REPO_ROOT:?}:${REPO_ROOT:?}" -w "${REPO_ROOT}" \
+    -v "${REPO_ROOT:?}:${REPO_ROOT:?}:z" -w "${REPO_ROOT}" \
     "quay.io/kubermatic/yamllint:0.1" \
     "${LINT_COMMAND[@]}"
 
