eb9ae0e3c041e92913af8ebbe83c03ba1fbc0803
Mark McLoughlin <markmc@redhat.com>
2022-03-30 13:27:51 +0100

    boilerplate: fix pylint warnings
    Address these warnings with pragma controls rather than hacking
    copied-and-pasted code.
    
      hack/boilerplate/verify_boilerplate.py:129:12: W0612: Unused variable 'i' (unused-variable)
      hack/boilerplate/verify_boilerplate.py:87:0: R0911: Too many return statements (8/6) (too-many-return-statements)
      hack/boilerplate/verify_boilerplate.py:87:0: R0912: Too many branches (15/12) (too-many-branches)

--- hack/boilerplate/verify_boilerplate.py
+++ hack/boilerplate/verify_boilerplate.py
@@ -84,7 +84,7 @@ def is_generated(data):
     return False
 
 
-def file_passes(filename, refs, regexs):  # pylint: disable=too-many-locals
+def file_passes(filename, refs, regexs):  # pylint: disable=too-many-locals,too-many-return-statements,too-many-branches
     try:
         # Pass the encoding parameter to avoid ascii decode error for some
         # platform.
@@ -126,6 +126,7 @@ def file_passes(filename, refs, regexs):  # pylint: disable=too-many-locals
 
     # check if we encounter a 'YEAR' placeholder if the file is generated
     if is_generated(file_data):
+        # pylint: disable=unused-variable
         for i, line in enumerate(data):
             if "Copyright YEAR" in line:
                 return False
