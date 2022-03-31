3696a80900e1e4f32ba3b2775b4ece9a2efda280
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2022-03-31 14:07:09 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/55ba9f6da3...3fd3706520 | 2022&#x2011;03&#x2011;23&nbsp;&#x2192;&nbsp;2022&#x2011;03&#x2011;31 | kubekins-e2e(1.20), kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(1.23), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -33,7 +33,7 @@ from helpers import ( # pylint: disable=import-error, no-name-in-module
 skip_jobs = [
 ]
 
-image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220323-55ba9f6da3-master"
+image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220331-3fd3706520-master"
 
 loader = jinja2.FileSystemLoader(searchpath="./templates")
 
