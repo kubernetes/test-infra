a09ab3ef18f7041c7507bb0a5f730f3578abe1f5
Erick Fejta <fejta@google.com>
2022-04-19 11:20:20 -0700

    Revert "Deck: Sort consistently"
    This reverts commit a84db6fb7e215afac9b6bb4fd5bc063b4390d274.
    
    Prevents spyglass from loading builds

--- prow/deck/jobs/jobs.go
+++ prow/deck/jobs/jobs.go
@@ -282,12 +282,7 @@ type byPJStartTime []prowapi.ProwJob
 func (a byPJStartTime) Len() int      { return len(a) }
 func (a byPJStartTime) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a byPJStartTime) Less(i, j int) bool {
-	if a[i].Status.StartTime.Time != a[j].Status.StartTime.Time {
-		return a[i].Status.StartTime.Time.After(a[j].Status.StartTime.Time)
-	}
-	// Start time only has second granularity and we often start many jobs in the
-	// same second. Use the name as tie breaker.
-	return a[i].Spec.Job < a[j].Spec.Job
+	return a[i].Status.StartTime.Time.After(a[j].Status.StartTime.Time)
 }
 
 func (ja *JobAgent) update() error {
