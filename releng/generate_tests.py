b774b7831f68b8b6ec7d5b6eaaf17ed7617756d8
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2022-07-27 15:06:59 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/61b7bafca6...f055b40439 | 2022&#x2011;07&#x2011;26&nbsp;&#x2192;&nbsp;2022&#x2011;07&#x2011;27 | kubekins-e2e(1.21), kubekins-e2e(1.22), kubekins-e2e(1.23), kubekins-e2e(1.24), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No k8s.gcr.io/test-infra changes.

--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -45,7 +45,7 @@ PROW_CONFIG_TEMPLATE = """
       containers:
       - args:
         env:
-        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220726-61b7bafca6-master
+        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220727-f055b40439-master
         resources:
           requests:
             cpu: 1000m
