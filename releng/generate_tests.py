a4dccbb86c368253033f42e6d9d81049762b1b1b
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2022-07-29 14:07:17 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/6b0cfd300e...444b10105a | 2022&#x2011;07&#x2011;08&nbsp;&#x2192;&nbsp;2022&#x2011;07&#x2011;29 | kubekins-e2e(1.24)
    https://github.com/kubernetes/test-infra/compare/61b7bafca6...444b10105a | 2022&#x2011;07&#x2011;26&nbsp;&#x2192;&nbsp;2022&#x2011;07&#x2011;29 | krte(1.22), krte(1.23), krte(1.24), krte(experimental), krte(master)
    https://github.com/kubernetes/test-infra/compare/f055b40439...444b10105a | 2022&#x2011;07&#x2011;27&nbsp;&#x2192;&nbsp;2022&#x2011;07&#x2011;29 | kubekins-e2e(1.22), kubekins-e2e(1.23), kubekins-e2e(1.24), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No k8s.gcr.io/test-infra changes.

--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -45,7 +45,7 @@ PROW_CONFIG_TEMPLATE = """
       containers:
       - args:
         env:
-        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220727-f055b40439-master
+        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20220729-444b10105a-master
         resources:
           requests:
             cpu: 1000m
