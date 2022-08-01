b669ef60a695c3206a06bf072998393b8e2ecab8
boranx <boran.seref@gmail.com>
2022-08-01 23:41:37 +0200

    curtain some of the rules that are reoccuring - those should be fixed by releasing rules individually

--- eslintrc.js
+++ eslintrc.js
@@ -64,6 +64,13 @@ module.exports = {
                 }
             }
         ],
+        "@typescript-eslint/require-await": "warn",
+        "@typescript-eslint/no-floating-promises": "warn",
+        "@typescript-eslint/no-unsafe-call": "warn",
+        "@typescript-eslint/no-unsafe-return": "warn",
+        "@typescript-eslint/no-unsafe-argument": "warn",
+        "@typescript-eslint/no-unsafe-assignment": "warn",
+        "@typescript-eslint/no-misused-promises": "warn",
         "@typescript-eslint/restrict-template-expressions": "warn",
         "@typescript-eslint/no-unsafe-assignment": "warn",
         "@typescript-eslint/no-unsafe-member-access": "off",
