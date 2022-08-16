f9e1b621efefa627c4100430fcec462d5c6f5d2f
Robert Kielty <rob@ii.coop>
2022-08-16 01:03:43 +0100

    Rename INPUT_ variables
    Co-Authored-By: hh <hh@users.noreply.github.com>

--- prow/cmd/hook/kodata/pre-entrypoint.sh
+++ prow/cmd/hook/kodata/pre-entrypoint.sh
@@ -11,21 +11,23 @@ if [ "${INPUT_HMAC}" != "" ]; then
 fi
 
 if [ "${INPUT_GH_APP_ID}" != "" ]; then
-    echo "${INPUT_GH_APP_ID}" > "$HOME/app_id"
+    echo "${INPUT_GH-APP-ID}" > "$HOME/app_id"
 fi
 
 if [ "${INPUT_GH_APP_PK}" != "" ]; then
-    echo "${INPUT_GH_APP_PK}" > "$HOME/app_pkey"
+    echo "${INPUT_GH-APP-PK}" > "$HOME/app_pkey"
 fi
 
 # HBD? Would have to consider whether or not to expose this capability to
 # Project Owners. For proof of concept demo purposes lets use ./config.yaml
 # and ./plugins.yaml from this repo.
 #
-if [ "${INPUT_HOOK_CONFIG}" != "" ]; then
-    echo "${INPUT_HOOK_CONFIG}" > $HOME/config.yaml
+if [ "${INPUT_HOOK-CONFIG}" != "" ]; then
+    echo "${INPUT_HOOK-CONFIG}" > $HOME/config.yaml
 fi
 
 if [ "${INPUT_PLUGIN_CONFIG}" != "" ]; then
-    echo "${INPUT_PLUGIN_CONFIG}" > $HOME/plugins.yaml
+    echo "${INPUT_PLUGIN-CONFIG}" > $HOME/plugins.yaml
 fi
+
+ls -la $HOME/
