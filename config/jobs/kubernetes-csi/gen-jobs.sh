1a96eb67ea6f309031178f711d1ad8de8faceec8
xing-yang <xingyang105@gmail.com>
2022-08-17 15:12:20 -0400

    Add jobs for 1.24 and remove jobs for 1.21

--- config/jobs/kubernetes-csi/gen-jobs.sh
+++ config/jobs/kubernetes-csi/gen-jobs.sh
@@ -24,23 +24,23 @@ base="$(dirname $0)"
 # irrelevant because the prow.sh script will pick a suitable KinD
 # image or build from source.
 k8s_versions="
-1.21
 1.22
 1.23
+1.24
 "
 
 # All the deployment versions we're testing.
 deployment_versions="
-1.21
 1.22
 1.23
+1.24
 "
 
 # The experimental version for which jobs are optional.
-experimental_k8s_version="1.23"
+experimental_k8s_version="1.24"
 
 # The latest stable Kubernetes version for testing alpha jobs
-latest_stable_k8s_version="1.22" # TODO: bump to 1.23 after testing a pull job
+latest_stable_k8s_version="1.23" # TODO: bump to 1.24 after testing a pull job
 
 # Tag of the hostpath driver we should use for sidecar pull jobs
 hostpath_driver_version="v1.8.0"
