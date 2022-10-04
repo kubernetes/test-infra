d2a8015e744abbe4d6a3e21ecb4aaa57e4863a25
Benjamin Elder <bentheelder@google.com>
2022-10-04 15:45:51 -0700

    emeritus fejta

--- kettle/stream_test.py
+++ kettle/stream_test.py
@@ -117,7 +117,7 @@ class HelperTest(unittest.TestCase):
 class StreamTest(unittest.TestCase):
 
     fake_buckets = {'kubernetes-jenkins':
-                        {'contact': 'fejta',
+                        {'contact': 'spiffxp',
                          'prefix': '',
                          'sequential': False,
                          'exclude_jobs': ['ci-test-infra-benchmark-demo',
