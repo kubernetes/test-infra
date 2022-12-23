c55d59e132013f8ab2d5d779a1a9fc5197cf25f9
Kubernetes Prow Robot <k8s-infra-ci-robot@kubernetes.io>
2022-12-23 14:06:57 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra images and test-infra images
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/5d9eb5b181...736a4da5ba | 2022&#x2011;12&#x2011;20&nbsp;&#x2192;&nbsp;2022&#x2011;12&#x2011;23 | kubekins-e2e(experimental)
    https://github.com/kubernetes/test-infra/compare/461b6105cf...736a4da5ba | 2022&#x2011;12&#x2011;21&nbsp;&#x2192;&nbsp;2022&#x2011;12&#x2011;23 | krte(1.22), krte(1.23), krte(1.24), krte(1.25), krte(1.26), krte(experimental), krte(master), kubekins-e2e(1.22), kubekins-e2e(1.23), kubekins-e2e(1.24), kubekins-e2e(1.25), kubekins-e2e(1.26), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No k8s.gcr.io/test-infra changes.

--- config/jobs/kubernetes-csi/gen-jobs.sh
+++ config/jobs/kubernetes-csi/gen-jobs.sh
@@ -46,7 +46,7 @@ latest_stable_k8s_version="1.25" # TODO: bump to 1.26 after testing a pull job
 hostpath_driver_version="v1.10.0"
 
 # We need this image because it has Docker in Docker and go.
-dind_image="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20221221-461b6105cf-master"
+dind_image="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20221223-736a4da5ba-master"
 
 # All kubernetes-csi repos which are part of the hostpath driver example.
 # For these repos we generate the full test matrix. For each entry here
