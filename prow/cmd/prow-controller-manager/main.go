fc7d11ded8b024eabbb8f43d2e89eaf6cf245726
Linus Arver <linusa@google.com>
2023-04-05 16:05:34 -0700

    PCM: document rationale behind requiredTestPodVerbs

--- prow/cmd/prow-controller-manager/main.go
+++ prow/cmd/prow-controller-manager/main.go
@@ -160,6 +160,15 @@ func main() {
 	// We require operating on test pods in build clusters with the following
 	// verbs. This is used during startup to check that we have the necessary
 	// authorizations on build clusters.
+	//
+	// NOTE: Setting up build cluster managers is tricky because if we don't
+	// have the required permissions, the controller manager setup machinery
+	// (library code, not our code) can return an error and this can essentially
+	// result in a fatal error, resulting in a crash loop on startup. Although
+	// other components such as crier, deck, and hook also need to talk to build
+	// clusters, we only perform this preemptive requiredTestPodVerbs check for
+	// PCM and sinker because only these latter components make use of the
+	// BuildClusterManagers() call.
 	requiredTestPodVerbs := []string{
 		"create",
 		"delete",
