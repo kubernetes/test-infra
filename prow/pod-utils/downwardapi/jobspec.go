6fa9466a789a024e87b40e0ac09d3b72931b470b
Dobes Vandermeer <dobes@formative.com>
2023-04-14 18:47:48 -0700

    Expose PR head ref name to job env
    Some tools really want to know the name of the branch being built (chromatic.com for example).
    
    This could also be handy in some places where currently only the PR # is displayed; it would be nice to show the branch name as well as that is generally more user friendly.

--- prow/pod-utils/downwardapi/jobspec.go
+++ prow/pod-utils/downwardapi/jobspec.go
@@ -98,13 +98,14 @@ const (
 	buildIDEnv     = "BUILD_ID"
 	prowBuildIDEnv = "BUILD_NUMBER" // Deprecated, will be removed in the future.
 
-	repoOwnerEnv   = "REPO_OWNER"
-	repoNameEnv    = "REPO_NAME"
-	pullBaseRefEnv = "PULL_BASE_REF"
-	pullBaseShaEnv = "PULL_BASE_SHA"
-	pullRefsEnv    = "PULL_REFS"
-	pullNumberEnv  = "PULL_NUMBER"
-	pullPullShaEnv = "PULL_PULL_SHA"
+	repoOwnerEnv       = "REPO_OWNER"
+	repoNameEnv        = "REPO_NAME"
+	pullBaseRefEnv     = "PULL_BASE_REF"
+	pullBaseShaEnv     = "PULL_BASE_SHA"
+	pullRefsEnv        = "PULL_REFS"
+	pullNumberEnv      = "PULL_NUMBER"
+	pullPullShaEnv     = "PULL_PULL_SHA"
+	pullHeadRefNameEnv = "PULL_HEAD_REF_NAME"
 )
 
 // EnvForSpec returns a mapping of environment variables
@@ -147,6 +148,8 @@ func EnvForSpec(spec JobSpec) (map[string]string, error) {
 
 	env[pullNumberEnv] = strconv.Itoa(spec.Refs.Pulls[0].Number)
 	env[pullPullShaEnv] = spec.Refs.Pulls[0].SHA
+	env[pullHeadRefNameEnv] = spec.Refs.Pulls[0].HeadRefName
+
 	return env, nil
 }
 
@@ -154,7 +157,7 @@ func EnvForSpec(spec JobSpec) (map[string]string, error) {
 func EnvForType(jobType prowapi.ProwJobType) []string {
 	baseEnv := []string{ci, jobNameEnv, JobSpecEnv, jobTypeEnv, prowJobIDEnv, buildIDEnv, prowBuildIDEnv}
 	refsEnv := []string{repoOwnerEnv, repoNameEnv, pullBaseRefEnv, pullBaseShaEnv, pullRefsEnv}
-	pullEnv := []string{pullNumberEnv, pullPullShaEnv}
+	pullEnv := []string{pullNumberEnv, pullPullShaEnv, pullHeadRefNameEnv}
 
 	switch jobType {
 	case prowapi.PeriodicJob:
