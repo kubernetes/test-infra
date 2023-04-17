346aa394c21aa886682136fd0d5e54fc19f4be0e
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2023-04-17 13:14:07 +0300

    fix loopclosure issues in tests
    "make go-lint" fails on clean master right now. Fix the loopclosure
    issues per https://github.com/golang/go/wiki/CommonMistakes guidance.

--- prow/plugins/updateconfig/updateconfig_test.go
+++ prow/plugins/updateconfig/updateconfig_test.go
@@ -1980,6 +1980,8 @@ func TestUpdateSize(t *testing.T) {
 		},
 	}
 	for _, tc := range testCases {
+		// copy to scoped variable, get rid of loopclosure linting error
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			client, err := GetConfigMapClient(fake.NewSimpleClientset().CoreV1(), ns, nil, kube.DefaultClusterAlias)
