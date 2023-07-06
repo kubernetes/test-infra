7b639e45ab714a48eb774cbbeb01bd4f132681e9
Linus Arver <linusa@google.com>
2023-07-06 16:45:19 -0700

    add Moonraker (inrepoconfig caching service)
    Moonraker is a standalone service whose sole purpose is to fetch
    inrepoconfigs from Git providers (GitHub, Gerrit). With Moonraker,
    individual Prow components no longer have to call out to the Git
    providers themselves. Instead, they can simply ask Moonraker for this
    information.
    
    Other benefits are:
    
    - Separation of concerns for Prow components (the concern for looking up
    inrepoconfigs lies solely with Moonraker).
    
    - Shared caching information. The Prow service cluster never asks for
    the same inrepoconfig information multiple times to the Git backend,
    because of the shared cache.

---
+++ prow/moonraker/moonraker.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package moonraker
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+
+	prowapi "k8s.io/test-infra/prow/apis/prowjobs/v1"
+	"k8s.io/test-infra/prow/config"
+)
+
+const (
+	PathGetInrepoconfig = "inrepoconfig"
+)
+
+type Moonraker struct {
+	ConfigAgent       *config.Agent
+	InRepoConfigCache *config.InRepoConfigCache
+}
+
+// payload is the message payload we use for Moonraker. For
+// forward-compatibility, we don't use a prowapi.Refs directly.
+type payload struct {
+	Refs prowapi.Refs `json:"refs"`
+}
+
+type ProwYAMLGetter interface {
+	GetProwYAML(payload *payload) (*config.ProwYAML, error)
+}
+
+// serveGetInrepoconfig returns a ProwYAML object marshaled into JSON.
+func (mr *Moonraker) ServeGetInrepoconfig(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		logrus.WithError(err).Info("unable to read request")
+		http.Error(w, fmt.Sprintf("bad request %v", err), http.StatusBadRequest)
+		return
+	}
+
+	payload := &payload{}
+	err = json.Unmarshal(body, payload)
+	if err != nil {
+		logrus.WithError(err).Info("unable to unmarshal getInrepoconfig request")
+		http.Error(w, fmt.Sprintf("unable to unmarshal getInrepoconfig request: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	baseSHAGetter := func() (string, error) {
+		return payload.Refs.BaseSHA, nil
+	}
+	var headSHAGetters []func() (string, error)
+	for _, pull := range payload.Refs.Pulls {
+		pull := pull
+		headSHAGetters = append(headSHAGetters, func() (string, error) {
+			return pull.SHA, nil
+		})
+	}
+	identifier := payload.Refs.Org + "/" + payload.Refs.Repo
+
+	prowYAML, err := mr.InRepoConfigCache.GetProwYAML(identifier, baseSHAGetter, headSHAGetters...)
+	if err != nil {
+		logrus.WithError(err).Error("unable to retrieve inrepoconfig ProwYAML")
+		http.Error(w, fmt.Sprintf("unable to retrieve inrepoconfig ProwYAML: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(prowYAML); err != nil {
+		logrus.WithError(err).Error("unable to encode inrepoconfig ProwYAML into JSON")
+		http.Error(w, fmt.Sprintf("unable to encode inrepoconfig ProwYAML into JSON: %v", err), http.StatusBadRequest)
+		return
+	}
+}
