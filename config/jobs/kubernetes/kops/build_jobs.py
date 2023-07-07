32fbd918d85e407ca064888f4cce3ef4bd8e4907
Prateek Gogia <pgogia@amazon.com>
2023-07-06 23:24:38 -0500

    Increase control plane count and fix variable name

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -1085,7 +1085,8 @@ def generate_presubmits_scale():
                 'KUBE_NODE_COUNT': "500",
                 'RUN_CL2_TEST': "true",
                 'CL2_SCHEDULER_THROUGHPUT_THRESHOLD': "25",
-                'CONTROL_PLANE_INSTANCE_TYPE': "c6g.16xlarge",
+                'CONTROL_PLANE_COUNT': "3",
+                'CONTROL_PLANE_SIZE': "c6g.16xlarge",
             }
         )
     ]
