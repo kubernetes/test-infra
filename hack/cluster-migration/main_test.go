4a7dc6cc5c4bfc698c8b0ee4ea8e8859a22bd417
rjsadow <richard.j.sadowski@gmail.com>
2023-08-28 08:34:57 -0400

    restructure data to work in a single structure
    Signed-off-by: rjsadow <richard.j.sadowski@gmail.com>

--- hack/cluster-migration/main_test.go
+++ hack/cluster-migration/main_test.go
@@ -17,46 +17,438 @@ limitations under the License.
 package main
 
 import (
-	"reflect"
 	"testing"
 
 	v1 "k8s.io/api/core/v1"
 	cfg "k8s.io/test-infra/prow/config"
 )
 
-func TestValidate(t *testing.T) {
-	tests := []struct {
-		config  Config
-		wantErr bool
-	}{
-		{config: Config{configPath: ""}, wantErr: true},
-		{config: Config{configPath: "path/to/config"}, wantErr: false},
+func TestConfigValidation_EmptyConfigPath(t *testing.T) {
+	config := Config{
+		configPath:    "",
+		jobConfigPath: "somepath",
+		repoReport:    false,
+		repo:          "someRepo",
+		output:        "someOutput",
 	}
 
-	for _, test := range tests {
-		err := test.config.validate()
-		if (err != nil) != test.wantErr {
-			t.Errorf("Expected error: %v, got: %v", test.wantErr, err != nil)
+	err := config.validate()
+	if err == nil {
+		t.Error("Expected an error due to empty configPath, but got none")
+	}
+
+	if err.Error() != "--config must set" {
+		t.Errorf("Expected error message '--config must set', but got: %s", err.Error())
+	}
+}
+
+func TestConfigValidation_ValidConfigPath(t *testing.T) {
+	config := Config{
+		configPath:    "validPath",
+		jobConfigPath: "somepath",
+		repoReport:    false,
+		repo:          "someRepo",
+		output:        "someOutput",
+	}
+
+	err := config.validate()
+	if err != nil {
+		t.Errorf("Did not expect an error, but got: %s", err.Error())
+	}
+}
+
+func TestGetJobStatus_NonDefaultCluster(t *testing.T) {
+	job := cfg.JobBase{
+		Cluster: "test-infra-trusted",
+	}
+
+	cluster, eligible := getJobStatus(job)
+	if cluster != "test-infra-trusted" || !eligible {
+		t.Errorf("Expected cluster 'test-infra-trusted' and eligible true, got cluster '%s' and eligible %v", cluster, eligible)
+	}
+}
+
+func TestGetJobStatus_DefaultClusterEligible(t *testing.T) {
+	type testCase struct {
+		name             string
+		job              cfg.JobBase
+		expectedCluster  string
+		expectedEligible bool
+	}
+	jobs := []testCase{
+		{
+			name: "Test Case: Job is in the default cluster",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: true,
+		},
+		//
+		{
+			name: "Test Case: Job is in the eks-prow-build-cluster cluster",
+			job: cfg.JobBase{
+				Cluster: "eks-prow-build-cluster",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+						},
+					},
+				},
+			},
+			expectedCluster:  "eks-prow-build-cluster",
+			expectedEligible: true,
+		},
+		//
+		{
+			name: "Test Case: Job is in the default cluster and has a cred label",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Labels: map[string]string{
+					"cred": "someValue",
+				},
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job container environment is derived from a secret",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							Env: []v1.EnvVar{
+								{
+									Name: "someVariable",
+									ValueFrom: &v1.EnvVarSource{
+										SecretKeyRef: &v1.SecretKeySelector{
+											Key: "someSecret",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Jobs container environment has \"cred\" in the name",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							Env: []v1.EnvVar{
+								{
+									Name: "somecred",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Jobs container environment has \"Cred\" in the name",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							Env: []v1.EnvVar{
+								{
+									Name: "someCred",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Jobs container arguments don't contain any disallowed words",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							Args: []string{
+								"test",
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: true,
+		},
+		//
+		{
+			name: "Test Case: Jobs container arguments contain \"gcp\"",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							Args: []string{
+								"--gcp-zone=us-central1-f",
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job container command doesn't contain disallowed words",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							Command: []string{
+								"someCommand",
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: true,
+		},
+		//
+		{
+			name: "Test Case: Job container command contains \"gcp\"",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							Command: []string{
+								"someCommand --gcp-zone=us-central1-f",
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job container volume mount contains \"Secret\"",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name: "someSecret",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job container volume mount contains \"secret\"",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name: "somesecret",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job container volume mount contains \"cred\"",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name: "somecred",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job volume contains \"cred\"",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "someCred",
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job volume is derived from unapproved secret",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "someVolume",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: "someSecret",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: false,
+		},
+		//
+		{
+			name: "Test Case: Job volume is derived from approved secret",
+			job: cfg.JobBase{
+				Cluster: "default",
+				Spec: &v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name: "someContainer",
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "someVolume",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: "service-account",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedCluster:  "",
+			expectedEligible: true,
+		},
+	}
+
+	for _, job := range jobs {
+		cluster, eligible := getJobStatus(job.job)
+		if cluster != job.expectedCluster || eligible != job.expectedEligible {
+			t.Errorf("%v: Expected cluster '%s' and eligible %v, got cluster '%s' and eligible %v", job.name, job.expectedCluster, job.expectedEligible, cluster, eligible)
 		}
 	}
 }
 
 func TestGetPercentage(t *testing.T) {
 	tests := []struct {
-		int1 int
-		int2 int
-		want string
+		int1, int2 int
+		expected   float64
 	}{
-		{10, 100, "10.00%"},
-		{1, 3, "33.33%"},
-		{5, 5, "100.00%"},
-		{0, 0, "100.00%"},
+		{10, 100, 10},
+		{5, 10, 50},
+		{0, 10, 0},
+		{10, 0, 100}, // Ensure handling division by zero
 	}
 
 	for _, test := range tests {
-		got := getPercentage(test.int1, test.int2)
-		if got != test.want {
-			t.Errorf("Expected: %v, got: %v", test.want, got)
+		result := getPercentage(test.int1, test.int2)
+		if result != test.expected {
+			t.Errorf("For inputs %d and %d, expected %.2f but got %.2f", test.int1, test.int2, test.expected, result)
+		}
+	}
+}
+
+func TestPrintPercentage(t *testing.T) {
+	tests := []struct {
+		input    float64
+		expected string
+	}{
+		{10.5678, "10.57%"},
+		{50, "50.00%"},
+		{0, "0.00%"},
+	}
+
+	for _, test := range tests {
+		result := printPercentage(test.input)
+		if result != test.expected {
+			t.Errorf("For input %.4f, expected '%s' but got '%s'", test.input, test.expected, result)
 		}
 	}
 }
@@ -64,257 +456,18 @@ func TestGetPercentage(t *testing.T) {
 func TestContainsAny(t *testing.T) {
 	tests := []struct {
 		s        string
-		list     []string
+		words    []string
 		expected bool
 	}{
-		{"testString", []string{"test", "string"}, true},
-		{"testString", []string{"none", "of", "these"}, false},
-		{"", []string{"none", "of", "these"}, false},
+		{"hello world", []string{"hello", "test"}, true},
+		{"hello world", []string{"test", "sample"}, false},
+		{"HELLO WORLD", []string{"hello"}, true}, // Ensure case-insensitivity
 	}
 
-	for _, tt := range tests {
-		result := containsAny(tt.s, tt.list)
-		if result != tt.expected {
-			t.Errorf("for input %q and list %v, expected %v, got %v", tt.s, tt.list, tt.expected, result)
-		}
-	}
-}
-
-func TestGetSortedKeys(t *testing.T) {
-	m := map[string][]string{
-		"b": {},
-		"a": {},
-		"c": {},
-	}
-	want := []string{"a", "b", "c"}
-
-	got := getSortedKeys(m)
-	for i := range want {
-		if got[i] != want[i] {
-			t.Errorf("Expected: %v, got: %v", want, got)
-			break
-		}
-	}
-}
-
-func TestContainsDisallowedLabel(t *testing.T) {
-	tests := []struct {
-		labels   map[string]string
-		expected bool
-	}{
-		{map[string]string{"name": "value"}, false},
-		{map[string]string{"credName": "value"}, true},
-		{map[string]string{"name": "credValue"}, false},
-		{map[string]string{}, false},
-	}
-
-	for _, tt := range tests {
-		result := containsDisallowedLabel(tt.labels)
-		if result != tt.expected {
-			t.Errorf("for labels %v, expected %v, got %v", tt.labels, tt.expected, result)
-		}
-	}
-}
-
-func TestContainsDisallowedAttributes(t *testing.T) {
-	tests := []struct {
-		container v1.Container
-		expected  bool
-	}{
-		{
-			v1.Container{
-				Env:     []v1.EnvVar{{Name: "NAME", Value: "value"}},
-				Args:    []string{"arg1", "arg2"},
-				Command: []string{"cmd1", "cmd2"},
-			},
-			false,
-		},
-		{
-			v1.Container{
-				Env:     []v1.EnvVar{{Name: "credName", Value: "value"}},
-				Args:    []string{"arg1", "arg2"},
-				Command: []string{"cmd1", "cmd2"},
-			},
-			true,
-		},
-		{
-			v1.Container{
-				Env:     []v1.EnvVar{{Name: "NAME", Value: "value"}},
-				Args:    []string{"arg1", "gcloud"},
-				Command: []string{"cmd1", "cmd2"},
-			},
-			true,
-		},
-	}
-
-	for _, tt := range tests {
-		result := containsDisallowedAttributes(tt.container)
-		if result != tt.expected {
-			t.Errorf("for container %v, expected %v, got %v", tt.container, tt.expected, result)
-		}
-	}
-}
-
-func TestContainsDisallowedVolumeMount(t *testing.T) {
-	tests := []struct {
-		volumeMounts []v1.VolumeMount
-		expected     bool
-	}{
-		{
-			[]v1.VolumeMount{
-				{Name: "name1", MountPath: "/path/to/dir1"},
-				{Name: "name2", MountPath: "/path/to/dir2"},
-			},
-			false,
-		},
-		{
-			[]v1.VolumeMount{
-				{Name: "credName", MountPath: "/path/to/dir1"},
-				{Name: "name2", MountPath: "/path/to/dir2"},
-			},
-			true,
-		},
-		{
-			[]v1.VolumeMount{
-				{Name: "name1", MountPath: "/path/to/credDir"},
-				{Name: "name2", MountPath: "/path/to/dir2"},
-			},
-			true,
-		},
-	}
-
-	for _, tt := range tests {
-		result := containsDisallowedVolumeMount(tt.volumeMounts)
-		if result != tt.expected {
-			t.Errorf("for volumeMounts %v, expected %v, got %v", tt.volumeMounts, tt.expected, result)
-		}
-	}
-}
-
-func TestContainsDisallowedVolume(t *testing.T) {
-	tests := []struct {
-		volumes  []v1.Volume
-		expected bool
-	}{
-		{
-			[]v1.Volume{{Name: "name1"}, {Name: "name2"}},
-			false,
-		},
-		{
-			[]v1.Volume{{Name: "credName"}, {Name: "name2"}},
-			true,
-		},
-	}
-
-	for _, tt := range tests {
-		result := containsDisallowedVolume(tt.volumes)
-		if result != tt.expected {
-			t.Errorf("for volumes %v, expected %v, got %v", tt.volumes, tt.expected, result)
-		}
-	}
-}
-
-func TestCheckIfEligable(t *testing.T) {
-	tests := []struct {
-		job      cfg.JobBase
-		expected bool
-	}{
-		// Add various JobBase configurations and test the expected outcome
-		// For example:
-		{
-			cfg.JobBase{Cluster: "test-infra-trusted"},
-			true,
-		},
-		// ... other test cases ...
-	}
-
-	for _, tt := range tests {
-		result := checkIfEligible(tt.job)
-		if result != tt.expected {
-			t.Errorf("for job %v, expected %v, got %v", tt.job, tt.expected, result)
-		}
-	}
-}
-
-func TestAllStaticJobs(t *testing.T) {
-	tests := []struct {
-		config   *cfg.Config
-		expected map[string][]cfg.JobBase
-	}{
-		// Scenario: No jobs in any of the sources
-		{
-			&cfg.Config{JobConfig: cfg.JobConfig{}},
-			map[string][]cfg.JobBase{},
-		},
-		// Scenario: One job in PresubmitsStatic
-		{
-			&cfg.Config{
-				JobConfig: cfg.JobConfig{
-					PresubmitsStatic: map[string][]cfg.Presubmit{
-						"path/to/repo1": {{JobBase: cfg.JobBase{Name: "job1"}}},
-					},
-				},
-			},
-			map[string][]cfg.JobBase{"to": {{Name: "job1"}}},
-		},
-		// Scenario: Multiple jobs in PresubmitsStatic for multiple repos
-		{
-			&cfg.Config{
-				JobConfig: cfg.JobConfig{
-					PresubmitsStatic: map[string][]cfg.Presubmit{
-						"path/to/repo1": {
-							{JobBase: cfg.JobBase{Name: "job1"}},
-							{JobBase: cfg.JobBase{Name: "job2"}},
-						},
-						"path/for/repo2": {
-							{JobBase: cfg.JobBase{Name: "jobA"}},
-						},
-					},
-				},
-			},
-			map[string][]cfg.JobBase{
-				"to":  {{Name: "job1"}, {Name: "job2"}},
-				"for": {{Name: "jobA"}},
-			},
-		},
-		// Scenario: One job in PostsubmitsStatic
-		{
-			&cfg.Config{
-				JobConfig: cfg.JobConfig{
-					PostsubmitsStatic: map[string][]cfg.Postsubmit{
-						"path/to/repo1": {{JobBase: cfg.JobBase{Name: "postJob1"}}},
-					},
-				},
-			},
-			map[string][]cfg.JobBase{"to": {{Name: "postJob1"}}},
-		},
-		// Scenario: Multiple jobs in PostsubmitsStatic for multiple repos
-		{
-			&cfg.Config{
-				JobConfig: cfg.JobConfig{
-					PostsubmitsStatic: map[string][]cfg.Postsubmit{
-						"path/to/repo1": {
-							{JobBase: cfg.JobBase{Name: "postJob1"}},
-							{JobBase: cfg.JobBase{Name: "postJob2"}},
-						},
-						"path/for/repo2": {
-							{JobBase: cfg.JobBase{Name: "postJobA"}},
-						},
-					},
-				},
-			},
-			map[string][]cfg.JobBase{
-				"to":  {{Name: "postJob1"}, {Name: "postJob2"}},
-				"for": {{Name: "postJobA"}},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		result := allStaticJobs(tt.config)
-		// DeepEqual check for map equality
-		if !reflect.DeepEqual(result, tt.expected) {
-			t.Errorf("for config %v, expected %v, got %v", tt.config, tt.expected, result)
+	for _, test := range tests {
+		result := containsAny(test.s, test.words)
+		if result != test.expected {
+			t.Errorf("For input '%s' with words %v, expected %v but got %v", test.s, test.words, test.expected, result)
 		}
 	}
 }
