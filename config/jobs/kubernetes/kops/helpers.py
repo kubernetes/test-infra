36e499c3fc91ca1160508bba020c8dea97cf3d2e
Peter Rifel <pgrifel@gmail.com>
2024-03-05 20:22:34 -0600

    Remove unused Ubuntu 18.04 references

--- config/jobs/kubernetes/kops/helpers.py
+++ config/jobs/kubernetes/kops/helpers.py
@@ -145,7 +145,6 @@ distro_images = {
     'rhel8': latest_aws_image('309956199498', 'RHEL-8.*_HVM-*-x86_64-*'),
     'rhel9': latest_aws_image('309956199498', 'RHEL-9.*_HVM-*-x86_64-*'),
     'rocky8': latest_aws_image('792107900819', 'Rocky-8-ec2-8.*.x86_64'),
-    'u1804': latest_aws_image('099720109477', 'ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*'), # pylint: disable=line-too-long
     'u2004': latest_aws_image('099720109477', 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*'), # pylint: disable=line-too-long
     'u2004arm64': latest_aws_image('099720109477', 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*', 'arm64'), # pylint: disable=line-too-long
     'u2204': latest_aws_image('099720109477', 'ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*'), # pylint: disable=line-too-long
@@ -163,7 +162,6 @@ distros_ssh_user = {
     'rhel8': 'ec2-user',
     'rhel9': 'ec2-user',
     'rocky8': 'rocky',
-    'u1804': 'ubuntu',
     'u2004': 'ubuntu',
     'u2004arm64': 'ubuntu',
     'u2204': 'ubuntu',
