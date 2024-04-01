ded3f14dcda2b41b4f59ad02b5373e442d3d2d8a
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2024-04-01 16:06:24 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-test-infra and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/1ded72f317...fc17bdd16b | 2024&#x2011;02&#x2011;23&nbsp;&#x2192;&nbsp;2024&#x2011;03&#x2011;29 | kubekins-e2e(1.27)
    https://github.com/kubernetes/test-infra/compare/3b134c2624...fc17bdd16b | 2024&#x2011;03&#x2011;08&nbsp;&#x2192;&nbsp;2024&#x2011;03&#x2011;29 | krte(1.26), krte(1.27), krte(1.28), krte(1.29), krte(experimental), krte(master)
    https://github.com/kubernetes/test-infra/compare/b09cdeb92c...fc17bdd16b | 2024&#x2011;03&#x2011;11&nbsp;&#x2192;&nbsp;2024&#x2011;03&#x2011;29 | kubekins-e2e(1.26), kubekins-e2e(1.27), kubekins-e2e(1.28), kubekins-e2e(1.29), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    Multiple distinct us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/5387c97ec5...fc17bdd16b | 2024&#x2011;03&#x2011;07&nbsp;&#x2192;&nbsp;2024&#x2011;03&#x2011;29 | images/kubekins-e2e(master)
    
    No gcr.io/k8s-staging-apisnoop/ changes.
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -33,7 +33,7 @@ from helpers import ( # pylint: disable=import-error, no-name-in-module
 skip_jobs = [
 ]
 
-image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240311-b09cdeb92c-master"
+image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240329-fc17bdd16b-master"
 
 loader = jinja2.FileSystemLoader(searchpath="./templates")
 
--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -49,7 +49,7 @@ PROW_CONFIG_TEMPLATE = """
       - command:
         args:
         env:
-        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240311-b09cdeb92c-master
+        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240329-fc17bdd16b-master
         resources:
           requests:
             cpu: 1000m
