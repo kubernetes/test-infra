767ed147bd60fb76613b7bd94036acf69553c1de
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2024-06-12 22:07:08 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-test-infra and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/17c6d50e24...597c402033 | 2024&#x2011;05&#x2011;15&nbsp;&#x2192;&nbsp;2024&#x2011;06&#x2011;11 | krte(1.27), krte(1.28), krte(1.29), krte(1.30), krte(experimental), krte(master), kubekins-e2e(1.27), kubekins-e2e(1.28), kubekins-e2e(1.29), kubekins-e2e(1.30), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    Multiple distinct us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/17c6d50e24...597c402033 | 2024&#x2011;05&#x2011;15&nbsp;&#x2192;&nbsp;2024&#x2011;06&#x2011;11 | images/kubekins-e2e(master)
    
    No gcr.io/k8s-staging-apisnoop/ changes.
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- config/jobs/kubernetes-csi/gen-jobs.sh
+++ config/jobs/kubernetes-csi/gen-jobs.sh
@@ -46,7 +46,7 @@ latest_stable_k8s_version="1.28"
 hostpath_driver_version="v1.12.1"
 
 # We need this image because it has Docker in Docker and go.
-dind_image="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240515-17c6d50e24-master"
+dind_image="gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240611-597c402033-master"
 
 # All kubernetes-csi repos which are part of the hostpath driver example.
 # For these repos we generate the full test matrix. For each entry here
