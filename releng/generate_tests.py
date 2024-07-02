fbb80edfc8ce205a49568e5f78e9076b1cdcbd4e
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2024-07-02 14:07:01 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-test-infra and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/1dde27f6a9...63170bb1ec | 2024&#x2011;06&#x2011;25&nbsp;&#x2192;&nbsp;2024&#x2011;07&#x2011;01 | kubekins-e2e(1.27), kubekins-e2e(1.28), kubekins-e2e(1.29), kubekins-e2e(1.30), kubekins-e2e(experimental), kubekins-e2e(go-canary), kubekins-e2e(master), kubekins-e2e(test-infra)
    
    No us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes.
    
    No gcr.io/k8s-staging-apisnoop/ changes.
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -49,7 +49,7 @@ PROW_CONFIG_TEMPLATE = """
       - command:
         args:
         env:
-        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240625-1dde27f6a9-master
+        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20240701-63170bb1ec-master
         resources:
           requests:
             cpu: 1000m
