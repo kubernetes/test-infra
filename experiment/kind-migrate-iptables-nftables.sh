1b524fa4f150fa4f9a76c8d457ba58d1a36c5c11
Antonio Ojea <aojea@google.com>
2024-07-19 17:52:20 +0000

    add a job to validate that kube-proxy can switch from iptables to nftables without problems

---
+++ experiment/kind-migrate-iptables-nftables.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+# Copyright 2024 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+# our exit handler (trap)
+cleanup() {
+    # always attempt to dump logs
+    kind "export" logs "${ARTIFACTS}/logs" || true
+    # KIND_IS_UP is true once we: kind create
+    if [[ "${KIND_IS_UP:-}" = true ]]; then
+        kind delete cluster || true
+    fi
+    # clean up e2e.test symlink
+    rm -f _output/bin/e2e.test
+    # remove our tempdir
+    # NOTE: this needs to be last, or it will prevent kind delete
+    if [[ -n "${TMP_DIR:-}" ]]; then
+        rm -rf "${TMP_DIR}"
+    fi
+}
+
+# install kind to a tempdir GOPATH from this script's kind checkout
+install_kind() {
+    # install `kind` to tempdir
+    TMP_DIR=$(mktemp -d)
+    curl -sLo "${TMP_DIR}/kind" https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+    chmod +x "${TMP_DIR}/kind"
+    PATH="${TMP_DIR}:${PATH}"
+    export PATH
+}
+
+# build kubernetes / node image, e2e binaries
+build() {
+    # build the node image w/ kubernetes
+    kind build node-image --kube-root="${PWD}"
+
+    # ensure kubectl is in path
+    local maybe_kubectl
+    maybe_kubectl="$(find "${PWD}/_output" -name "kubectl" -type f)"
+    if [[ -n "${maybe_kubectl}" ]]; then
+        PATH="$(dirname "${maybe_kubectl}"):${PATH}"
+        export PATH
+    fi
+
+    # release some memory after building
+    sync || true
+    echo 1 > /proc/sys/vm/drop_caches || true
+}
+
+
+# up a cluster with kind
+create_cluster() {
+    # create the config file
+    cat <<EOF > "${ARTIFACTS}/kind-config.yaml"
+# config for 1 control plane node and 2 workers
+# necessary for conformance
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+# the control plane node
+- role: control-plane
+- role: worker
+- role: worker
+EOF
+    # mark the cluster as up for cleanup
+    # even if kind create fails, kind delete can clean up after it
+    KIND_IS_UP=true
+
+    KUBECONFIG="${HOME}/.kube/kind-config-default"
+    export KUBECONFIG
+
+    # actually create, with:
+    # - do not delete created nodes from a failed cluster create (for debugging)
+    # - wait up to one minute for the nodes to be "READY"
+    # - set log leve to debug
+    # - use our multi node config
+    kind create cluster \
+        --image=kindest/node:latest \
+        --retain \
+        --wait=1m \
+        --loglevel=debug \
+        "--config=${ARTIFACTS}/kind-config.yaml"
+}
+
+# A function that changes the kube-proxy mode
+# Args:
+#   $1 : kube-proxy mode: nftables, iptables
+set_kubeproxy_mode() {
+    local proxymode="${1:-}"
+    # Get the current config
+    original_kube_proxy=$(kubectl get -oyaml -n=kube-system configmap/kube-proxy)
+    echo "Original kube proxy config:"
+    echo "${original_kube_proxy}"
+    # Patch it
+    fixed_kube_proxy=$(
+        printf '%s' "${original_kube_proxy}" | sed \
+            's/\(.*mode:\)\( .*\)/\1 '"${proxymode}"'/' \
+        )
+    echo "Patched kube-proxy config:"
+    echo "${fixed_kube_proxy}"
+    printf '%s' "${fixed_kube_proxy}" | kubectl apply -f -
+    # restart kube-proxy
+    kubectl -n kube-system rollout restart ds kube-proxy
+    # wait until rollout is complete
+    kubectl -n kube-system rollout status ds kube-proxy --timeout=30s
+}
+
+
+# setup kind, build kubernetes, create a cluster, run the e2es
+main() {
+    # ensure artifacts exists when not in CI
+    ARTIFACTS="${ARTIFACTS:-${PWD}/_artifacts}"
+    mkdir -p "${ARTIFACTS}"
+    export ARTIFACTS
+    # now build an run the cluster and tests
+    trap cleanup EXIT
+    install_kind
+    build
+    create_cluster
+    # run smoke tests
+    set_kubeproxy_mode "nftables"
+
+    set_kubeproxy_mode "iptables"
+
+}
+
+main
