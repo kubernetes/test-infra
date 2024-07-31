1e0462f7a5b2af057b025f6279cddbbd41b02f3a
Davanum Srinivas <davanum@gmail.com>
2024-07-31 15:23:28 -0400

    Latest k8s master does not have Disable feature gates for cloud/credential providers
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -1258,7 +1258,7 @@ def generate_misc():
                        "--set=spec.kubeAPIServer.auditLogPath=/var/log/kube-apiserver-audit.log",
                        "--set=spec.kubeAPIServer.runtimeConfig=api/all=true"
                    ],
-                   kubernetes_feature_gates="AllAlpha,DisableCloudProviders,DisableKubeletCloudCredentialProviders,-EventedPLEG",
+                   kubernetes_feature_gates="AllAlpha,-EventedPLEG",
                    focus_regex=r'\[Feature:(AdmissionWebhookMatchConditions|InPlacePodVerticalScaling|SidecarContainers|StorageVersionAPI|PodPreset|StatefulSetAutoDeletePVC)\]|Networking',
                    skip_regex=r'\[Feature:(SCTPConnectivity|Volumes|Networking-Performance)\]|IPv6|csi-hostpath-v0',
                    test_timeout_minutes=240,
