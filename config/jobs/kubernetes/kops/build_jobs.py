c8754775cf503418f82d5c11ba8120585fb5fc2f
Peter Rifel <pgrifel@gmail.com>
2024-09-11 18:10:22 -0500

    Remove unused artifacts sandbox job

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -507,21 +507,6 @@ def generate_misc():
                    k8s_version='1.29',
                    extra_dashboards=['kops-misc']),
 
-        # A one-off scenario testing the artifacts-sandbox.k8s.io mirror
-        build_test(name_override="kops-artifacts-sandbox",
-                   runs_per_day=3,
-                   cloud="aws",
-                   k8s_version='stable',
-                   extra_dashboards=['kops-misc'],
-                   scenario='smoketest',
-                   env={
-                       'KOPS_BASE_URL': "https://artifacts-sandbox.k8s.io/binaries/kops/1.28.4/",
-                       'KOPS_VERSION': "v1.28.4",
-                       'K8S_VERSION': "v1.28.6",
-                       'KOPS_SKIP_E2E': '1',
-                       'KOPS_CONTROL_PLANE_SIZE': '3',
-                   }),
-
         # A one-off scenario testing AWS EKS Pod Identity
         build_test(name_override="kops-aws-eks-pod-identity",
                    runs_per_day=3,
