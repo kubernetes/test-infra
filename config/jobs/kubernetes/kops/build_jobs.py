29ac49e054454b6769dce7572e024fbfa53c8894
Peter Rifel <pgrifel@gmail.com>
2024-10-07 18:15:51 -0500

    Partially revert discovery store migration

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -130,7 +130,11 @@ def build_test(cloud='aws',
     if irsa and cloud == "aws" and scenario is None:
         if extra_flags is None:
             extra_flags = []
-        extra_flags.append("--discovery-store=s3://k8s-kops-ci-prow/discovery")
+        if build_cluster == "k8s-infra-kops-prow-build":
+            # TODO: migrate to an s3 bucket within the k8s-infra-kops-prow-build cluster's account
+            extra_flags.append("--discovery-store=s3://k8s-kops-prow/discovery")
+        else:
+            extra_flags.append("--discovery-store=s3://k8s-kops-ci-prow/discovery")
 
     marker, k8s_deploy_url, test_package_url, test_package_dir = k8s_version_info(k8s_version)
     args = create_args(kops_channel, networking, extra_flags, kops_image)
