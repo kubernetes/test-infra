f6913f2ec0cbc66bbc231483b68322876f82bdd0
Hemant Kumar <hekumar@redhat.com>
2025-01-06 10:51:49 -0500

    Revert building of e2e from release branches

--- config/jobs/kubernetes-csi/gen-jobs.sh
+++ config/jobs/kubernetes-csi/gen-jobs.sh
@@ -264,7 +264,6 @@ pull_optional() {
     local tests="$1"
     local kubernetes="$2"
     local deployment_suffix="$3"
-    local repo="$4"
 
     # https://github.com/kubernetes-csi/csi-driver-host-path/pull/282 has not been merged yet,
     # therefore pull jobs which depend on the new deployment flavors have to be optional.
@@ -282,19 +281,6 @@ pull_optional() {
 	else
             echo "false"
 	fi
-    fi
-}
-
-kubernetes_branch_name() {
-    local kubernetes="$1"
-    local repo="$2"
-    if [ "$repo" == "external-resizer" ]; then
-	# Since kubernetes tag from which e2e defaults to v1.xx.0 tags, we need to pull
-	# latest branch to get fixes we need for external-resizer. We will remove this workaround
-	# once we upgrade all sidecars to use a newer TAG.
-	echo "\"release-${kubernetes}\""
-    else
-	echo "\"${kubernetes}.0\""
     fi
 }
 
@@ -413,7 +399,7 @@ EOF
         # unrelated to the PR. Testing against the latest Kubernetes is covered
         # by periodic jobs (see https://testgrid.k8s.io/sig-storage-csi-ci#Summary).
         - name: CSI_PROW_KUBERNETES_VERSION
-          value: $(kubernetes_branch_name "$kubernetes" "$repo")
+          value: "$kubernetes.0"
         - name: CSI_PROW_KUBERNETES_DEPLOYMENT
           value: "$deployment"
         - name: CSI_PROW_DEPLOYMENT_SUFFIX
