aaadc74f9345f6c90ee85baa54f7e55d29ac8671
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2025-01-21 19:06:29 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-test-infra and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/3006692a6f...4aed057712 | 2024&#x2011;12&#x2011;30&nbsp;&#x2192;&nbsp;2025&#x2011;01&#x2011;21 | kubekins-e2e(1.29), kubekins-e2e(1.30), kubekins-e2e(1.31), kubekins-e2e(1.32), kubekins-e2e(go-canary), kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/82546285df...a6327e92ae | 2025&#x2011;01&#x2011;11&nbsp;&#x2192;&nbsp;2025&#x2011;01&#x2011;21 | commenter, configurator, issue-creator, label_sync
    
    No us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes.
    
    No gcr.io/k8s-staging-apisnoop/ changes.
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- config/jobs/kubernetes/kops/build_jobs.py
+++ config/jobs/kubernetes/kops/build_jobs.py
@@ -43,7 +43,7 @@ from helpers import ( # pylint: disable=import-error, no-name-in-module
 skip_jobs = [
 ]
 
-image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20241230-3006692a6f-master"
+image = "gcr.io/k8s-staging-test-infra/kubekins-e2e:v20250121-4aed057712-master"
 
 loader = jinja2.FileSystemLoader(searchpath=os.path.join(script_dir, "templates"))
 
