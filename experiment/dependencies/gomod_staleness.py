a0b85132f8ceed9d8cccb737228bed0d98f27881
Davanum Srinivas <davanum@gmail.com>
2025-05-17 11:34:59 -0400

    fix pylint and add boilerplate
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- experiment/dependencies/gomod_staleness.py
+++ experiment/dependencies/gomod_staleness.py
@@ -1,4 +1,20 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
+
+# Copyright 2025 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=line-too-long
 
 import json
 import subprocess
@@ -48,6 +64,7 @@ def sanitize(param):
 
 
 def source(pkg, old, new):
+    # pylint: disable=too-many-return-statements, too-many-branches, no-else-return
     if pkg.startswith("cloud.google.com/go"):
         return "github.com/googleapis/google-cloud-go", old, new
     elif pkg.startswith("golang.org/x/"):
