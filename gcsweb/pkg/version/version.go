53e9d1126d4a2093b369d89ad3fe147567f3d485
Benjamin Elder <bentheelder@google.com>
2025-10-28 21:50:06 -0700

    fix gcsweb lint errors

--- gcsweb/pkg/version/version.go
+++ gcsweb/pkg/version/version.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package version contains version metadata for the binary
 package version
 
 // VERSION is the app-global version string, which should be substituted with a
