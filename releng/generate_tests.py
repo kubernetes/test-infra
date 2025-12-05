c56e11b457f032721e1a592c51359af38a6270bf
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2025-12-05 23:06:41 +0000

    Bumping k8s-testimages images and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-test-infra and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-testimages/ changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/27e1f5f482...d1700a27d1 | 2025&#x2011;12&#x2011;02&nbsp;&#x2192;&nbsp;2025&#x2011;12&#x2011;05 | kubekins-e2e(1.32), kubekins-e2e(1.33), kubekins-e2e(1.34), kubekins-e2e(1.35), kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/1bf8d2445c...d1700a27d1 | 2025&#x2011;12&#x2011;04&nbsp;&#x2192;&nbsp;2025&#x2011;12&#x2011;05 | krte(1.32), krte(1.33), krte(1.34), krte(1.35), krte(master)
    
    Multiple distinct us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/e2c2c9806f...d1700a27d1 | 2025&#x2011;10&#x2011;21&nbsp;&#x2192;&nbsp;2025&#x2011;12&#x2011;05 | images/kubekins-e2e(1.35), images/kubekins-e2e(go-canary), images/kubekins-e2e(master)
    
    No gcr.io/k8s-staging-apisnoop/ changes.
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -49,7 +49,7 @@ PROW_CONFIG_TEMPLATE = """
       - command:
         args:
         env:
-        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20251202-27e1f5f482-master
+        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20251205-d1700a27d1-master
         resources:
           requests:
             cpu: 1000m
