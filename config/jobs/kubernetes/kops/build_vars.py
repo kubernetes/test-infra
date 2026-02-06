94f165250ae2f7dc613243462a8f9fc40a29e5fe
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2026-02-06 22:06:52 +0000

    Bumping Boskos and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-staging-boskos changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/e2c2c9806f...38cfa9523f | 2025&#x2011;10&#x2011;21&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/6ef4f0b08f...38cfa9523f | 2026&#x2011;01&#x2011;08&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | kubekins-e2e(1.35)
    https://github.com/kubernetes/test-infra/compare/f10a7ebcce...38cfa9523f | 2026&#x2011;01&#x2011;27&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | krte(1.32), krte(1.33), krte(1.34), krte(1.35), krte(master), kubekins-e2e(1.32), kubekins-e2e(1.33), kubekins-e2e(1.34), kubekins-e2e(1.35), kubekins-e2e(master)
    
    Multiple distinct us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/855adc2699...38cfa9523f | 2025&#x2011;12&#x2011;09&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | images/kubekins-e2e(1.32), images/kubekins-e2e(1.33), images/kubekins-e2e(1.34), images/kubekins-e2e(1.35)
    https://github.com/kubernetes/test-infra/compare/72d275a594...38cfa9523f | 2025&#x2011;12&#x2011;09&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | images/kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/e2c483ffe9...38cfa9523f | 2026&#x2011;01&#x2011;20&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | images/kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/f10a7ebcce...38cfa9523f | 2026&#x2011;01&#x2011;27&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | images/kubekins-e2e(1.33), images/kubekins-e2e(1.34), images/kubekins-e2e(1.35), images/kubekins-e2e(go-canary)
    https://github.com/kubernetes/test-infra/compare/c1affcc8de...38cfa9523f | 2026&#x2011;01&#x2011;27&nbsp;&#x2192;&nbsp;2026&#x2011;02&#x2011;05 | images/kubekins-e2e(master)
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- config/jobs/kubernetes/kops/build_vars.py
+++ config/jobs/kubernetes/kops/build_vars.py
@@ -17,7 +17,7 @@
 # pylint: disable=line-too-long
 skip_jobs = []
 
-image = "us-central1-docker.pkg.dev/k8s-staging-test-infra/images/kubekins-e2e:v20260127-c1affcc8de-master"
+image = "us-central1-docker.pkg.dev/k8s-staging-test-infra/images/kubekins-e2e:v20260205-38cfa9523f-master"
 
 k8s_versions = [
     "master",
--- releng/generate_tests.py
+++ releng/generate_tests.py
@@ -49,7 +49,7 @@ PROW_CONFIG_TEMPLATE = """
       - command:
         args:
         env:
-        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20260127-f10a7ebcce-master
+        image: gcr.io/k8s-staging-test-infra/kubekins-e2e:v20260205-38cfa9523f-master
         resources:
           requests:
             cpu: 1000m
