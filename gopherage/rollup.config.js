8e7455768e60a0acdddc923e307ffe1a8db2fd2c
s0wa48 <msmaciejsowinski@gmail.com>
2026-02-27 18:28:33 +0100

    refactor(deps): migrate @bazel/terser from v3.1.0 to v5.8.1

--- gopherage/rollup.config.js
+++ gopherage/rollup.config.js
@@ -1,6 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default {
   context: "window",
