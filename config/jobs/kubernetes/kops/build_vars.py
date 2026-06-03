9bb275cdb5e23ddfa04ce8083522e1905787cb0a
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2026-06-03 14:07:09 +0000

    Bumping Boskos and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-staging-boskos changes.
    
    Multiple distinct gcr.io/k8s-staging-test-infra changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/f09cee265c...2cbf4bdb47 | 2026&#x2011;05&#x2011;11&nbsp;&#x2192;&nbsp;2026&#x2011;06&#x2011;01 | kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/f8de63d82b...2cbf4bdb47 | 2026&#x2011;05&#x2011;25&nbsp;&#x2192;&nbsp;2026&#x2011;06&#x2011;01 | kubekins-e2e(1.32), kubekins-e2e(1.33), kubekins-e2e(1.34), kubekins-e2e(1.35), kubekins-e2e(1.36), kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/f8de63d82b...ff716d044c | 2026&#x2011;05&#x2011;25&nbsp;&#x2192;&nbsp;2026&#x2011;06&#x2011;01 | krte(1.34), krte(1.35), krte(1.36), krte(master)
    
    Multiple distinct us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/c27e3ff179...2cbf4bdb47 | 2026&#x2011;05&#x2011;04&nbsp;&#x2192;&nbsp;2026&#x2011;06&#x2011;01 | images/kubekins-e2e(master)
    https://github.com/kubernetes/test-infra/compare/f8de63d82b...2cbf4bdb47 | 2026&#x2011;05&#x2011;25&nbsp;&#x2192;&nbsp;2026&#x2011;06&#x2011;01 | images/kubekins-e2e(1.33), images/kubekins-e2e(1.34), images/kubekins-e2e(1.35), images/kubekins-e2e(1.36), images/kubekins-e2e(master)
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- config/jobs/kubernetes/kops/build_vars.py
+++ config/jobs/kubernetes/kops/build_vars.py
@@ -17,7 +17,7 @@
 # pylint: disable=line-too-long
 skip_jobs = []
 
-image = "us-central1-docker.pkg.dev/k8s-staging-test-infra/images/kubekins-e2e:v20260525-f8de63d82b-master"
+image = "us-central1-docker.pkg.dev/k8s-staging-test-infra/images/kubekins-e2e:v20260601-2cbf4bdb47-master"
 
 k8s_versions = [
     "master",
