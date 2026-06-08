f2880004ac7d6cc9813103fbc16c594ac290343c
alimaazamat <alima.azamat2003@gmail.com>
2026-06-08 14:46:47 -0700

    Fix presubmits

--- experiment/sig-node-kep-board-sync/sync.sh
+++ experiment/sig-node-kep-board-sync/sync.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
-# Copyright 2026 The Kubernetes Authors.
-# SPDX-License-Identifier: Apache-2.0
+# Copyright The Kubernetes Authors.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Mirrors single-select field values from a source GitHub Projects v2 board
 # (e.g. the SIG Release Enhancement Tracking board, project 264) onto a target
 # board (e.g. the SIG Node KEP tracking board, project 265), translating field
