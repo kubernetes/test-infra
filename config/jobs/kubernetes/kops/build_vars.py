8257468e27b883f72bd71fc27e14997f3b1aa8b4
Kubernetes Prow Robot <75457971+k8s-infra-ci-robot@users.noreply.github.com>
2026-06-16 09:19:01 +0000

    Bumping Boskos and k8s-staging-test-infra GCR images and k8s-staging-test-infra AR images and k8s-staging-apisnoop-apisnoop
    No gcr.io/k8s-staging-boskos changes.
    
    No gcr.io/k8s-staging-test-infra changes.
    
    Multiple distinct us-central1-docker.pkg.dev/k8s-staging-test-infra/images changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/ffb9a4e720...605ef6f86b | 2026&#x2011;06&#x2011;15&nbsp;&#x2192;&nbsp;2026&#x2011;06&#x2011;16 | images/kubekins-e2e(1.33), images/kubekins-e2e(1.34), images/kubekins-e2e(1.35), images/kubekins-e2e(1.36), images/kubekins-e2e(master)
    
    No gcr.io/k8s-staging-apisnoop/ changes.

--- config/jobs/kubernetes/kops/build_vars.py
+++ config/jobs/kubernetes/kops/build_vars.py
@@ -17,7 +17,7 @@
 # pylint: disable=line-too-long
 skip_jobs = []
 
-image = "us-central1-docker.pkg.dev/k8s-staging-test-infra/images/kubekins-e2e:v20260615-ffb9a4e720-master"
+image = "us-central1-docker.pkg.dev/k8s-staging-test-infra/images/kubekins-e2e:v20260616-605ef6f86b-master"
 
 k8s_versions = [
     "master",
