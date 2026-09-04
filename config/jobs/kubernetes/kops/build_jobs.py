kubernetes/test-infra

commit c2576cbb70e781a03e83a628d9233cc7591f3df5

Skip additional cilium-eni and amazonvpc jobs on older kops versions

config/jobs/kubernetes/kops/build_jobs.py
@@ -485,13 +485,17 @@ def generate_grid():
                             continue
                         # Fixes for these were backported to 1.34 in https://github.com/kubernetes/kops/pull/17935
                         # but not to any earlier kops versions.
-                        if ((distro_short in ('al2023', 'al2023arm64', 'u2204', 'u2404', 'u2510', 'u2204arm64', 'u2404arm64', 'u2510arm64') and networking == 'amazon-vpc') or
-                        (distro_short in ('u2404', 'u2510', 'u2404arm64', 'u2510arm64') and networking == 'cilium-eni')):
+                        if ((distro_short in ('al2023', 'al2023arm64', 'u2204', 'u2404', 'u2510', 'u2204arm64', 'u2404arm64', 'u2510arm64', 'deb12', 'deb13') and networking == 'amazon-vpc') or
+                        (distro_short in ('u2404', 'u2510', 'u2404arm64', 'u2510arm64', 'deb13', 'al2023', 'al2023arm64') and networking == 'cilium-eni')):
                             continue
                         # Fixes for these were backported to 1.34 in https://github.com/kubernetes/kops/issues/17914
                         # but not to any earlier kops versions.
                         if networking_arg == 'kube-router' and distro_short in ('deb13', 'al2023', 'al2023arm64', 'rhel9', 'flatcar'):
                             continue
+                    # Fixes in https://github.com/kubernetes/kops/pull/17940
+                    # but not backported to earlier kops versions.
+                    if kops_version == '1.34' and (distro_short in ('deb13') and networking == 'amazon-vpc') or (distro_short in ('deb13', 'al2023', 'al2023arm64') and networking == 'cilium-eni'):
+                        continue
                     extra_flags = []
                     if 'arm64' in distro:
                         extra_flags.extend([

commit 0da9a68c580e83d7464671960e26360718aba5b6

[kops] Add an optional cilium-eni presubmit for Amazon Linux 2027
cilium-eni exercises the same Amazon Linux specific networking helpers as
amazonvpc, but through a different caller and without disableManageForeignRoutes,
so the two jobs are not redundant.

This combination has never actually run on Amazon Linux. The grid skips
cilium-eni for al2023, al2023arm64, and deb13 because of an operator precedence
bug introduced in c2576cbb70: the intended `kops_version == '1.34'` guard binds
only to the amazon-vpc clause, so the cilium-eni clause has applied to every
kops version since 2026-02-26, four days after kubernetes/kops#17940 fixed the
combination. Removing that skip is a separate change.

Flags mirror the amazonvpc job, with CILIUM_ENI_EXTRA_CONFIG_FLAGS in place of
AMAZON_VPC_ENV_FLAGS.

config/jobs/kubernetes/kops/build_jobs.py
@@ -2357,6 +2357,20 @@ def generate_presubmits_e2e():
             optional=True,
         ),
         presubmit_test(
+            distro='al2027',
+            k8s_version='stable',
+            kops_channel='alpha',
+            name='pull-kops-e2e-k8s-aws-cilium-eni-al2027',
+            extra_flags=[
+                "--node-size=r5d.xlarge",
+                "--control-plane-size=r5d.xlarge",
+                *CILIUM_ENI_EXTRA_CONFIG_FLAGS,
+            ],
+            networking='cilium-eni',
+            tab_name='e2e-aws-cilium-eni-al2027',
+            optional=True,
+        ),
+        presubmit_test(
             cloud='gce',
             k8s_version='stable',
             distro='deb12arm64',

config/jobs/kubernetes/kops/kops-presubmits-e2e.yaml
@@ -1,5 +1,5 @@
 # Test jobs generated by build_jobs.py (do not manually edit)
-# 41 jobs
+# 42 jobs
 presubmits:
   kubernetes/kops:
 
@@ -460,6 +460,71 @@ presubmits:
       testgrid-dashboards: kops-presubmits, presubmits-kops, sig-cluster-lifecycle-kops
       testgrid-days-of-results: '90'
       testgrid-tab-name: e2e-aws-amazonvpc-al2027
+
+  # {"cloud": "aws", "distro": "al2027", "extra_flags": "--node-size=r5d.xlarge --control-plane-size=r5d.xlarge --set=cluster.spec.networking.cilium.extraConfig=aws-enable-prefix-delegation=true --set=cluster.spec.networking.cilium.extraConfig=ipam-min-allocate=80 --set=cluster.spec.networking.cilium.extraConfig=ipam-pre-allocate=10", "k8s_version": "stable", "kops_channel": "alpha", "networking": "cilium-eni"}
+  - name: pull-kops-e2e-k8s-aws-cilium-eni-al2027
+    cluster: k8s-infra-kops-prow-build
+    branches:
+    - master
+    always_run: false
+    optional: true
+    skip_report: false
+    labels:
+      preset-service-account: "true"
+      preset-k8s-kops-aws-credential: "true"
+    decorate: true
+    decoration_config:
+      timeout: 90m
+    path_alias: k8s.io/kops
+    spec:
+      containers:
+      - image: us-central1-docker.pkg.dev/k8s-staging-test-infra/images/kubekins-e2e:v20260831-6ed6668adc-master
+        imagePullPolicy: Always
+        command:
+        - runner.sh
+        args:
+        - bash
+        - -c
+        - |
+            make test-e2e-install
+            kubetest2 kops \
+            -v 2 \
+            --up --build --down \
+            --cloud-provider=aws \
+            --create-args="--image='137112412989/al2027-preview-ami-2027.0.20260903.0-kernel-7.1-x86_64' --channel=alpha --networking=cilium-eni --node-size=r5d.xlarge --control-plane-size=r5d.xlarge --set=cluster.spec.networking.cilium.extraConfig=aws-enable-prefix-delegation=true --set=cluster.spec.networking.cilium.extraConfig=ipam-min-allocate=80 --set=cluster.spec.networking.cilium.extraConfig=ipam-pre-allocate=10" \
+            --kubernetes-version=https://dl.k8s.io/release/stable.txt \
+            --kops-binary-path=/home/prow/go/src/k8s.io/kops/.build/dist/linux/amd64/kops \
+            --test=kops \
+            -- \
+            --test-args="-test.timeout=60m" \
+            --test-package-marker=stable.txt \
+            --parallel=25
+        securityContext:
+          privileged: true
+        env:
+        - name: KUBE_SSH_USER
+          value: ec2-user
+        - name: GINKGO_NO_COLOR
+          value: "TRUE"
+        - name: GOPATH
+          value: /home/prow/go
+        resources:
+          limits:
+            cpu: "4"
+            memory: 6Gi
+          requests:
+            cpu: "4"
+            memory: 6Gi
+    annotations:
+      test.kops.k8s.io/cloud: aws
+      test.kops.k8s.io/distro: al2027
+      test.kops.k8s.io/extra_flags: --node-size=r5d.xlarge --control-plane-size=r5d.xlarge --set=cluster.spec.networking.cilium.extraConfig=aws-enable-prefix-delegation=true --set=cluster.spec.networking.cilium.extraConfig=ipam-min-allocate=80 --set=cluster.spec.networking.cilium.extraConfig=ipam-pre-allocate=10
+      test.kops.k8s.io/k8s_version: stable
+      test.kops.k8s.io/kops_channel: alpha
+      test.kops.k8s.io/networking: cilium-eni
+      testgrid-dashboards: kops-presubmits, presubmits-kops, sig-cluster-lifecycle-kops
+      testgrid-days-of-results: '90'
+      testgrid-tab-name: e2e-aws-cilium-eni-al2027
 
   # {"cloud": "gce", "distro": "deb12arm64", "extra_flags": "--gce-service-account=default --node-size=t2a-standard-2 --control-plane-size=t2a-standard-2 --zones=us-central1-a", "k8s_version": "stable", "kops_channel": "alpha", "networking": "kindnet"}
   - name: pull-kops-e2e-k8s-gce-distro-debian12-arm64